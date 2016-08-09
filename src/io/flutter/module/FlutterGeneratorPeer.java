76ea66a311c392fe55f3216c03aab1b3e7408842
pq <pquitslund@google.com>
2016-08-09 15:50:38 -0700

    `FlutterModuleBuilder` plumbing (#1).
    Adds a (stubbed) Flutter module builder to the *New Project* wizard.
    
    More context in https://github.com/pq/flutter-intellij/issues/1.

---
+++ src/io/flutter/module/FlutterGeneratorPeer.java
@@ -0,0 +1,26 @@
+//TODO: license
+package io.flutter.module;
+
+import com.intellij.openapi.ui.ComboBox;
+import com.intellij.ui.ComboboxWithBrowseButton;
+import com.intellij.ui.components.JBLabel;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class FlutterGeneratorPeer {
+
+  private JPanel myMainPanel;
+  private ComboboxWithBrowseButton mySdkPathComboWithBrowse;
+  private JBLabel myVersionLabel;
+  private JBLabel myErrorLabel; // shown in IntelliJ IDEA only
+
+  @NotNull
+  public JComponent getComponent() {
+    return myMainPanel;
+  }
+
+  private void createUIComponents() {
+    mySdkPathComboWithBrowse = new ComboboxWithBrowseButton(new ComboBox<>());
+  }
+}
