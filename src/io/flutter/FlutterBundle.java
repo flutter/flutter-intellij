4cc60c5442ac194d4797678e9cffce4f46d0d731
pq <pquitslund@google.com>
2016-08-09 12:58:35 -0700

    Rename packages to `io.flutter`.

--- src/io/flutter/FlutterBundle.java
+++ src/io/flutter/FlutterBundle.java
@@ -1,4 +1,19 @@
-package org.jetbrains.flutter;
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.flutter;
 
 import com.intellij.CommonBundle;
 import org.jetbrains.annotations.NonNls;
@@ -12,7 +27,7 @@ import java.util.ResourceBundle;
 public class FlutterBundle {
 
   private static Reference<ResourceBundle> ourBundle;
-  @NonNls private static final String BUNDLE = "org.jetbrains.flutter.FlutterBundle";
+  @NonNls private static final String BUNDLE = "io.flutter.FlutterBundle";
 
   public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
     return CommonBundle.message(getBundle(), key, params);
