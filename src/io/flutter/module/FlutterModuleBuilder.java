b1385fa1eae0e5138490ebafa66ee1846cd9acdf
pq <pquitslund@google.com>
2016-08-17 15:47:41 -0700

    Basic `flutter create` wizard integration (#1).
    Project creation now:
      * calls `flutter create`
      * adds Dart support
      * opens `lib/main.dart`

--- src/io/flutter/module/FlutterModuleBuilder.java
+++ src/io/flutter/module/FlutterModuleBuilder.java
@@ -5,20 +5,78 @@
  */
 package io.flutter.module;
 
+import com.intellij.execution.ExecutionException;
 import com.intellij.ide.util.projectWizard.ModuleBuilder;
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 import com.intellij.ide.util.projectWizard.WizardContext;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.projectRoots.SdkTypeId;
+import com.intellij.openapi.roots.ContentEntry;
 import com.intellij.openapi.roots.ModifiableRootModel;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
+import com.intellij.openapi.vfs.VirtualFile;
+import icons.FlutterIcons;
+import io.flutter.FlutterBundle;
+import io.flutter.sdk.FlutterSdk;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
 public class FlutterModuleBuilder extends ModuleBuilder {
+
+  @Override
+  public String getName() {
+    return getPresentableName();
+  }
+
+  @Override
+  public String getPresentableName() {
+    return FlutterBundle.message("flutter.module.name");
+  }
+
+  @Override
+  public Icon getBigIcon() {
+    return FlutterIcons.Flutter_16;
+  }
+
+  @Override
+  public Icon getNodeIcon() {
+    return FlutterIcons.Flutter_16;
+  }
+
   @Override
   public void setupRootModel(ModifiableRootModel model) throws ConfigurationException {
+    final ContentEntry contentEntry = doAddContentEntry(model);
+    final VirtualFile baseDir = contentEntry == null ? null : contentEntry.getFile();
+    if (baseDir != null) {
+      setupProject(model, baseDir);
+    }
+  }
+
+  @Override
+  public ModuleWizardStep[] createWizardSteps(@NotNull WizardContext wizardContext, @NotNull ModulesProvider modulesProvider) {
+    //TODO(pq): replace with new custom wizard (or suppress useless frameworks page)
+    return super.createWizardSteps(wizardContext, modulesProvider);
+  }
+
+  @Override
+  public boolean isSuitableSdkType(SdkTypeId sdkType) {
+    // TODO(pq): add FlutterSdkType and implement
+    return super.isSuitableSdkType(sdkType);
+  }
+
+  @Override
+  public boolean validate(Project current, Project dest) {
+
+    if (FlutterSdk.getGlobalFlutterSdk() == null) {
+      //TODO(pq): prompt to configure SDK.
+      return false;
+    }
+
+    //TODO(pq): add validation
+    return super.validate(current, dest);
   }
 
   @Override
@@ -32,27 +90,27 @@ public class FlutterModuleBuilder extends ModuleBuilder {
     return FlutterModuleType.getInstance();
   }
 
-  @Nullable
-  @Override
-  public ModuleWizardStep getCustomOptionsStep(WizardContext context, Disposable parentDisposable) {
-    return new FlutterModuleWizardStep();
+
+  void setupProject(@NotNull final ModifiableRootModel model,
+                    @NotNull final VirtualFile baseDir) {
+    
+    final FlutterSdk sdk = getFlutterSdk();
+    if (sdk == null) {
+      //TODO(pq): prompt to configure SDK.
+      return;
+    }
+
+    // Create files.
+    try {
+      sdk.run(FlutterSdk.Command.CREATE, model.getModule(), baseDir, baseDir.getPath());
+    }
+    catch (ExecutionException e) {
+      //TODO(pq): handle exceptions
+      e.printStackTrace();
+    }
   }
 
-  private class FlutterModuleWizardStep extends ModuleWizardStep {
-
-    private final FlutterGeneratorPeer myPeer;
-
-    FlutterModuleWizardStep() {
-      myPeer = new FlutterGeneratorPeer();
-    }
-
-    @Override
-    public JComponent getComponent() {
-      return myPeer.getComponent();
-    }
-
-    @Override
-    public void updateDataModel() {
-    }
+  FlutterSdk getFlutterSdk() {
+    return FlutterSdk.getGlobalFlutterSdk();
   }
 }
