d9efdb2561165d4ea7715d9870670ef294b69a28
Steve Messick <messick@google.com>
2016-09-07 12:58:04 -0700

    Work around bad constructor decl

---
+++ third_party/intellij-plugins-dart/src/com/jetbrains/lang/dart/ide/runner/server/vmService/DartVmServiceBreakpointHandlerZ.java
@@ -0,0 +1,9 @@
+package com.jetbrains.lang.dart.ide.runner.server.vmService;
+
+import org.jetbrains.annotations.NotNull;
+
+public class DartVmServiceBreakpointHandlerZ extends DartVmServiceBreakpointHandler {
+  protected DartVmServiceBreakpointHandlerZ(@NotNull DartVmServiceDebugProcess debugProcess) {
+    super(debugProcess);
+  }
+}
