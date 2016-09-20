a667fab429d9e3e86b1dff01390189c769532837
pq <pquitslund@google.com>
2016-09-20 13:36:41 -0700

    Console folding for `flutter create`.

--- src/io/flutter/console/FlutterConsoleFolding.java
+++ src/io/flutter/console/FlutterConsoleFolding.java
@@ -9,6 +9,7 @@ import com.intellij.execution.ConsoleFolding;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.CommandLineTokenizer;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.util.PathUtil;
 import io.flutter.sdk.FlutterSdk;
 import io.flutter.sdk.FlutterSdkUtil;
 import org.jetbrains.annotations.Nullable;
@@ -16,51 +17,82 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class FlutterConsoleFolding extends ConsoleFolding {
-  @Override
-  public boolean shouldFoldLine(String line) {
-    if (!line.contains("flutter run")) return false;
+    @Override
+    public boolean shouldFoldLine(String line) {
+        if (!line.contains("flutter run") && !line.contains("flutter --no-color create")) return false;
 
-    try {
-      FlutterSdk sdk = FlutterSdk.getGlobalFlutterSdk();
-      if (sdk == null) return false;
-      final String flutterPath = FlutterSdkUtil.pathToFlutterTool(sdk.getHomePath());
-      return line.startsWith(flutterPath + " run");
-    }
-    catch (ExecutionException e) {
-      return false;
-    }
-  }
-
-  @Nullable
-  @Override
-  public String getPlaceholderText(List<String> lines) {
-    // /Users/.../flutter/bin/flutter run --start-paused --debug-port 50354
-
-    String fullText = StringUtil.join(lines, "\n");
-
-    final CommandLineTokenizer tok = new CommandLineTokenizer(fullText);
-    if (!tok.hasMoreTokens()) return fullText;
-
-    final String filePath = tok.nextToken();
-
-    final StringBuilder builder = new StringBuilder();
-    builder.append("flutter");
-
-    while (tok.hasMoreTokens()) {
-      String token = tok.nextToken();
-
-      // strip off --start-paused
-      if (token.equals("--start-paused")) continue;
-
-      // strip off --debug-port 50354
-      if (token.equals("--debug-port")) {
-        if (tok.hasMoreTokens()) tok.nextToken();
-        continue;
-      }
-
-      builder.append(" ").append(token);
+        try {
+            FlutterSdk sdk = FlutterSdk.getGlobalFlutterSdk();
+            if (sdk == null) return false;
+            final String flutterPath = FlutterSdkUtil.pathToFlutterTool(sdk.getHomePath());
+            return line.startsWith(flutterPath + " run") || line.startsWith(flutterPath + " --no-color create");
+        } catch (ExecutionException e) {
+            return false;
+        }
     }
 
-    return builder.toString();
-  }
+    @Nullable
+    @Override
+    public String getPlaceholderText(List<String> lines) {
+        String fullText = StringUtil.join(lines, "\n");
+        if (fullText.contains("flutter run")) {
+            return flutterRunPlaceholder(fullText);
+        }
+        return flutterCreatePlaceholder(fullText);
+    }
+
+    private String flutterCreatePlaceholder(String fullText) {
+        // /Users/.../flutter --no-color create /Users/.../projectName
+
+        final CommandLineTokenizer tok = new CommandLineTokenizer(fullText);
+        if (!tok.hasMoreTokens()) return fullText;
+
+        final String filePath = tok.nextToken(); // eat flutter binary name
+        final StringBuilder builder = new StringBuilder();
+        builder.append("flutter create");
+
+        while (tok.hasMoreTokens()) {
+            String token = tok.nextToken();
+
+            // strip off --no-color
+            if (token.equals("--no-color")) continue;
+
+            // strip off create
+            if (token.equals("create")) continue;
+
+            String projectName = PathUtil.getFileName(token);
+
+            builder.append(" ").append(projectName);
+        }
+
+        return builder.toString();
+    }
+
+    private String flutterRunPlaceholder(String fullText) {
+        // /Users/.../flutter/bin/flutter run --start-paused --debug-port 50354
+
+        final CommandLineTokenizer tok = new CommandLineTokenizer(fullText);
+        if (!tok.hasMoreTokens()) return fullText;
+
+        final String filePath = tok.nextToken(); // eat flutter binary name
+        final StringBuilder builder = new StringBuilder();
+        builder.append("flutter");
+
+        while (tok.hasMoreTokens()) {
+            String token = tok.nextToken();
+
+            // strip off --start-paused
+            if (token.equals("--start-paused")) continue;
+
+            // strip off --debug-port 50354
+            if (token.equals("--debug-port")) {
+                if (tok.hasMoreTokens()) tok.nextToken();
+                continue;
+            }
+
+            builder.append(" ").append(token);
+        }
+
+        return builder.toString();
+    }
 }
