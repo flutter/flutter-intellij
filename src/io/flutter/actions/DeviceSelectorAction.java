4316d02cf94564d91a476623510a3e8a02f58357
Phil Quitslund <pq@users.noreply.github.com>
2016-09-26 15:18:42 -0700

    Hookup discovered devices to device menu. (#154)

--- src/io/flutter/actions/DeviceSelectorAction.java
+++ src/io/flutter/actions/DeviceSelectorAction.java
@@ -8,39 +8,84 @@ package io.flutter.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.actionSystem.ex.ComboBoxAction;
 import com.intellij.openapi.project.DumbAware;
 import icons.FlutterIcons;
+import io.flutter.run.daemon.ConnectedDevice;
+import io.flutter.run.daemon.FlutterDaemonService;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 
 public class DeviceSelectorAction extends ComboBoxAction implements DumbAware {
 
+  final private List<SelectDeviceAction> actions = new ArrayList<>();
+
+  @NotNull
+  @Override
+  protected DefaultActionGroup createPopupActionGroup(JComponent button) {
+    final DefaultActionGroup group = new DefaultActionGroup();
+    FlutterDaemonService service = FlutterDaemonService.getInstance();
+    if (service != null) {
+      final Collection<ConnectedDevice> devices = service.getConnectedDevices();
+      actions.clear();
+      actions.addAll(devices.stream().map(SelectDeviceAction::new).collect(Collectors.toList()));
+      group.addAll(actions);
+    }
+    return group;
+  }
+
+
+  @Override
+  public void update(AnActionEvent e) {
+    super.update(e);
+    Presentation presentation = e.getPresentation();
+
+    FlutterDaemonService service = FlutterDaemonService.getInstance();
+    if (service == null) {
+      return;
+    }
+
+    ConnectedDevice device = service.getSelectedDevice();
+    if (device == null) {
+      return;
+    }
+
+    for (SelectDeviceAction action : actions) {
+      if (Objects.equals(action.device, device)) {
+        Presentation templatePresentation = action.getTemplatePresentation();
+        presentation.setIcon(templatePresentation.getIcon());
+        presentation.setText(templatePresentation.getText());
+        presentation.setEnabled(true);
+        return;
+      }
+    }
+  }
+
+
+  private static class SelectDeviceAction extends AnAction {
+
     @NotNull
-    @Override
-    protected DefaultActionGroup createPopupActionGroup(JComponent button) {
-        DefaultActionGroup group = new DefaultActionGroup();
-        //TODO: fill in from FlutterDaemonService.getInstance().getConnectedDevices().
-        return group;
+    private final ConnectedDevice device;
+
+    SelectDeviceAction(@NotNull ConnectedDevice device) {
+      super(device.deviceName(), null, FlutterIcons.Phone);
+      this.device = device;
     }
 
     @Override
-    public void update(AnActionEvent e) {
-        //TODO: fill in.
+    public void actionPerformed(AnActionEvent e) {
+      FlutterDaemonService service = FlutterDaemonService.getInstance();
+      if (service != null) {
+        service.setSelectedDevice(device);
+      }
     }
-
-
-    private static class SelectDeviceAction extends AnAction {
-
-        SelectDeviceAction(String deviceName) {
-            super(deviceName, null, FlutterIcons.Flutter);
-        }
-
-        @Override
-        public void actionPerformed(AnActionEvent e) {
-            //TODO: fill in.
-        }
-    }
+  }
 }
