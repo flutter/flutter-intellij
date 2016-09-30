5896d79fd27c01d7144fc7a1da329ac03c83da66
Devon Carew <devoncarew@google.com>
2016-09-30 10:48:06 -0700

    update copyright

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
-# for details. All rights reserved. Use of this source code is governed by a
-# BSD-style license that can be found in the LICENSE file.
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 # Fast fail the script on failures.
 set -e
