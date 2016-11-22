ac29800663a393df7900e9fa2b8abbe7227942a7
pq <pquitslund@google.com>
2016-11-22 15:20:23 -0800

    Run mode behavior re-work (#445).
    Essentially treats run like debug without breakpoints (as described in #445).

--- src/io/flutter/run/FlutterRunnerBase.java
+++ src/io/flutter/run/FlutterRunnerBase.java
@@ -6,30 +6,13 @@
 package io.flutter.run;
 
 
-import com.intellij.execution.ExecutionException;
-import com.intellij.execution.ExecutionResult;
 import com.intellij.execution.configurations.RunProfile;
-import com.intellij.execution.configurations.RunProfileState;
-import com.intellij.execution.configurations.RuntimeConfigurationError;
 import com.intellij.execution.executors.DefaultDebugExecutor;
 import com.intellij.execution.executors.DefaultRunExecutor;
 import com.intellij.execution.runners.DefaultProgramRunner;
-import com.intellij.execution.runners.ExecutionEnvironment;
-import com.intellij.execution.ui.RunContentDescriptor;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.xdebugger.XDebugProcess;
-import com.intellij.xdebugger.XDebugProcessStarter;
-import com.intellij.xdebugger.XDebugSession;
-import com.intellij.xdebugger.XDebuggerManager;
-import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
 import com.jetbrains.lang.dart.ide.runner.ObservatoryConnector;
-import com.jetbrains.lang.dart.ide.runner.server.vmService.DartVmServiceDebugProcessZ;
-import com.jetbrains.lang.dart.sdk.DartSdk;
 import com.jetbrains.lang.dart.util.DartUrlResolver;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -38,8 +21,7 @@ import org.jetbrains.annotations.Nullable;
  * This class is copied from the Dart plugin and modified to use DartVmServiceDebugProcessZ to control the debugger, and to define
  * ObservatoryConnector.
  */
-public class FlutterRunnerBase extends DefaultProgramRunner {
-  private static final Logger LOG = Logger.getInstance(FlutterRunnerBase.class.getName());
+abstract class FlutterRunnerBase extends DefaultProgramRunner {
 
   @NotNull
   @Override
@@ -50,41 +32,7 @@ public class FlutterRunnerBase extends DefaultProgramRunner {
   @Override
   public boolean canRun(final @NotNull String executorId, final @NotNull RunProfile profile) {
     return (profile instanceof FlutterRunConfiguration && (DefaultRunExecutor.EXECUTOR_ID.equals(executorId) ||
-                                                                   DefaultDebugExecutor.EXECUTOR_ID.equals(executorId)));
-  }
-
-  @Override
-  protected RunContentDescriptor doExecute(@NotNull RunProfileState state, @NotNull ExecutionEnvironment env) throws ExecutionException {
-    final String executorId = env.getExecutor().getId();
-
-    if (DefaultRunExecutor.EXECUTOR_ID.equals(executorId)) {
-      return super.doExecute(state, env);
-    }
-
-    if (DefaultDebugExecutor.EXECUTOR_ID.equals(executorId)) {
-      try {
-        final String dasExecutionContextId;
-
-        final RunProfile runConfig = env.getRunProfile();
-        if (runConfig instanceof FlutterRunConfigurationBase &&
-            DartAnalysisServerService.getInstance().serverReadyForRequest(env.getProject())) {
-          final String path = ((FlutterRunConfigurationBase)runConfig).getRunnerParameters().getFilePath();
-          assert path != null; // already checked
-          dasExecutionContextId = DartAnalysisServerService.getInstance().execution_createContext(path);
-        }
-        else {
-          dasExecutionContextId = null; // remote debug or can't start DAS
-        }
-
-        return doExecuteDartDebug(state, env, dasExecutionContextId);
-      }
-      catch (RuntimeConfigurationError e) {
-        throw new ExecutionException(e);
-      }
-    }
-
-    LOG.error("Unexpected executor id: " + executorId);
-    return null;
+                                                           DefaultDebugExecutor.EXECUTOR_ID.equals(executorId)));
   }
 
   protected int getTimeout() {
@@ -96,65 +44,7 @@ public class FlutterRunnerBase extends DefaultProgramRunner {
     return null;
   }
 
-  private RunContentDescriptor doExecuteDartDebug(final @NotNull RunProfileState state,
-                                                  final @NotNull ExecutionEnvironment env,
-                                                  final @Nullable String dasExecutionContextId) throws RuntimeConfigurationError,
-                                                                                                       ExecutionException {
-    final DartSdk sdk = DartSdk.getDartSdk(env.getProject());
-    assert (sdk != null); // already checked
-
-    final RunProfile runConfiguration = env.getRunProfile();
-    final VirtualFile contextFileOrDir;
-    VirtualFile currentWorkingDirectory;
-    final ExecutionResult executionResult;
-    final String debuggingHost;
-    final int observatoryPort;
-
-    if (runConfiguration instanceof FlutterRunConfigurationBase) {
-      contextFileOrDir = ((FlutterRunConfigurationBase)runConfiguration).getRunnerParameters().getDartFile();
-
-      final String cwd =
-        ((FlutterRunConfigurationBase)runConfiguration).getRunnerParameters().computeProcessWorkingDirectory(env.getProject());
-      currentWorkingDirectory = LocalFileSystem.getInstance().findFileByPath((cwd));
-
-      executionResult = state.execute(env.getExecutor(), this);
-      if (executionResult == null) {
-        return null;
-      }
-
-      debuggingHost = null;
-      observatoryPort = ((FlutterAppState)state).getObservatoryPort();
-    }
-    else {
-      LOG.error("Unexpected run configuration: " + runConfiguration.getClass().getName());
-      return null;
-    }
-
-    FileDocumentManager.getInstance().saveAllDocuments();
-
-    final XDebuggerManager debuggerManager = XDebuggerManager.getInstance(env.getProject());
-    final XDebugSession debugSession = debuggerManager.startSession(env, new XDebugProcessStarter() {
-      @Override
-      @NotNull
-      public XDebugProcess start(@NotNull final XDebugSession session) {
-        final DartUrlResolver dartUrlResolver = getDartUrlResolver(env.getProject(), contextFileOrDir);
-        return new DartVmServiceDebugProcessZ(session,
-                                              StringUtil.notNullize(debuggingHost, "localhost"),
-                                              observatoryPort,
-                                              executionResult,
-                                              dartUrlResolver,
-                                              dasExecutionContextId,
-                                              false,
-                                              getTimeout(),
-                                              currentWorkingDirectory,
-                                              getConnector());
-      }
-    });
-
-    return debugSession.getRunContentDescriptor();
-  }
-
-  protected DartUrlResolver getDartUrlResolver(@NotNull final Project project, @NotNull final VirtualFile contextFileOrDir) {
+  DartUrlResolver getDartUrlResolver(@NotNull final Project project, @NotNull final VirtualFile contextFileOrDir) {
     return DartUrlResolver.getInstance(project, contextFileOrDir);
   }
 }
--- src/io/flutter/run/daemon/RunMode.java
+++ src/io/flutter/run/daemon/RunMode.java
@@ -5,22 +5,34 @@
  */
 package io.flutter.run.daemon;
 
+import org.jetbrains.annotations.NonNls;
+
 /**
  * Associate a Java name with the strings passed to Flutter that determine execution mode.
  */
 public enum RunMode {
 
+  @NonNls
   RELEASE("release"),
 
+  @NonNls
   DEBUG("debug") {
     public boolean isDebug() {
       return true;
     }
   },
 
-  PROFILE("profile");
+  @NonNls
+  PROFILE("profile"),
 
-  private String myModeString;
+  @NonNls
+  RUN("run") {
+    public boolean isDebug() {
+      return true;
+    }
+  };
+
+  private final String myModeString;
 
   RunMode(String modeString) {
     myModeString = modeString;
@@ -30,6 +42,9 @@ public enum RunMode {
     return myModeString;
   }
 
+  /**
+   * Returns true if this is a reload/restart enabled mode (run|debug).
+   */
   public boolean isDebug() {
     return false;
   }
