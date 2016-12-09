7872a0bec02e70de6faae1125af1f520da2a0f22
pq <pquitslund@google.com>
2016-12-09 14:23:08 -0800

    Top-level Flutter Tool menu (#515).
    Contents to be filled in.  To start, groups existing upgrade and doctor commands.
    
    See: #515.

---
+++ src/io/flutter/actions/FlutterToolsActionGroup.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2016 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+package io.flutter.actions;
+
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+
+public class FlutterToolsActionGroup extends DefaultActionGroup {
+
+}
