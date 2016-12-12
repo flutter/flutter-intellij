52843630963504966a26c88de106cfa5c164c849
Devon Carew <devoncarew@google.com>
2016-12-12 06:15:16 -0800

    add feedback and getting started menu items (#520)
    * add feedback and getting started menu items
    
    * mention the menu item

---
+++ src/io/flutter/actions/FlutterGettingStarted.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2016 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+package io.flutter.actions;
+
+import com.intellij.ide.browsers.BrowserLauncher;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.DumbAwareAction;
+import org.jetbrains.annotations.NotNull;
+
+public class FlutterGettingStarted extends DumbAwareAction {
+  @Override
+  public void actionPerformed(@NotNull final AnActionEvent e) {
+    final String url = "https://flutter.io/intellij-ide/";
+    BrowserLauncher.getInstance().browse(url, null);
+  }
+}
