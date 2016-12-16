eeb1048aadaaf533ba1846683dfe1fc088c3edfc
Devon Carew <devoncarew@google.com>
2016-12-16 00:04:55 -0800

    improve the console folding for iOS

--- src/io/flutter/console/FlutterConsoleFolding.java
+++ src/io/flutter/console/FlutterConsoleFolding.java
@@ -25,6 +25,11 @@ public class FlutterConsoleFolding extends ConsoleFolding {
   // CoreSimulatorBridge: Requesting launch of ... with options: {
   private static final Pattern iosPattern = Pattern.compile("^\\w+: .* \\{$");
 
+  //     [x86_64] libnetcore-856.20.4
+  // 0   libsystem_network.dylib             0x0000000111918682 __nw_create_backtrace_string + 123
+  // 1   libnetwork.dylib                    0x0000000111ab2932 nw_socket_add_input_handler + 3100
+  private static final String iosCrashPrefix = "\t        [";
+
   @Override
   public boolean shouldFoldLine(String line) {
     if (line.contains(marker)) {
@@ -32,18 +37,18 @@ public class FlutterConsoleFolding extends ConsoleFolding {
       return true;
     }
 
-    if (isFolding && line.startsWith(("\t"))) {
-      return true;
-    }
-
-    if (iosPattern.matcher(line).matches()) {
+    if (iosPattern.matcher(line).matches() || line.startsWith(iosCrashPrefix)) {
       isFolding = true;
       return false;
     }
 
-    isFolding = false;
-
-    return false;
+    if (isFolding && line.startsWith(("\t"))) {
+      return true;
+    }
+    else {
+      isFolding = false;
+      return false;
+    }
   }
 
   @Nullable
@@ -52,7 +57,12 @@ public class FlutterConsoleFolding extends ConsoleFolding {
     final String fullText = StringUtil.join(lines, "\n");
     final int index = fullText.indexOf(marker);
     if (index == -1) {
-      return " ... }";
+      if (lines.stream().anyMatch((s) -> s.endsWith("}"))) {
+        return " ... }";
+      }
+      else {
+        return " ...";
+      }
     }
     else {
       return "flutter " + fullText.substring(index + marker.length());
