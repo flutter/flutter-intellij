7d2dddd580c9cb662483fc59429531b33ff5b25e
pq <pquitslund@google.com>
2017-05-03 14:21:02 -0700

    Console hyperlink support for opening (xcode) resources.
    Related to: https://github.com/flutter/flutter-intellij/issues/880

--- src/io/flutter/console/FlutterConsoleFilter.java
+++ src/io/flutter/console/FlutterConsoleFilter.java
@@ -6,9 +6,13 @@
 package io.flutter.console;
 
 import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.filters.Filter;
 import com.intellij.execution.filters.HyperlinkInfo;
 import com.intellij.execution.filters.OpenFileHyperlinkInfo;
+import com.intellij.execution.process.OSProcessHandler;
+import com.intellij.execution.process.ProcessAdapter;
+import com.intellij.execution.process.ProcessEvent;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
@@ -22,6 +26,38 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class FlutterConsoleFilter implements Filter {
+
+  private static class OpenExternalFileHyperlink implements HyperlinkInfo {
+
+    private final String myPath;
+
+    OpenExternalFileHyperlink(VirtualFile file) {
+      myPath = file.getPath();
+    }
+
+    @Override
+    public void navigate(Project project) {
+      try {
+        final GeneralCommandLine cmd = new GeneralCommandLine().withExePath("open").withParameters(myPath);
+        final OSProcessHandler handler = new OSProcessHandler(cmd);
+        handler.addProcessListener(new ProcessAdapter() {
+          @Override
+          public void processTerminated(final ProcessEvent event) {
+            if (event.getExitCode() != 0) {
+              FlutterMessages.showError("Error Opening ", myPath);
+            }
+          }
+        });
+        handler.startNotify();
+      }
+      catch (ExecutionException e) {
+        FlutterMessages.showError(
+          "Error Opening External File",
+          "Exception: " + e.getMessage());
+      }
+    }
+  }
+
 
   private static final Logger LOG = Logger.getInstance(FlutterConsoleFilter.class);
 
@@ -37,13 +73,24 @@ public class FlutterConsoleFilter implements Filter {
       return getFlutterDoctorResult(line, entireLength - line.length());
     }
 
+    boolean openAsExternalFile = false;
+
     String pathPart = line.trim();
 
     // Check for, e.g., "Launching lib/main.dart"
-    if (line.startsWith("Launching ")) {
-      final String[] parts = line.split(" ");
+    if (pathPart.startsWith("Launching ")) {
+      final String[] parts = pathPart.split(" ");
       if (parts.length > 1) {
         pathPart = parts[1];
+      }
+    }
+
+    // Check for, e.g., "open ios/Runner.xcworkspace"
+    if (pathPart.startsWith("open ")) {
+      final String[] parts = pathPart.split(" ");
+      if (parts.length > 1) {
+        pathPart = parts[1];
+        openAsExternalFile = true;
       }
     }
 
@@ -55,7 +102,10 @@ public class FlutterConsoleFilter implements Filter {
       final VirtualFile file = LocalFileSystem.getInstance().findFileByPath(path);
       if (!pathPart.isEmpty() && file != null && file.exists()) {
         final int lineStart = entireLength - line.length() + line.indexOf(pathPart);
-        return new Result(lineStart, lineStart + pathPart.length(), new OpenFileHyperlinkInfo(module.getProject(), file, 0, 0));
+
+        final HyperlinkInfo hyperlinkInfo =
+          openAsExternalFile ? new OpenExternalFileHyperlink(file) : new OpenFileHyperlinkInfo(module.getProject(), file, 0, 0);
+        return new Result(lineStart, lineStart + pathPart.length(), hyperlinkInfo);
       }
     }
 
