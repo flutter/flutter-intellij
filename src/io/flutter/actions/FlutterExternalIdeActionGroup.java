8628585630c9a1b31e45283031f47fc8aa881ed0
pq <pquitslund@google.com>
2017-05-30 14:16:01 -0700

    Improve “Open in Xcode” filtering (#880)
    Allows for opening from:
      * project dir
      * `ios` dir

--- src/io/flutter/actions/FlutterExternalIdeActionGroup.java
+++ src/io/flutter/actions/FlutterExternalIdeActionGroup.java
@@ -9,15 +9,18 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.vfs.VirtualFile;
 import io.flutter.FlutterUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 
 public class FlutterExternalIdeActionGroup extends DefaultActionGroup {
   @Override
   public void update(AnActionEvent e) {
-    final boolean enabled =  SystemInfo.isMac && isXcodeFile(e);
+    final boolean enabled = SystemInfo.isMac && isXcodeFile(e);
     final Presentation presentation = e.getPresentation();
     presentation.setEnabled(enabled);
     presentation.setVisible(enabled);
@@ -25,6 +28,17 @@ public class FlutterExternalIdeActionGroup extends DefaultActionGroup {
 
   private boolean isXcodeFile(AnActionEvent e) {
     final VirtualFile file = CommonDataKeys.VIRTUAL_FILE.getData(e.getDataContext());
-    return file != null && file.exists() && FlutterUtils.isXcodeProjectFileName(file.getName());
+    if (file == null || !file.exists()) return false;
+    final Project project = e.getProject();
+    return
+      isProjectDirectory(file, project) || isIOsDirectory(file) || FlutterUtils.isXcodeProjectFileName(file.getName());
+  }
+
+  private boolean isIOsDirectory(@NotNull VirtualFile file) {
+    return file.isDirectory() && file.getName().equals("ios");
+  }
+
+  private boolean isProjectDirectory(@NotNull VirtualFile file, @Nullable Project project) {
+    return file.isDirectory() && project != null && project.getBaseDir().getPath().equals(file.getPath());
   }
 }
