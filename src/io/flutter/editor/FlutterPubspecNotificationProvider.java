6f58b7058ecaccf8bdcf62535e29fc6c3e30155e
Devon Carew <devoncarew@google.com>
2017-06-07 14:40:09 -0700

    support nested packages (#1094)
    * support nested packages
    
    * review comments

--- src/io/flutter/editor/FlutterPubspecNotificationProvider.java
+++ src/io/flutter/editor/FlutterPubspecNotificationProvider.java
@@ -76,10 +76,12 @@ public class FlutterPubspecNotificationProvider extends EditorNotifications.Prov
       icon(FlutterIcons.Flutter);
       text("Flutter commands");
 
-      HyperlinkLabel label = createActionLabel("Packages get", "flutter.packages.get");
+      // "flutter.packages.get"
+      HyperlinkLabel label = createActionLabel("Packages get", () -> runPackagesGet(false));
       label.setToolTipText("Install referenced packages");
 
-      label = createActionLabel("Packages upgrade", "flutter.packages.upgrade");
+      // "flutter.packages.upgrade"
+      label = createActionLabel("Packages upgrade", () -> runPackagesGet(true));
       label.setToolTipText("Upgrade referenced packages to the latest versions");
 
       myLinksPanel.add(new JSeparator(SwingConstants.VERTICAL));
@@ -89,6 +91,24 @@ public class FlutterPubspecNotificationProvider extends EditorNotifications.Prov
       myLinksPanel.add(new JSeparator(SwingConstants.VERTICAL));
       label = createActionLabel("Flutter doctor", "flutter.doctor");
       label.setToolTipText("Validate installed tools and their versions");
+    }
+
+    private void runPackagesGet(boolean upgrade) {
+      // We can assert below since we know we're on a pubspec.yaml file in a project, with a valid Flutter SDK.
+      final Project project = ProjectLocator.getInstance().guessProjectForFile(myFile);
+      assert (project != null);
+      final FlutterSdk sdk = FlutterSdk.getFlutterSdk(project);
+      assert (sdk != null);
+
+      final PubRoot root = PubRoot.forDirectory(myFile.getParent());
+      if (root != null) {
+        if (!upgrade) {
+          sdk.startPackagesGet(root, project);
+        }
+        else {
+          sdk.startPackagesUpgrade(root, project);
+        }
+      }
     }
   }
 }
