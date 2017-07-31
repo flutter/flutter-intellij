0b72932f5ba3ca8faeede3c871d26f0f0f097577
Phil Quitslund <pq@users.noreply.github.com>
2017-07-31 14:53:58 -0700

    Migrate to a LinkLabel (#1255)

--- src/io/flutter/module/settings/SettingsHelpForm.java
+++ src/io/flutter/module/settings/SettingsHelpForm.java
@@ -5,16 +5,13 @@
  */
 package io.flutter.module.settings;
 
+import com.intellij.ide.browsers.BrowserLauncher;
+import com.intellij.ui.components.labels.LinkLabel;
 import io.flutter.FlutterBundle;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 /**
  * The settings panel that list helps.
@@ -35,7 +32,7 @@ public class SettingsHelpForm {
   private JLabel orgLabel;
   private JLabel orgDescription;
 
-  private JTextPane gettingStartedUrl;
+  private LinkLabel gettingStartedUrl;
 
   public SettingsHelpForm() {
     projectNameLabel.setText(FlutterBundle.message("flutter.module.create.settings.help.label"));
@@ -50,26 +47,14 @@ public class SettingsHelpForm {
     orgLabel.setText(FlutterBundle.message("flutter.module.create.settings.help.org.label"));
     orgDescription.setText(FlutterBundle.message("flutter.module.create.settings.help.org.description"));
 
-    gettingStartedUrl.setText(FlutterBundle.message("flutter.module.create.settings.help.getting_started_html"));
+    gettingStartedUrl.setText(FlutterBundle.message("flutter.module.create.settings.help.getting_started_link_text"));
     gettingStartedUrl.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-    gettingStartedUrl.addMouseListener(new MouseAdapter() {
-      public void mouseClicked(MouseEvent e) {
-        if (e.getClickCount() > 0) {
-          if (Desktop.isDesktopSupported()) {
-            Desktop desktop = Desktop.getDesktop();
-            try {
-              URI uri = new URI("https://flutter.io/getting-started/");
-              desktop.browse(uri);
-            } catch (IOException | URISyntaxException ex) {
-              // do nothing
-            }
-          } else {
-            //do nothing
-          }
-        }
-      }
-    });
+    gettingStartedUrl.setIcon(null);
+    //noinspection unchecked
+    gettingStartedUrl
+      .setListener((label, linkUrl) -> BrowserLauncher.getInstance().browse("https://flutter.io/getting-started/", null), null);
   }
+
 
   @NotNull
   public JComponent getComponent() {
