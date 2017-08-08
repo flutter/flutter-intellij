cb38b9d3bc851ec488a28fa6edb6a67d05b9215a
pq <pquitslund@google.com>
2017-08-08 15:18:34 -0700

    Initial “toggle inspect mode action” (#1104).

--- src/io/flutter/view/FlutterView.java
+++ src/io/flutter/view/FlutterView.java
@@ -93,6 +93,7 @@ public class FlutterView implements PersistentStateComponent<FlutterView.State>,
     toolWindowEx.setTitleActions(
       new DebugDrawAction(this),
       new PerformanceOverlayAction(this),
+      new ToggleInspectModeAction(this),
       new TogglePlatformAction(this)
     );
 
@@ -234,6 +235,16 @@ class TimeDilationAction extends AbstractToggleableAction {
     final Map<String, Object> params = new HashMap<>();
     params.put("timeDilation", isSelected(event) ? 5.0 : 1.0);
     view.getFlutterApp().callServiceExtension("ext.flutter.timeDilation", params);
+  }
+}
+
+class ToggleInspectModeAction extends AbstractToggleableAction {
+  ToggleInspectModeAction(@NotNull FlutterView view) {
+    super(view, "Toggle Inspect Mode", "Toggle Inspect Mode", AllIcons.Actions.Menu_find);
+  }
+
+  protected void perform(AnActionEvent event) {
+    view.getFlutterApp().callBooleanExtension("ext.flutter.widgetInspector", isSelected(event));
   }
 }
 
