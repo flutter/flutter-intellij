716ae69ef9f30cc2f1a0c19babf44bcaeb140020
Steve Messick <messick@google.com>
2017-08-28 14:26:50 -0700

    Remove dead code

--- src/io/flutter/module/settings/ProjectType.java
+++ src/io/flutter/module/settings/ProjectType.java
@@ -19,12 +19,6 @@ public class ProjectType {
 
   private void createUIComponents() {
     projectTypeCombo = new ComboBox<>();
-    //noinspection unchecked
-    //projectTypeCombo.setModel(new DefaultComboBoxModel(new String[]{
-    //  FlutterBundle.message("flutter.module.create.settings.type.application"),
-    //  FlutterBundle.message("flutter.module.create.settings.type.plugin"),
-    //  FlutterBundle.message("flutter.module.create.settings.type.package"),
-    //}));
     projectTypeCombo.setModel(new EnumComboBoxModel<>(FlutterProjectType.class));
     projectTypeCombo.setToolTipText(FlutterBundle.message("flutter.module.create.settings.type.tip"));
   }
