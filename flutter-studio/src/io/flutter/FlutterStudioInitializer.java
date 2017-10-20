2666223d2ed8dd90a96859f728a9d1f3fdae963e
stevemessick <stevemessick@users.noreply.github.com>
2017-10-20 14:52:57 -0700

    Check that Android Studio is up-to-date (#1410)
    * Add a check for old versions
    
    * typo

--- flutter-studio/src/io/flutter/FlutterStudioInitializer.java
+++ flutter-studio/src/io/flutter/FlutterStudioInitializer.java
@@ -5,11 +5,25 @@
  */
 package io.flutter;
 
+import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.ui.Messages;
+
 public class FlutterStudioInitializer implements Runnable {
+
+  private static void reportVersionIncompatibility(ApplicationInfo info) {
+    Messages.showErrorDialog("The Flutter plugin requires a more recent version of Android Studio.", "Version Mismatch");
+  }
+
   @Override
   public void run() {
-    // TODO(messick): Remove this class if it is never needed.
     // Unlike StartupActivity, this runs before the welcome screen (FlatWelcomeFrame) is displayed.
     // StartupActivity runs just before a project is opened.
+    ApplicationInfo info = ApplicationInfo.getInstance();
+    if ("Google".equals(info.getCompanyName())) {
+      String version = info.getFullVersion();
+      if (version.startsWith("2.") || version.contains("Canary") || (version.contains("Beta") && !version.endsWith("7"))) {
+        reportVersionIncompatibility(info);
+      }
+    }
   }
 }
