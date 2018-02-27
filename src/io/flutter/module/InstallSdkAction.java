08d2e241be7f717ea90a0764dacd5be5656b9ea7
Devon Carew <devoncarew@google.com>
2018-02-27 07:11:40 -0800

    install the flutter sdk using the beta channel by default (#1841)

--- src/io/flutter/module/InstallSdkAction.java
+++ src/io/flutter/module/InstallSdkAction.java
@@ -329,7 +329,7 @@ public class InstallSdkAction extends DumbAwareAction {
       @Override
       GeneralCommandLine getCommandLine() {
         return gitCommandBase().withWorkDirectory(myInstallPath)
-          .withParameters("clone", "-b", "dev", "https://github.com/flutter/flutter.git");
+          .withParameters("clone", "-b", "beta", "https://github.com/flutter/flutter.git");
       }
 
       @NotNull
