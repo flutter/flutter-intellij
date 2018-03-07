b774c391012dcdd8d37411a79be1cecf0f33f09f
Steve Messick <messick@google.com>
2018-03-07 14:54:55 -0800

    Remove hard dependency on deprecated method

--- flutter-studio/src/io/flutter/actions/FlutterNewProjectAction.java
+++ flutter-studio/src/io/flutter/actions/FlutterNewProjectAction.java
@@ -11,15 +11,22 @@ import com.android.tools.idea.wizard.model.ModelWizard;
 import com.android.tools.idea.wizard.model.ModelWizardDialog;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbAware;
 import io.flutter.module.FlutterProjectType;
 import io.flutter.project.ChoseProjectTypeStep;
 import io.flutter.project.FlutterProjectModel;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.NoSuchElementException;
 
+import static com.intellij.util.ReflectionUtil.getMethod;
+
 public class FlutterNewProjectAction extends AnAction implements DumbAware {
+  private static final Logger LOG = Logger.getInstance("#io.flutter.actions.FlutterNewProjectAction");
+
   public FlutterNewProjectAction() {
     this("New Flutter Project...");
   }
@@ -39,8 +46,18 @@ public class FlutterNewProjectAction extends AnAction implements DumbAware {
     ModelWizard wizard = new ModelWizard.Builder()
       .addStep(new ChoseProjectTypeStep(model))
       .build();
-    ModelWizardDialog dialog =
-      new StudioWizardDialogBuilder(wizard, "Create New Flutter Project").setUseNewUx(true).build();
+    StudioWizardDialogBuilder builder = new StudioWizardDialogBuilder(wizard, "Create New Flutter Project");
+    // TODO(messick) Remove this reflection call after 3.2 is stable.
+    Method method = getMethod(builder.getClass(), "setUseNewUx", Boolean.class);
+    if (method != null) {
+      try {
+        method.invoke(builder, Boolean.TRUE);
+      }
+      catch (IllegalAccessException | InvocationTargetException e1) {
+        LOG.error(e1);
+      }
+    } // End of code to remove
+    ModelWizardDialog dialog = builder.build();
     try {
       dialog.show();
     }
