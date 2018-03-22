d2b62911f1d4430bacde755fb309d8e5e0d389f0
Devon Carew <devoncarew@google.com>
2018-03-22 08:57:19 -0700

    handle user printed json output in stdout

--- testSrc/unit/io/flutter/run/daemon/DaemonApiTest.java
+++ testSrc/unit/io/flutter/run/daemon/DaemonApiTest.java
@@ -7,6 +7,7 @@ package io.flutter.run.daemon;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -74,7 +75,7 @@ public class DaemonApiTest {
     checkLog("{\"method\":\"device.enable\",\"id\":0}");
     assertFalse(result.isDone());
 
-    api.dispatch("{id: \"0\"}", null);
+    api.dispatch(new JsonParser().parse("{id: \"0\"}").getAsJsonObject(), null);
     assertTrue(result.isDone());
     assertNull(result.get());
   }
@@ -87,7 +88,7 @@ public class DaemonApiTest {
   }
 
   private void replyWithResult(Future result, String resultJson) {
-    api.dispatch("{id: \"0\", result: " + resultJson + "}", null);
+    api.dispatch(new JsonParser().parse("{id: \"0\", result: " + resultJson + "}").getAsJsonObject(), null);
     assertTrue(result.isDone());
   }
 
