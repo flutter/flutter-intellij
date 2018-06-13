713b3279ca84e5d90c9d6c9643845221b0b0ab3a
quangson91 <quangson91@gmail.com>
2018-06-13 23:39:45 +0700

    + add simple tool window with toolbar & console view + using kotlin expression to reduce line of code

--- src/io/flutter/logging/v2/FlutterLogView.kt
+++ src/io/flutter/logging/v2/FlutterLogView.kt
@@ -14,82 +14,44 @@ import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.ui.ConsoleView
 import com.intellij.execution.ui.ConsoleViewContentType
 import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.ui.SimpleToolWindowPanel
+import io.flutter.logging.FlutterLogFilterPanel
 import javax.swing.JComponent
 
 class FlutterLogView(
     environment: ExecutionEnvironment
 ) : ConsoleView {
   private val myConsoleView: ConsoleView = TextConsoleBuilderFactory.getInstance().createBuilder(environment.project).console
+  private val filterPanel = FlutterLogFilterPanel { }
+  private val simpleToolWindowPanel: SimpleToolWindowPanel = SimpleToolWindowPanel(true, true).apply {
+    setToolbar(filterPanel.root)
+    setContent(myConsoleView.component)
+  }
 
   override fun hasDeferredOutput(): Boolean {
     return myConsoleView.hasDeferredOutput()
   }
 
-  override fun clear() {
-    myConsoleView.clear()
-  }
+  override fun clear() = myConsoleView.clear()
 
-  override fun setHelpId(helpId: String) {
-    myConsoleView.setHelpId(helpId)
-  }
-
-  override fun print(text: String, contentType: ConsoleViewContentType) {
-    myConsoleView.print(text, contentType)
-  }
-
-  override fun getContentSize(): Int {
-    return myConsoleView.contentSize
-  }
+  override fun setHelpId(helpId: String) = myConsoleView.setHelpId(helpId)
+  override fun print(text: String, contentType: ConsoleViewContentType) = myConsoleView.print(text, contentType)
+  override fun getContentSize(): Int = myConsoleView.contentSize
 
   override fun setOutputPaused(value: Boolean) {
     myConsoleView.isOutputPaused = value
   }
 
-  override fun createConsoleActions(): Array<AnAction> {
-    return myConsoleView.createConsoleActions()
-  }
-
-  override fun getComponent(): JComponent {
-    return myConsoleView.component
-  }
-
-  override fun performWhenNoDeferredOutput(runnable: Runnable) {
-    myConsoleView.performWhenNoDeferredOutput(runnable)
-  }
-
-  override fun attachToProcess(processHandler: ProcessHandler?) {
-    myConsoleView.attachToProcess(processHandler)
-  }
-
-  override fun getPreferredFocusableComponent(): JComponent {
-    return myConsoleView.preferredFocusableComponent
-  }
-
-  override fun isOutputPaused(): Boolean {
-    return myConsoleView.isOutputPaused
-  }
-
-  override fun addMessageFilter(filter: Filter) {
-    myConsoleView.addMessageFilter(filter)
-  }
-
-  override fun printHyperlink(hyperlinkText: String, info: HyperlinkInfo?) {
-    myConsoleView.printHyperlink(hyperlinkText, info)
-  }
-
-  override fun canPause(): Boolean {
-    return myConsoleView.canPause()
-  }
-
-  override fun allowHeavyFilters() {
-    myConsoleView.allowHeavyFilters()
-  }
-
-  override fun dispose() {
-    myConsoleView.dispose()
-  }
-
-  override fun scrollTo(offset: Int) {
-    myConsoleView.scrollTo(offset)
-  }
+  override fun createConsoleActions(): Array<AnAction> = myConsoleView.createConsoleActions()
+  override fun getComponent(): JComponent = simpleToolWindowPanel
+  override fun performWhenNoDeferredOutput(runnable: Runnable) = myConsoleView.performWhenNoDeferredOutput(runnable)
+  override fun attachToProcess(processHandler: ProcessHandler?) = myConsoleView.attachToProcess(processHandler)
+  override fun getPreferredFocusableComponent(): JComponent = myConsoleView.preferredFocusableComponent
+  override fun isOutputPaused(): Boolean = myConsoleView.isOutputPaused
+  override fun addMessageFilter(filter: Filter) = myConsoleView.addMessageFilter(filter)
+  override fun printHyperlink(hyperlinkText: String, info: HyperlinkInfo?) = myConsoleView.printHyperlink(hyperlinkText, info)
+  override fun canPause(): Boolean = myConsoleView.canPause()
+  override fun allowHeavyFilters() = myConsoleView.allowHeavyFilters()
+  override fun dispose() = myConsoleView.dispose()
+  override fun scrollTo(offset: Int) = myConsoleView.scrollTo(offset)
 }
