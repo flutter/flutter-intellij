224bf754b04e814903ed3ca7465331eeb8a33a13
Muzaffar Auhammud <muzaffar@hackers.mu>
2018-06-17 17:50:47 +0400

    Fix build error: Improve search for emulator tool

--- src/io/flutter/android/AndroidSdk.java
+++ src/io/flutter/android/AndroidSdk.java
@@ -23,6 +23,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;
 
 /**
  * A wrapper around an Android SDK on disk.
