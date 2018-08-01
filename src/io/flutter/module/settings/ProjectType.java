65f17ef37eefb8174b3ac0790c43c59f1a732b22
pq <pquitslund@gmail.com>
2018-08-01 15:29:32 -0700

    make project type experimental.modules aware

--- src/io/flutter/module/settings/ProjectType.java
+++ src/io/flutter/module/settings/ProjectType.java
@@ -6,22 +6,64 @@
 package io.flutter.module.settings;
 
 import com.intellij.openapi.ui.ComboBox;
-import com.intellij.ui.EnumComboBoxModel;
 import io.flutter.FlutterBundle;
 import io.flutter.module.FlutterProjectType;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
 
 public class ProjectType {
+  // TODO(pq): replace w/ simple EnumComboBoxModel when add-to-app is complete and we no longer need to filter out MODULE types.
+  private static final class ProjectTypeComboBoxModel extends AbstractListModel<FlutterProjectType>
+    implements ComboBoxModel<FlutterProjectType> {
+    private final List<FlutterProjectType> myList = new ArrayList<>(EnumSet.allOf(FlutterProjectType.class));
+    private FlutterProjectType mySelected;
+
+    public ProjectTypeComboBoxModel() {
+      // Remove MODULE type until add-to-app is complete.
+      if (System.getProperty("flutter.experimental.modules", null) == null) {
+        myList.remove(FlutterProjectType.MODULE);
+      }
+      mySelected = myList.get(0);
+    }
+
+    @Override
+    public int getSize() {
+      return myList.size();
+    }
+
+    @Override
+    public FlutterProjectType getElementAt(int index) {
+      return myList.get(index);
+    }
+
+    @Override
+    public void setSelectedItem(Object item) {
+      setSelectedItem((FlutterProjectType)item);
+    }
+
+    @Override
+    public FlutterProjectType getSelectedItem() {
+      return mySelected;
+    }
+
+    public void setSelectedItem(FlutterProjectType item) {
+      mySelected = item;
+      fireContentsChanged(this, 0, getSize());
+    }
+  }
+
   private JPanel projectTypePanel;
   private ComboBox projectTypeCombo;
 
   private void createUIComponents() {
     projectTypeCombo = new ComboBox<>();
     //noinspection unchecked
-    projectTypeCombo.setModel(new EnumComboBoxModel<>(FlutterProjectType.class));
+    projectTypeCombo.setModel(new ProjectTypeComboBoxModel());
     projectTypeCombo.setToolTipText(FlutterBundle.message("flutter.module.create.settings.type.tip"));
   }
 
@@ -31,7 +73,7 @@ public class ProjectType {
   }
 
   public FlutterProjectType getType() {
-    return (FlutterProjectType) projectTypeCombo.getSelectedItem();
+    return (FlutterProjectType)projectTypeCombo.getSelectedItem();
   }
 
   public void addListener(ItemListener listener) {
