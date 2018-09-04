86e062b713b4c129fcbe9d118a25e40cb3ce8fe8
Konstantin Shcheglov <scheglov@google.com>
2018-09-03 17:58:28 -0700

    Add 'Wrap with Container' action.

--- gen/icons/FlutterIcons.java
+++ gen/icons/FlutterIcons.java
@@ -71,6 +71,7 @@ public class FlutterIcons {
   public static final Icon RemoveWidget = load("/icons/preview/remove_widget.png");
   public static final Icon Row = load("/icons/preview/row.png");
   public static final Icon Center = load("/icons/preview/center.png");
+  public static final Icon Container = load("/icons/preview/container.png");
   public static final Icon Up = load("/icons/preview/up.png");
   public static final Icon Down = load("/icons/preview/down.png");
   public static final Icon ExtractMethod = load("/icons/preview/extract_method.png");
