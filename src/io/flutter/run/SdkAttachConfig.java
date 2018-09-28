3eb2d562ff86483b73bead1df6963d418af0cd1c
stevemessick <stevemessick@users.noreply.github.com>
2018-09-28 14:25:41 -0700

    Move attach-debug classes to main module (#2635)

--- src/io/flutter/run/SdkAttachConfig.java
+++ src/io/flutter/run/SdkAttachConfig.java
@@ -1,9 +1,19 @@
 /*
- * Copyright 2018 The Chromium Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package io.flutter.run.attach;
+package io.flutter.run;
 
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.Executor;
@@ -27,7 +37,6 @@ import io.flutter.FlutterBundle;
 import io.flutter.console.FlutterConsoleFilter;
 import io.flutter.dart.DartPlugin;
 import io.flutter.pub.PubRoot;
-import io.flutter.run.*;
 import io.flutter.run.daemon.FlutterApp;
 import io.flutter.run.daemon.FlutterDevice;
 import io.flutter.run.daemon.RunMode;
