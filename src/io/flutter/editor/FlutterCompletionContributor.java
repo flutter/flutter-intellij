3145700b8608e6693666b683a7dc0c95e2c74f46
Devon Carew <devoncarew@google.com>
2018-10-26 15:16:52 -0700

    remove some reflection specific to 2018.1 and older versions

--- src/io/flutter/editor/FlutterCompletionContributor.java
+++ src/io/flutter/editor/FlutterCompletionContributor.java
@@ -7,7 +7,6 @@ package io.flutter.editor;
 
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.openapi.project.Project;
-import com.intellij.util.ReflectionUtil;
 import com.intellij.util.ui.ColorIcon;
 import com.intellij.util.ui.EmptyIcon;
 import com.intellij.util.ui.JBUI;
@@ -20,8 +19,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Objects;
 
 
@@ -35,21 +32,12 @@ public class FlutterCompletionContributor extends DartCompletionExtension {
   public LookupElementBuilder createLookupElement(@NotNull final Project project, @NotNull final CompletionSuggestion suggestion) {
     final Icon icon = findIcon(suggestion);
     if (icon != null) {
-      final LookupElementBuilder lookup = DartServerCompletionContributor.createLookupElement(project, suggestion).withTypeText("", icon, false);
-
-      // 2018.1 introduces a new API to specify right alignment for type icons (the default previously).
-      // TODO(pq): remove reflective access when 2018.1 is our minimum.
-      final Method rightAligned = ReflectionUtil.getMethod(lookup.getClass(), "withTypeIconRightAligned");
-      if (rightAligned != null) {
-        try {
-          return (LookupElementBuilder)rightAligned.invoke(lookup, true);
-        }
-        catch (IllegalAccessException | InvocationTargetException e) {
-          // Shouldn't happen but if it does fall back on default.
-        }
-      }
-      return lookup;
+      final LookupElementBuilder lookup =
+        DartServerCompletionContributor.createLookupElement(project, suggestion).withTypeText("", icon, false);
+      // Specify right alignment for type icons.
+      return lookup.withTypeIconRightAligned(true);
     }
+
     return null;
   }
 
@@ -70,7 +58,7 @@ public class FlutterCompletionContributor extends DartCompletionExtension {
           else if (Objects.equals(declaringType, "Icons")) {
             final Icon icon = FlutterMaterialIcons.getMaterialIconForName(name);
             // If we have no icon, show an empty node (which is preferable to the default "IconData" text).
-            return  icon != null ? icon : EMPTY_ICON;
+            return icon != null ? icon : EMPTY_ICON;
           }
         }
       }
