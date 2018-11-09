c486d436b625990f15706296fa3ec316b2ef3ed9
Steve Messick <messick@google.com>
2018-11-09 15:24:12 -0800

    Enhance error reporter to recognize Dart stack traces

--- src/io/flutter/FlutterErrorReportSubmitter.java
+++ src/io/flutter/FlutterErrorReportSubmitter.java
@@ -4,15 +4,21 @@
  * found in the LICENSE file.
  */
 
+// Not sure how others debug this but here's one technique.
+// Edit com.intellij.ide.plugins.PluginManagerCore.
+// Add this line at the top of getPluginByClassName():
+// if (true) return getPlugins()[getPlugins().length-1].getPluginId(); // DEBUG do not merge
 package io.flutter;
 
+import static com.intellij.openapi.actionSystem.CommonDataKeys.PROJECT;
+
+import com.intellij.diagnostic.IdeaReportingEvent;
 import com.intellij.ide.DataManager;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManager;
 import com.intellij.ide.scratch.ScratchRootType;
 import com.intellij.lang.Language;
 import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.diagnostic.ErrorReportSubmitter;
 import com.intellij.openapi.diagnostic.IdeaLoggingEvent;
@@ -24,19 +30,21 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.Consumer;
+import io.flutter.run.daemon.DaemonApi;
 import io.flutter.sdk.FlutterSdk;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.awt.*;
+import java.awt.Component;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class FlutterErrorReportSubmitter extends ErrorReportSubmitter {
   private static final Logger LOG = Logger.getInstance(FlutterErrorReportSubmitter.class);
+  private static final String[] KNOWN_ERRORS = new String[]{"Bad state: No element"};
+  private static final String COMPLETION_EXCEPTION_PREFIX = "java.util.concurrent.CompletionException: java.io.IOException: ";
 
   @NotNull
   @Override
@@ -51,22 +59,55 @@ public class FlutterErrorReportSubmitter extends ErrorReportSubmitter {
                           @NotNull Component parentComponent,
                           @NotNull Consumer<SubmittedReportInfo> consumer) {
     if (events.length == 0) {
-      consumer.consume(new SubmittedReportInfo(
-        null,
-        null,
-        SubmittedReportInfo.SubmissionStatus.FAILED));
+      fail(consumer);
       return;
     }
 
+    String stackTrace = null, errorMessage = null;
+    String destUrl = "https://github.com/flutter/flutter-intellij/issues/new";
+    for (IdeaLoggingEvent event : events) {
+      String stackTraceText = event.getThrowableText();
+      if (stackTraceText.startsWith(COMPLETION_EXCEPTION_PREFIX)) {
+        stackTraceText = stackTraceText.substring(COMPLETION_EXCEPTION_PREFIX.length());
+        if (stackTraceText.startsWith(DaemonApi.FLUTTER_ERROR_PREFIX)) {
+          String message = stackTraceText.substring(DaemonApi.FLUTTER_ERROR_PREFIX.length());
+          int start = message.indexOf(": ") + 2;
+          if (start == 0) continue;
+          int end = message.indexOf('\n');
+          if (end < 0) end = message.length();
+          String error = message.substring(start, end);
+          stackTrace = message.substring(end + 1);
+          for (String err : KNOWN_ERRORS) {
+            if (error.contains(err)) {
+              if (end != message.length()) {
+                // Dart stack trace included so extract it and set the issue target to the Flutter repo.
+                errorMessage = err;
+                int endOfDartStack = stackTrace.indexOf("\\n\"\n");
+                if (endOfDartStack > 0) {
+                  // Get only the part between quotes. If the format is wrong just use the whole thing.
+                  stackTrace = stackTrace.substring(1, endOfDartStack);
+                  // Only send it to Flutter if it is a known issue and in the expected format.
+                  destUrl = "https://github.com/flutter/flutter/issues/new";
+                }
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+
     final DataContext dataContext = DataManager.getInstance().getDataContext(parentComponent);
-    Project project = PlatformDataKeys.PROJECT.getData(dataContext);
+    Project project = PROJECT.getData(dataContext);
     if (project == null) {
       project = ProjectManager.getInstance().getDefaultProject();
     }
 
     final StringBuilder builder = new StringBuilder();
 
-    builder.append("Please file this bug report at https://github.com/flutter/flutter-intellij/issues/new.\n");
+    builder.append("Please file this bug report at ");
+    builder.append(destUrl);
+    builder.append(".\n");
     builder.append("\n");
     builder.append("---\n");
     builder.append("\n");
@@ -95,7 +136,6 @@ public class FlutterErrorReportSubmitter extends ErrorReportSubmitter {
 
     final IdeaPluginDescriptor dartPlugin = PluginManager.getPlugin(PluginId.getId("Dart"));
     if (dartPlugin != null) {
-      //noinspection ConstantConditions
       builder.append(" • Dart plugin `").append(dartPlugin.getVersion()).append("`");
     }
     builder.append("\n\n");
@@ -115,20 +155,33 @@ public class FlutterErrorReportSubmitter extends ErrorReportSubmitter {
     }
     builder.append("\n");
 
-    for (IdeaLoggingEvent event : events) {
-      builder.append("## Exception\n");
-      builder.append("\n");
-      builder.append(event.getMessage()).append("\n");
-      builder.append("\n");
-
-      if (event.getThrowable() != null) {
-        builder.append("```\n");
-        builder.append(event.getThrowableText().trim()).append("\n");
-        builder.append("```\n");
+    if (stackTrace == null) {
+      for (IdeaLoggingEvent event : events) {
+        builder.append("## Exception\n");
+        builder.append("\n");
+        builder.append(event.getMessage()).append("\n");
         builder.append("\n");
 
-        FlutterInitializer.getAnalytics().sendException(event.getThrowable(), false);
+        if (event.getThrowable() != null) {
+          builder.append("```\n");
+          builder.append(event.getThrowableText().trim()).append("\n");
+          builder.append("```\n");
+          builder.append("\n");
+
+          FlutterInitializer.getAnalytics().sendException(event.getThrowable(), false);
+        }
       }
+    }
+    else {
+
+      builder.append("## Exception\n");
+      builder.append("\n");
+      builder.append(errorMessage).append("\n");
+      builder.append("\n");
+      builder.append("```\n");
+      builder.append(stackTrace.replaceAll("\\\\n", "\n")).append("\n");
+      builder.append("```\n");
+      builder.append("\n");
     }
 
     final String text = builder.toString().trim() + "\n";
@@ -143,10 +196,7 @@ public class FlutterErrorReportSubmitter extends ErrorReportSubmitter {
       new OpenFileDescriptor(project, file).navigate(true);
     }
     else {
-      consumer.consume(new SubmittedReportInfo(
-        null,
-        null,
-        SubmittedReportInfo.SubmissionStatus.FAILED));
+      fail(consumer);
     }
 
     consumer.consume(new SubmittedReportInfo(
@@ -162,7 +212,7 @@ public class FlutterErrorReportSubmitter extends ErrorReportSubmitter {
     return new SubmittedReportInfo(null, "0", SubmittedReportInfo.SubmissionStatus.FAILED);
   }
 
-  private String getFlutterVersion(final FlutterSdk sdk) {
+  private static String getFlutterVersion(final FlutterSdk sdk) {
     try {
       final String flutterPath = sdk.getHomePath() + "/bin/flutter";
       final ProcessBuilder builder = new ProcessBuilder(flutterPath, "--version");
@@ -177,7 +227,15 @@ public class FlutterErrorReportSubmitter extends ErrorReportSubmitter {
     }
   }
 
+  private static void fail(@NotNull Consumer<SubmittedReportInfo> consumer) {
+    consumer.consume(new SubmittedReportInfo(
+      null,
+      null,
+      SubmittedReportInfo.SubmissionStatus.FAILED));
+  }
+
   private static byte[] readFully(InputStream in) throws IOException {
+    //noinspection resource
     final ByteArrayOutputStream out = new ByteArrayOutputStream();
     final byte[] temp = new byte[4096];
     int count = in.read(temp);
