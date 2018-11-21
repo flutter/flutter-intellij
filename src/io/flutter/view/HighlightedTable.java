d9b6491402b5946962d8ef77ca30b3b329701ba5
Terry Lucas <terry@google.com>
2018-11-21 08:41:54 -0800

    Expose HighlightedTable handles selection/hover feedback. (#2858)
    * Expose HighlightedTable handles selection/hover feedback.
    
    * Removed private method.
    
    * Added static to some color constants.

---
+++ src/io/flutter/view/HighlightedTable.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2018 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+package io.flutter.view;
+
+import com.intellij.ui.JBColor;
+import com.intellij.ui.table.JBTable;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+import javax.swing.event.MouseInputAdapter;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableModel;
+
+/**
+ * JBTable custom selection and hover renderer for a table view.
+ */
+public class HighlightedTable extends JBTable {
+  final static private JBColor HOVER_BACKGROUND_COLOR =
+    new JBColor(new Color(0xCFE6EF), JBColor.LIGHT_GRAY.brighter());
+  final static private JBColor HOVER_FOREGROUND_COLOR = JBColor.BLACK;
+
+  private int rollOverRowIndex = -1;
+  private int lastClickedRow = -1;
+
+  public HighlightedTable(TableModel model) {
+    super(model);
+    RollOverListener listener = new RollOverListener();
+    addMouseMotionListener(listener);
+    addMouseListener(listener);
+  }
+
+  @Override
+  public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+    Component c = super.prepareRenderer(renderer, row, column);
+    Font font = c.getFont();
+    if (font != null) {
+      // Iff we have a font for this component (TableRow).
+      if (lastClickedRow == row) {
+        c.setFont(font.deriveFont(Font.BOLD));
+        c.setForeground(getSelectionForeground());
+        c.setBackground(getSelectionBackground());
+      }
+      else {
+        c.setFont(font.deriveFont(Font.PLAIN));
+        if (row == rollOverRowIndex) {
+          c.setForeground(HOVER_FOREGROUND_COLOR);
+          c.setBackground(HOVER_BACKGROUND_COLOR);
+        }
+        else {
+          c.setForeground(getForeground());
+          c.setBackground(getBackground());
+        }
+      }
+    }
+
+    return c;
+  }
+
+  private class RollOverListener extends MouseInputAdapter {
+    @Override
+    public void mouseExited(MouseEvent e) {
+      rollOverRowIndex = -1;
+      repaint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+      int row = rowAtPoint(e.getPoint());
+      if( row != rollOverRowIndex ) {
+        rollOverRowIndex = row;
+        repaint();
+      }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+      lastClickedRow = rowAtPoint(e.getPoint());
+      repaint();
+    }
+  }
+}
