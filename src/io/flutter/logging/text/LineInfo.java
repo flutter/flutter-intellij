2c946f90474b2e1fc35cbc9eab40b3170c75c99c
Phil Quitslund <pq@users.noreply.github.com>
2019-01-10 14:20:31 -0800

    Log tree class refactor (#3068)
    * log tree class re-org
    
    * rename
    
    * copyrights

--- src/io/flutter/logging/text/LineInfo.java
+++ src/io/flutter/logging/text/LineInfo.java
@@ -3,7 +3,7 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-package io.flutter.logging.util;
+package io.flutter.logging.text;
 
 import com.intellij.execution.filters.Filter;
 import com.intellij.ui.SimpleTextAttributes;
