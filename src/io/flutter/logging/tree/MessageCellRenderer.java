317262325e1246b9b9476d8ee323bcb579c92098
Phil Quitslund <pq@users.noreply.github.com>
2019-01-28 13:13:23 -0800

    => ComboArrowRight

--- src/io/flutter/logging/tree/MessageCellRenderer.java
+++ src/io/flutter/logging/tree/MessageCellRenderer.java
@@ -46,7 +46,8 @@ public class MessageCellRenderer extends AbstractEntryCellRender {
 
     // Append data badge
     if (JsonUtils.hasJsonData(entry.getData())) {
-      setIcon(AllIcons.General.ArrowRight);
+      // TODO(pq): change to ArrowRight when we're no longer supporting 3.3.
+      setIcon(AllIcons.General.ComboArrowRight);
     }
   }
 }
