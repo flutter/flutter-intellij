850d4c655808bddb17c8b6bc62c2a2a8680ad915
pq <pquitslund@gmail.com>
2019-01-29 07:16:53 -0800

    renames

--- testSrc/unit/io/flutter/samples/FlutterSampleTest.java
+++ testSrc/unit/io/flutter/samples/FlutterSampleTest.java
@@ -44,7 +44,7 @@ public class FlutterSampleTest {
   static final String CARD_DESC = "This sample shows creation of a [Card] widget that shows album information\nand two actions.";
 
   @Test
-  public void testParseCardlDescription() {
+  public void testParseCardDescription() {
     assertEquals("<p>This sample shows creation of a <strong>Card</strong> widget that shows album information and two actions.</p>",
                  parse(CARD_DESC));
   }
@@ -53,7 +53,7 @@ public class FlutterSampleTest {
     "This example shows a [Scaffold] with an [AppBar], a [BottomAppBar] and a\n[FloatingActionButton]. The [body] is a [Text] placed in a [Center] in order\nto center the text within the [Scaffold] and the [FloatingActionButton] is\ncentered and docked within the [BottomAppBar] using\n[FloatingActionButtonLocation.centerDocked]. The [FloatingActionButton] is\nconnected to a callback that increments a counter.";
 
   @Test
-  public void testScaffoldlDescription() {
+  public void testScaffoldDescription() {
     assertEquals(
       "<p>This example shows a <strong>Scaffold</strong> with an <strong>AppBar</strong>, a <strong>BottomAppBar</strong> and a <strong>FloatingActionButton</strong>. The <strong>body</strong> is a <strong>Text</strong> placed in a <strong>Center</strong> in order to center the text within the <strong>Scaffold</strong> and the <strong>FloatingActionButton</strong> is centered and docked within the <strong>BottomAppBar</strong> using <strong>FloatingActionButtonLocation.centerDocked</strong>. The <strong>FloatingActionButton</strong> is connected to a callback that increments a counter.</p>",
       parse(SCAFFOLD_DESC));
