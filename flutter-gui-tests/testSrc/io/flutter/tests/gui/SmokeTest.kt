d1b1d6894aede7b9ec042e954ad2ced349f004eb
Steve Messick <messick@google.com>
2019-03-01 11:27:07 -0800

    Fix up after review

--- flutter-gui-tests/testSrc/io/flutter/tests/gui/SmokeTest.kt
+++ flutter-gui-tests/testSrc/io/flutter/tests/gui/SmokeTest.kt
@@ -23,40 +23,8 @@ class SmokeTest : GuiTestCase() {
         moveTo(1)
       }
       val editorCode = editor.getCurrentFileContents(false)
-      Assert.assertTrue(editorCode!!.unifyCode().startsWith(codeText.unifyCode()))
+      Assert.assertTrue(editorCode!!.isNotEmpty())
       closeProjectAndWaitWelcomeFrame()
     }
   }
-
-  // Copied from CommandLineProjectGuiTest, just for safety.
-  private fun String.unifyCode(): String =
-    MultiLineLabelUI.convertTabs(StringUtil.convertLineSeparators(this), 2)
-
-  private val codeText: String = """import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-"""
 }
