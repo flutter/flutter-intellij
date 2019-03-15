6045a1656e591474339ce670e976052b506546a9
Steve Messick <messick@google.com>
2019-03-15 16:23:03 -0700

    Unpack canned projects

--- flutter-gui-tests/testSrc/io/flutter/tests/gui/SmokeTest.kt
+++ flutter-gui-tests/testSrc/io/flutter/tests/gui/SmokeTest.kt
@@ -1,8 +1,6 @@
 package io.flutter.tests.gui
 
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.ui.MultiLineLabelUI
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.testGuiFramework.framework.RunWithIde
 import com.intellij.testGuiFramework.impl.GuiTestCase
 import com.intellij.testGuiFramework.launcher.ide.CommunityIde
@@ -15,8 +13,18 @@ class SmokeTest : GuiTestCase() {
   private val LOG = Logger.getInstance(this.javaClass)
 
   @Test
-  fun testStartup() {
+  fun createBasicProject() {
     ProjectCreator.createProject(projectName = "guitest")
+    checkProject()
+  }
+
+  @Test
+  fun importSimpleProject() {
+    ProjectCreator.importProject()
+    checkProject()
+  }
+
+  private fun checkProject() {
     ideFrame {
       editor {
         // Wait until current file has appeared in current editor and set focus to editor.
