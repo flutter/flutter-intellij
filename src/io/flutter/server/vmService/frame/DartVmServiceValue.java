3d1656209b9d9a4d0934fd06268421762dffa277
Jing Bian <jingbian@google.com>
2019-03-18 21:05:53 +0100

    Added TODO to get the actual DiagnosticsNode object.

--- src/io/flutter/server/vmService/frame/DartVmServiceValue.java
+++ src/io/flutter/server/vmService/frame/DartVmServiceValue.java
@@ -162,6 +162,7 @@ public class DartVmServiceValue extends XNamedValue {
       .evaluateInTargetContext(myIsolateId, myInstanceRef.getId(), "toStringDeep()", new VmServiceConsumers.EvaluateConsumerWrapper() {
         @Override
         public void received(final InstanceRef toStringInstanceRef) {
+          // TODO: Get actual DiagnosticsNode object instead of calling toStringDeep here.
           if (toStringInstanceRef.getKind() == InstanceKind.String) {
             String content = toStringInstanceRef.getValueAsString();
             int firstLineBreak = content.indexOf('\n');
