66bf8eae3f1c24e2591ec68b4cad7129cba87836
Devon Carew <devoncarew@google.com>
2019-05-28 08:50:41 -0700

    don't disable closing labels as part of UI Guides

--- src/io/flutter/settings/FlutterSettings.java
+++ src/io/flutter/settings/FlutterSettings.java
@@ -37,7 +37,6 @@ public class FlutterSettings {
   private static final String showBuildMethodGuidesKey = "io.flutter.editor.showBuildMethodGuides";
   private static final String showMultipleChildrenGuidesKey = "io.flutter.editor.showMultipleChildrenGuides";
   private static final String showBuildMethodsOnScrollbarKey = "io.flutter.editor.showBuildMethodsOnScrollbarKey";
-  private static final String disableDartClosingLabelsKey = "io.flutter.editor.disableDartClosingLabelsKey";
 
   public static FlutterSettings getInstance() {
     return ServiceManager.getService(FlutterSettings.class);
@@ -98,10 +97,6 @@ public class FlutterSettings {
     if (isShowBuildMethodsOnScrollbar()) {
       analytics.sendEvent("settings", afterLastPeriod(showBuildMethodsOnScrollbarKey));
     }
-    if (!isDisableDartClosingLabels()) {
-      // The default value is true so only send the event if the setting was turned off.
-      analytics.sendEvent("settings", afterLastPeriod(disableDartClosingLabelsKey + "_off"));
-    }
 
     if (useFlutterLogView()) {
       analytics.sendEvent("settings", afterLastPeriod(useFlutterLogView));
@@ -143,6 +138,7 @@ public class FlutterSettings {
 
   public void setDisableTrackWidgetCreation(boolean value) {
     getPropertiesComponent().setValue(disableTrackWidgetCreationKey, value, false);
+
     fireEvent();
   }
 
@@ -260,17 +256,6 @@ public class FlutterSettings {
 
     fireEvent();
   }
-
-  public boolean isDisableDartClosingLabels() {
-    return getPropertiesComponent().getBoolean(disableDartClosingLabelsKey, true);
-  }
-
-  public void setDisableDartClosingLabels(boolean value) {
-    getPropertiesComponent().setValue(disableDartClosingLabelsKey, value, true);
-
-    fireEvent();
-  }
-
 
   public boolean isShowMultipleChildrenGuides() {
     return getPropertiesComponent().getBoolean(showMultipleChildrenGuidesKey, false);
