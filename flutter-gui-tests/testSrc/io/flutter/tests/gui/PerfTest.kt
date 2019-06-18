40272c6c1047a170b4d971504e111aefe2d88a25
Steve Messick <messick@google.com>
2019-06-18 11:15:17 -0700

    Test perf view

--- flutter-gui-tests/testSrc/io/flutter/tests/gui/PerfTest.kt
+++ flutter-gui-tests/testSrc/io/flutter/tests/gui/PerfTest.kt
@@ -10,22 +10,54 @@ import com.intellij.testGuiFramework.framework.RunWithIde
 import com.intellij.testGuiFramework.impl.GuiTestCase
 import com.intellij.testGuiFramework.launcher.ide.CommunityIde
 import io.flutter.tests.gui.fixtures.flutterPerfFixture
+import org.fest.swing.timing.Condition
 import org.fest.swing.timing.Pause.pause
 import org.junit.Test
+import kotlin.test.expect
 
 @RunWithIde(CommunityIde::class)
 class PerfTest : GuiTestCase() {
 
   @Test
-  fun widgetTree() {
+  fun test() {
     ProjectCreator.importProject()
     ideFrame {
       launchFlutterApp()
-      val inspector = flutterPerfFixture(this)
-      inspector.populate()
-
+      val monitor = flutterPerfFixture(this)
+      monitor.populate()
       pause()
 
+      // Quickly check UI integrity.
+      val mem = monitor.memoryTabFixture()
+      expect(false, mem.heapDisplayLabels()::isEmpty)
+      expect(true, mem.heapDisplay()::isEnabled)
+      val perf = monitor.perfTabFixture()
+      expect(false, perf.controlCheckboxes()::isEmpty)
+      val performanceCheckbox = perf.performanceCheckbox()
+      expect(true, performanceCheckbox::isEnabled)
+      val repaintCheckbox = perf.repaintCheckbox()
+      expect(true, repaintCheckbox::isEnabled)
+      val frames = perf.frameRenderingPanel()
+      var currCount = frames.componentCount()
+      var prevCount = currCount
+      expect(true) { currCount >= 0 }
+
+      performanceCheckbox.click()
+      pause()
+      performanceCheckbox.click()
+      pause()
+      currCount = frames.componentCount()
+      expect(true) { currCount > prevCount }
+      prevCount = currCount
+      repaintCheckbox.click()
+      pause()
+      repaintCheckbox.click()
+      pause()
+      currCount = frames.componentCount()
+      expect(true) { currCount > prevCount }
+      prevCount = currCount
+
+      pause()
       runner().stop()
     }
   }
