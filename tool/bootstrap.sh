a0af9590f389061f881146b1bfef24aba28b972d
Devon Carew <devoncarew@google.com>
2019-08-13 12:33:39 -0700

    try using chocolatey

--- tool/bootstrap.sh
+++ tool/bootstrap.sh
@@ -9,37 +9,11 @@ set -e
 
 if [[ $TRAVIS_OS_NAME == "windows" ]]; then
 
-  export TOOLS_PATH=tools
-  mkdir ${TOOLS_PATH}
-
-  export JAVA_HOME=${JAVA_HOME:-$TOOLS_PATH/jdk}
-  mkdir ${JAVA_HOME}
-
-  export JAVA_VERSION=${JAVA_VERSION:-11.0.2}
-
-  pushd ${TOOLS_PATH}
-
-  if [ ! -f "${TOOLS_PATH}/jdk-${JAVA_VERSION}-windows-i586.zip" ]; then
-    echo "Downloading https://download.oracle.com/java/GA/jdk11/9/GPL/openjdk-${JAVA_VERSION}_windows-x64_bin.zip..."
-    curl -fsS -o openjdk-${JAVA_VERSION}_windows-x64_bin.zip https://download.oracle.com/java/GA/jdk11/9/GPL/openjdk-${JAVA_VERSION}_windows-x64_bin.zip
-    rm -rf ${JAVA_HOME}
-  fi
-  if [ ! -d "${JAVA_HOME}" ]; then
-    echo "Extracting jdk-${JAVA_VERSION}-windows-i586.zip..."
-    7z x openjdk-${JAVA_VERSION}_windows-x64_bin.zip -y -o${TOOLS_PATH}/
-    mv ${TOOLS_PATH}/jdk-${JAVA_VERSION} ${JAVA_HOME}
-  fi
-
-  popd
-
-  export PATH=${PATH}:${JAVA_HOME}
-
-  ls -l ${JAVA_HOME}
-  echo ${PATH}
-
+  choco install jdk10 -params "source=false"
   java -version
 
-  # TODO: install ant
+  choco install ant
+  echo $(ant -version)
 
 else
 
