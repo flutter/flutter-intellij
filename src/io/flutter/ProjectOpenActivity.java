973ce9131319bb9d350715f6c2e2bab8b8f8969f
Steve Messick <messick@google.com>
2019-09-17 07:35:57 -0700

    Fix merge error

--- src/io/flutter/ProjectOpenActivity.java
+++ src/io/flutter/ProjectOpenActivity.java
@@ -15,6 +15,7 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectType;
+import com.intellij.openapi.project.ProjectTypeService;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.ui.Messages;
 import icons.FlutterIcons;
