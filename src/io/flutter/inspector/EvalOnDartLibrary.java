2be1707507b0d70c7edc9b58bd62ec310436a464
Devon Carew <devoncarew@google.com>
2019-11-05 12:26:19 -0800

    fix an issue with using an alarm after its been disposed (#4053)
    * fix an issue with using an alarm after its been disposed
    
    * call response.complete(null)

--- src/io/flutter/inspector/EvalOnDartLibrary.java
+++ src/io/flutter/inspector/EvalOnDartLibrary.java
@@ -100,7 +100,14 @@ public class EvalOnDartLibrary implements Disposable {
         final CompletableFuture<?> previousDone = allPendingRequestsDone;
         allPendingRequestsDone = response;
         // Actually schedule this request only after the previous request completes.
-        previousDone.whenCompleteAsync((v, error) -> myRequestsScheduler.addRequest(wrappedRequest, 0));
+        previousDone.whenCompleteAsync((v, error) -> {
+          if (myRequestsScheduler.isDisposed()) {
+            response.complete(null);
+          }
+          else {
+            myRequestsScheduler.addRequest(wrappedRequest, 0);
+          }
+        });
       }
     }
     return response;
