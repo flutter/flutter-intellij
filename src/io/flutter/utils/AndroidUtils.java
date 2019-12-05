2a17c42c60d09b340e44f876e3673b93a3af84d8
Steve Messick <messick@google.com>
2019-12-05 11:34:58 -0800

    Change separator char

--- src/io/flutter/utils/AndroidUtils.java
+++ src/io/flutter/utils/AndroidUtils.java
@@ -603,7 +603,7 @@ public class AndroidUtils {
         content.append("project(':");
         content.append(flutterModuleName);
         content.append("').projectDir = new File('");
-        content.append(pathToModule);
+        content.append(pathToModule.replace('\\', '/'));
         content.append("')\n");
       }
       return content.toString();
