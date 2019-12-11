e1464a48d460df422c23937fb8161b671cac23a6
Steve Messick <messick@google.com>
2019-12-11 14:50:07 -0800

    Launch flutter attach when the Android app starts

--- flutter-studio/src/io/flutter/FlutterStudioStartupActivity.java
+++ flutter-studio/src/io/flutter/FlutterStudioStartupActivity.java
@@ -5,28 +5,45 @@
  */
 package io.flutter;
 
+import static com.android.tools.idea.gradle.project.importing.NewProjectSetup.ANDROID_PROJECT_TYPE;
 import static com.intellij.util.ReflectionUtil.findAssignableField;
+import static io.flutter.actions.AttachDebuggerAction.ATTACH_IS_ACTIVE;
+import static io.flutter.actions.AttachDebuggerAction.findRunConfig;
 
 import com.android.tools.idea.gradle.project.sync.GradleSyncListener;
 import com.android.tools.idea.gradle.project.sync.GradleSyncState;
 import com.intellij.ProjectTopics;
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.debugger.engine.DebugProcess;
+import com.intellij.debugger.engine.DebugProcessListener;
+import com.intellij.debugger.impl.DebuggerManagerListener;
+import com.intellij.debugger.impl.DebuggerSession;
+import com.intellij.execution.RunManagerEx;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.ModuleListener;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectTypeService;
 import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.util.ThreeState;
 import com.intellij.util.concurrency.AppExecutorUtil;
 import com.intellij.util.messages.MessageBusConnection;
 import com.intellij.util.messages.Topic;
-import io.flutter.actions.OpenAndroidModule;
+import io.flutter.actions.AttachDebuggerAction;
 import io.flutter.android.AndroidModuleLibraryManager;
 import io.flutter.project.FlutterProjectCreator;
+import io.flutter.pub.PubRoot;
+import io.flutter.run.FlutterReloadManager;
+import io.flutter.run.SdkRunConfig;
+import io.flutter.sdk.FlutterSdk;
 import io.flutter.settings.FlutterSettings;
 import io.flutter.utils.AndroidUtils;
 import io.flutter.utils.FlutterModuleUtils;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.List;
+import java.util.Objects;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -37,17 +54,17 @@ public class FlutterStudioStartupActivity implements StartupActivity {
   public void runActivity(@NotNull Project project) {
     MessageBusConnection connection = project.getMessageBus().connect(project);
     // GRADLE_SYNC_TOPIC is not public in Android Studio 3.5. It is in 3.6. It isn't defined in 3.4.
-    Topic topic = getStaticFieldValue(GradleSyncState.class, Topic.class, "GRADLE_SYNC_TOPIC");
-    assert topic != null;
     //noinspection unchecked
-    connection.subscribe((Topic<GradleSyncListener>)topic, makeSyncListener(project));
+    Topic<GradleSyncListener> topic = getStaticFieldValue(GradleSyncState.class, Topic.class, "GRADLE_SYNC_TOPIC");
+    assert topic != null;
+    connection.subscribe(topic, makeSyncListener(project));
 
     if (!FlutterModuleUtils.hasFlutterModule(project)) {
       connection.subscribe(ProjectTopics.MODULES, new ModuleListener() {
         @Override
         public void moduleAdded(@NotNull Project proj, @NotNull Module mod) {
           if (AndroidUtils.FLUTTER_MODULE_NAME.equals(mod.getName())) {
-            connection.disconnect();
+            //connection.disconnect(); TODO(messick) Test this deletion!
             AppExecutorUtil.getAppExecutorService().execute(() -> {
               AndroidUtils.enableCoeditIfAddToAppDetected(project);
             });
@@ -55,6 +72,12 @@ public class FlutterStudioStartupActivity implements StartupActivity {
         }
       });
       return;
+    }
+    else {
+      if (ANDROID_PROJECT_TYPE.equals(ProjectTypeService.getProjectType(project))) {
+        // This is an add-to-app project.
+        connection.subscribe(DebuggerManagerListener.TOPIC, makeAddToAppAttachListener(project));
+      }
     }
 
     // Unset this flag for all projects, mainly to ease the upgrade from 3.0.1 to 3.1.
@@ -106,6 +129,64 @@ public class FlutterStudioStartupActivity implements StartupActivity {
 
       @Override
       public void sourceGenerationFinished(@NotNull Project project) {
+      }
+    };
+  }
+
+  @NotNull
+  private static DebuggerManagerListener makeAddToAppAttachListener(@NotNull Project project) {
+
+    return new DebuggerManagerListener() {
+
+      DebugProcessListener dpl = new DebugProcessListener() {
+        @Override
+        public void processDetached(@NotNull DebugProcess process, boolean closedByUser) {
+          ThreeState state = project.getUserData(ATTACH_IS_ACTIVE);
+          if (state != null) {
+            project.putUserData(ATTACH_IS_ACTIVE, null);
+          }
+        }
+
+        @Override
+        public void processAttached(@NotNull DebugProcess process) {
+          if (project.getUserData(ATTACH_IS_ACTIVE) != null) {
+            return;
+          }
+          // Launch flutter attach if a run config can be found.
+          if (findRunConfig(project) == null) {
+            // Either there is no Flutter run config or there are more than one.
+            if (RunManagerEx.getInstanceEx(project).getSelectedConfiguration() instanceof SdkRunConfig) {
+              // The selected run config at this point is not Flutter, so we can't start the process automatically.
+              return;
+            }
+          }
+          FlutterSdk sdk = FlutterSdk.getFlutterSdk(project);
+          if (sdk == null) {
+            return;
+          }
+          // If needed, DataContext could be saved by FlutterReloadManager.beforeActionPerformed() in project user data.
+          DataContext context = DataContext.EMPTY_CONTEXT;
+          List<Module> modules = FlutterModuleUtils.findModulesWithFlutterContents(project);
+          assert modules.size() == 1; // TODO(messick) Need to change this if multiple :flutter sub-projects supported.
+          PubRoot pubRoot = PubRoot.forDirectory(Objects.requireNonNull(modules.get(0).getModuleFile()).getParent());
+          Application app = ApplicationManager.getApplication();
+          project.putUserData(ATTACH_IS_ACTIVE, ThreeState.fromBoolean(true));
+          // Note: Using block comments to preserve formatting.
+          app.invokeLater( /* After the Android launch completes, */
+            () -> app.executeOnPooledThread( /* but not on the EDT, */
+              () -> app.runReadAction( /* with read access, */
+                () -> new AttachDebuggerAction().startCommand(project, sdk, pubRoot, context)))); /* attach. */
+        }
+      };
+
+      @Override
+      public void sessionCreated(DebuggerSession session) {
+        session.getProcess().addDebugProcessListener(dpl);
+      }
+
+      @Override
+      public void sessionRemoved(DebuggerSession session) {
+        session.getProcess().removeDebugProcessListener(dpl);
       }
     };
   }
--- src/io/flutter/actions/AttachDebuggerAction.java
+++ src/io/flutter/actions/AttachDebuggerAction.java
@@ -6,11 +6,14 @@
 package io.flutter.actions;
 
 import com.google.common.base.Joiner;
+import com.intellij.execution.ExecutionListener;
+import com.intellij.execution.ExecutionManager;
 import com.intellij.execution.Executor;
 import com.intellij.execution.ProgramRunnerUtil;
 import com.intellij.execution.RunManagerEx;
 import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.runners.ExecutionEnvironmentBuilder;
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -20,7 +23,10 @@ import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.Key;
 import com.intellij.openapi.wm.ToolWindowId;
+import com.intellij.util.ThreeState;
+import com.intellij.util.messages.MessageBusConnection;
 import io.flutter.FlutterConstants;
 import io.flutter.FlutterInitializer;
 import io.flutter.pub.PubRoot;
@@ -32,6 +38,7 @@ import io.flutter.sdk.FlutterSdk;
 import io.flutter.sdk.FlutterSdkUtil;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
@@ -39,6 +46,15 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class AttachDebuggerAction extends FlutterSdkAction {
+
+  // Is 'flutter attach' running for a project?
+  // The three states are true, false, and non-existent (null).
+  //   true = run automatically via the debug listener in FlutterStudioStartupActivity
+  //   false = run from button press here
+  //   null = no attach process is running
+  // The button is still required because there may be multiple Flutter run configs.
+  public static final Key<ThreeState> ATTACH_IS_ACTIVE = new Key<>("attach-is-active");
+
   @Override
   public void startCommand(@NotNull Project project, @NotNull FlutterSdk sdk, @Nullable PubRoot root, @NotNull DataContext context) {
     // NOTE: When making changes here, consider making similar changes to RunFlutterAction.
@@ -91,6 +107,10 @@ public class AttachDebuggerAction extends FlutterSdkAction {
     ExecutionEnvironment env = builder.activeTarget().dataContext(context).build();
     FlutterLaunchMode.addToEnvironment(env, FlutterLaunchMode.DEBUG);
 
+    if (project.getUserData(ATTACH_IS_ACTIVE) == null) {
+      project.putUserData(ATTACH_IS_ACTIVE, ThreeState.fromBoolean(false));
+      onAttachTermination(project, (p) -> p.putUserData(ATTACH_IS_ACTIVE, null));
+    }
     ProgramRunnerUtil.executeConfiguration(env, false, true);
   }
 
@@ -121,12 +141,15 @@ public class AttachDebuggerAction extends FlutterSdkAction {
     else {
       enabled = true;
     }
+    if (enabled && (project.getUserData(ATTACH_IS_ACTIVE) != null)) {
+      enabled = false;
+    }
     e.getPresentation().setVisible(true);
     e.getPresentation().setEnabled(enabled);
   }
 
   @Nullable
-  private static RunConfiguration findRunConfig(Project project) {
+  public static RunConfiguration findRunConfig(Project project) {
     // Look for a Flutter run config. If exactly one is found then return it otherwise return null.
     RunManagerEx mgr = RunManagerEx.getInstanceEx(project);
     List<RunConfiguration> configs = mgr.getAllConfigurationsList();
@@ -139,6 +162,33 @@ public class AttachDebuggerAction extends FlutterSdkAction {
       }
     }
     return count == 1 ? sdkConfig : null;
+  }
+
+  private static void onAttachTermination(@NotNull Project project, @NotNull Consumer<Project> runner) {
+    MessageBusConnection connection = project.getMessageBus().connect();
+
+    // Need an ExecutionListener to clean up project-scoped state when the Stop button is clicked.
+    connection.subscribe(ExecutionManager.EXECUTION_TOPIC, new ExecutionListener() {
+      Object handler;
+
+      @Override
+      public void processStarted(@NotNull String executorId, @NotNull ExecutionEnvironment env, @NotNull ProcessHandler handler) {
+        if (env.getRunProfile() instanceof SdkAttachConfig) {
+          this.handler = handler;
+        }
+      }
+
+      @Override
+      public void processTerminated(@NotNull String executorId,
+                                    @NotNull ExecutionEnvironment env,
+                                    @NotNull ProcessHandler handler,
+                                    int exitCode) {
+        if (this.handler == handler) {
+          runner.accept(project);
+          connection.disconnect();
+        }
+      }
+    });
   }
 
   private static void showSelectConfigDialog() {
