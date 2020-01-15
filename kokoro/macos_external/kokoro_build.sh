6fd24c28375302551e47af20805518550f154b39
Steve Messick <messick@google.com>
2020-01-15 11:16:17 -0800

    Clean up

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -3,18 +3,13 @@
 # Fail on any error.
 set -e
 
-# Display commands being run.
-# WARNING: please only enable 'set -x' if necessary for debugging, and be very
-#  careful if you handle credentials (e.g. from Keystore) with 'set -x':
-#  statements like "export VAR=$(cat /tmp/keystore/credentials)" will result in
-#  the credentials being printed in build logs.
-#  Additionally, recursive invocation with credentials as command-line
-#  parameters, will print the full command, with credentials, in the build logs.
+# Display commands being run. Only do this while debugging and be careful
+# that no confidential information is displayed.
 # set -x
-
 
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
 # The final directory name in this path is determined by the scm name specified
 # in the job configuration.
 cd ${KOKORO_ARTIFACTS_DIR}/github/flutter-intellij-kokoro
+
 ./tool/kokoro/build.sh
--- tool/kokoro/build.sh
+++ tool/kokoro/build.sh
@@ -3,13 +3,8 @@
 # Fail on any error.
 set -e
 
-# Display commands being run.
-# WARNING: please only enable 'set -x' if necessary for debugging, and be very
-#  careful if you handle credentials (e.g. from Keystore) with 'set -x':
-#  statements like "export VAR=$(cat /tmp/keystore/credentials)" will result in
-#  the credentials being printed in build logs.
-#  Additionally, recursive invocation with credentials as command-line
-#  parameters, will print the full command, with credentials, in the build logs.
+# Display commands being run. Only do this while debugging and be careful
+# that no confidential information is displayed.
 # set -x
 
 if [ "$1" == "release" ]; then
