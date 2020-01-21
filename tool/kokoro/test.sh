3524e7d3a74d92d3eddba09bc7dce128678b8f7b
Steve Messick <messick@google.com>
2020-01-21 12:52:20 -0800

    Check pre-reqs for build

--- tool/kokoro/test.sh
+++ tool/kokoro/test.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 # Fail on any error.
-#set -e
+set -e
 
 # Display commands being run. Only do this while debugging and be careful
 # that no confidential information is displayed.
 # set -x
 
-java -version
-echo "JAVA_HOME=$JAVA_HOME"
-ant -version
-curl --version
-zip --version
+./bin/plugin test
 
-echo "kokoro build finished"
+echo "kokoro test finished"
