244b854e081518899d579ee30363def7cfa3132a
Steve Messick <messick@google.com>
2020-01-22 13:40:46 -0800

    Refactor build script

--- tool/kokoro/test.sh
+++ tool/kokoro/test.sh
@@ -1,36 +1,15 @@
 #!/bin/bash
 
-# Fail on any error.
-set -e
 
-# Display commands being run. Only do this while debugging and be careful
-# that no confidential information is displayed.
-# set -x
-
-git clone --depth 1 https://github.com/flutter/flutter.git ../flutter
-export PATH="$PATH":`pwd`/../flutter/bin:`pwd`/../flutter/bin/cache/dart-sdk/bin
-flutter config --no-analytics
-flutter doctor
-export FLUTTER_SDK=`pwd`/../flutter
-
-java -version
-echo "JAVA_HOME=$JAVA_HOME"
-
-echo "install ant"
-curl https://www-us.apache.org/dist//ant/binaries/apache-ant-1.10.7-bin.tar.gz > ../ant.tar.gz
-(cd ..; tar fx ant.tar.gz)
-export PATH=$PATH:`pwd`/../apache-ant-1.10.7/bin
-
-echo "pub get `pwd`"; pub get --no-precompile
-(cd testData/sample_tests; echo "pub get `pwd`"; pub get --no-precompile)
-(cd tool/plugin; echo "pub get `pwd`"; pub get --no-precompile)
+source ./setup.sh
+setup
 
 echo "kokoro test start"
 ./bin/plugin test
 
 echo "kokoro test finished"
 
-
+# This is temporary, duplicating build.sh
 echo "kokoro build start"
 ./bin/plugin build --channel=dev
 
