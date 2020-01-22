907a2fc21d9ae53187c39c0d450df4b66854b0d1
stevemessick <stevemessick@users.noreply.github.com>
2020-01-22 07:27:48 -0800

    Test (#4313)

--- tool/kokoro/test.sh
+++ tool/kokoro/test.sh
@@ -7,6 +7,18 @@ set -e
 # that no confidential information is displayed.
 # set -x
 
+git clone --depth 1 https://github.com/flutter/flutter.git ../flutter
+export PATH="$PATH":`pwd`/../flutter/bin:`pwd`/../flutter/bin/cache/dart-sdk/bin
+flutter config --no-analytics
+flutter doctor
+export FLUTTER_SDK=`pwd`/../flutter
+
+java -version
+echo "JAVA_HOME=$JAVA_HOME"
+ant -version
+curl --version
+zip --version
+
 ./bin/plugin test
 
 echo "kokoro test finished"
