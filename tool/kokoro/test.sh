053b6e11aaca3eb0395d005d297c6e2e4a150bff
Steve Messick <messick@google.com>
2020-02-10 10:55:26 -0800

    testing

--- tool/kokoro/test.sh
+++ tool/kokoro/test.sh
@@ -9,11 +9,13 @@ echo "kokoro test start"
 
 echo "kokoro test finished"
 
-#TODO(messick) Temprary
+#TODO(messick) Temporary
 echo "kokoro build start"
-echo "KOKORO_KEYSTORE_DIR $KOKORO_KEYSTORE_DIR"
-ls -l $KOKORO_KEYSTORE_DIR/74840_flutter-intellij-plugin-auth-token
+#echo "KOKORO_KEYSTORE_DIR $KOKORO_KEYSTORE_DIR"
+#ls -l $KOKORO_KEYSTORE_DIR/74840_flutter-intellij-plugin-auth-token
+$KOKORO_KEYSTORE_DIR /tmpfs/src/keystore
+#-rw-r--r--  1 kbuilder  admin  61 Feb 10 10:45 /tmpfs/src/keystore/74840_flutter-intellij-plugin-auth-token
 
-#./bin/plugin build --channel=dev
+./bin/plugin build --channel=dev
 
 echo "kokoro build finished"
