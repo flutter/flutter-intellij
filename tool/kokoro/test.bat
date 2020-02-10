07a4b0ae15f2ce3d183a64b3af8e99b57adde02c
Steve Messick <messick@google.com>
2020-02-10 15:17:41 -0800

    fix rem

--- tool/kokoro/test.bat
+++ tool/kokoro/test.bat
@@ -1,23 +1,28 @@
-# Copied from setup.sh
-# TODO(messick) Translate to CMD
+REM Copied from setup.sh
+REM TODO(messick) Translate to CMD
 
-#  git clone --depth 1 https://github.com/flutter/flutter.git ../flutter
-#  export PATH="$PATH":`pwd`/../flutter/bin:`pwd`/../flutter/bin/cache/dart-sdk/bin
-#  flutter config --no-analytics
-#  flutter doctor
-#  export FLUTTER_SDK=`pwd`/../flutter
+REM  git clone --depth 1 https://github.com/flutter/flutter.git ../flutter
+REM  export PATH="$PATH":`pwd`/../flutter/bin:`pwd`/../flutter/bin/cache/dart-sdk/bin
+REM  flutter config --no-analytics
+REM  flutter doctor
+REM  export FLUTTER_SDK=`pwd`/../flutter
 
-#  java -version
-#  echo "JAVA_HOME=$JAVA_HOME"
+REM  java -version
+REM  echo "JAVA_HOME=$JAVA_HOME"
 
-#  echo "install ant"
-#  curl https://www-us.apache.org/dist/ant/binaries/apache-ant-1.10.7-bin.tar.gz > ../ant.tar.gz
-#  (cd ..; tar fx ant.tar.gz)
-#  export PATH=$PATH:`pwd`/../apache-ant-1.10.7/bin
+REM  echo "install ant"
+REM  curl https://www-us.apache.org/dist/ant/binaries/apache-ant-1.10.7-bin.tar.gz > ../ant.tar.gz
+REM  (cd ..; tar fx ant.tar.gz)
+REM  export PATH=$PATH:`pwd`/../apache-ant-1.10.7/bin
 
-#  export FLUTTER_KEYSTORE_ID=74840
-#  export FLUTTER_KEYSTORE_NAME=flutter-intellij-plugin-auth-token
+REM  export FLUTTER_KEYSTORE_ID=74840
+REM  export FLUTTER_KEYSTORE_NAME=flutter-intellij-plugin-auth-token
 
-#  (cd tool/plugin; echo "pub get `pwd`"; pub get --no-precompile)
+REM  (cd tool/plugin; echo "pub get `pwd`"; pub get --no-precompile)
 
-# ./bin/plugin test
+REM ./bin/plugin test
+
+java -version
+ant -version
+curl --version
+git --version
