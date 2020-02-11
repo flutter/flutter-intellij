f2a2100a59215bb07debdceebf037ad6c90ea30b
Steve Messick <messick@google.com>
2020-02-11 15:18:46 -0800

    debugging

--- kokoro/gcp_windows/kokoro_test.bat
+++ kokoro/gcp_windows/kokoro_test.bat
@@ -2,7 +2,7 @@ cd %KOKORO_ARTIFACTS_DIR%\github\flutter-intellij-kokoro
 
 rem Use choco to install dart so we get the latest version without having to edit this script
 rem Can't find where choco puts dart-sdk
-rem choco -y install dart-sdk
+choco -y install dart-sdk
 rem call RefreshEnv.cmd
 
 call tool\kokoro\test.bat
