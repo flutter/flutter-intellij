55ff853ef4a1d8d2b6d46b5ab2e97bfb533e26c4
Steve Messick <messick@google.com>
2020-02-11 12:06:08 -0800

    debugging

--- tool/kokoro/test.bat
+++ tool/kokoro/test.bat
@@ -1,6 +1,8 @@
 rem @echo off
-git clone --depth 1 https://github.com/flutter/flutter.git ../flutter
-set PATH=%PATH%;..\flutter\bin\cache\dart-sdk\bin
+choco install dart-sdk
+
+REM "%~dp0" is the directory of this file including trailing backslash
+SET PATH=%~dp0bin;%PATH%
 
 java -version
 echo "JAVA_HOME=%JAVA_HOME%"
@@ -11,16 +13,16 @@ cd ant
 curl https://www-us.apache.org/dist/ant/binaries/apache-ant-1.10.7-bin.zip > ant.zip
 unzip -q ant.zip
 cd ..
-set PATH=%PATH%;ant\apache-ant-1.10.7\bin
+set PATH=%PATH%;%~dp0ant\apache-ant-1.10.7\bin
 rem ant -version
 
 set FLUTTER_KEYSTORE_ID=74840
 set FLUTTER_KEYSTORE_NAME=flutter-intellij-plugin-auth-token
 
 cd tool\plugin
-dir /s/o ..\..\..
+rem dir /s/o ..\..\..
 echo "pub get"
-..\..\..\flutter\bin\cache\dart-sdk\bin\pub get --no-precompile || goto :error
+pub get --no-precompile || goto :error
 cd ..\..
 
 dart tool\plugin\bin\main.dart test || goto :error
