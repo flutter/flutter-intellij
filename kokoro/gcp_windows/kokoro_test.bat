6007c2fbcfe92f3520dee03bca74a85ab8be6c33
Steve Messick <messick@google.com>
2020-02-11 07:41:39 -0800

    install newer powershell

--- kokoro/gcp_windows/kokoro_test.bat
+++ kokoro/gcp_windows/kokoro_test.bat
@@ -1,2 +1,4 @@
 cd %KOKORO_ARTIFACTS_DIR%\github\flutter-intellij-kokoro
+curl https://github.com/PowerShell/PowerShell/releases/download/v6.2.4/PowerShell-6.2.4-win-x64.msi > PowerShell-6.2.4-win-x64.msi
+msiexec.exe /package PowerShell-6.2.4-win-x64.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
 powershell.exe -File tool\kokoro\test.ps1
