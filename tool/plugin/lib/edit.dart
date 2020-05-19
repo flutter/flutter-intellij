dc071a649df6d3b87ab689020a08a23a86c752e7
Steve Messick <messick@google.com>
2020-05-19 14:58:21 -0700

    reformat

--- tool/plugin/lib/edit.dart
+++ tool/plugin/lib/edit.dart
@@ -199,125 +199,9 @@ Future<int> applyEdits(BuildSpec spec, Function compileFn) async {
       }
     });
 
-//    var files = <File, String>{};
-//    var processedFile, source;
-//
-//    // TODO: Remove this when we no longer support the corresponding version.
-//    try {
-//      if (spec.version.startsWith('3.6')) {
-//        processedFile = File(
-//            'flutter-studio/src/io/flutter/android/AndroidModuleLibraryManager.java');
-//        source = processedFile.readAsStringSync();
-//        files[processedFile] = source;
-//        source = source.replaceAll(
-//          'super(filePath',
-//          'super(filePath.toString()',
-//        );
-//        // Starting with 3.6 we need to call a simplified init().
-//        // This is where the $PROJECT_FILE$ macro is defined, #registerComponents.
-//        source = source.replaceAll(
-//          'getStateStore().setPath(path',
-//          'getStateStore().setPath(path.toString()',
-//        );
-//        processedFile.writeAsStringSync(source);
-//      }
-//
-//      if (!spec.version.startsWith('4.')) {
-//        processedFile = File(
-//            'flutter-studio/src/io/flutter/project/FlutterProjectSystem.java');
-//        source = processedFile.readAsStringSync();
-//        files[processedFile] = source;
-//        source = source.replaceAll(
-//          'import com.android.tools.idea.projectsystem.SourceProvidersFactory;',
-//          '',
-//        );
-//        source = source.replaceAll(' SourceProvidersFactory ', ' Object ');
-//        source = source.replaceAll(
-//          'gradleProjectSystem.getSourceProvidersFactory()',
-//          'new Object()',
-//        );
-//        source = source.replaceAll(
-//          'gradleProjectSystem.getAndroidFacetsWithPackageName(project, packageName, scope)',
-//          'Collections.emptyList()',
-//        );
-//        if (spec.version.startsWith('3.6')) {
-//          source = source.replaceAll(
-//            'gradleProjectSystem.getSubmodules()',
-//            'new java.util.ArrayList()',
-//          );
-//        }
-//        processedFile.writeAsStringSync(source);
-//
-//        processedFile = File(
-//            'flutter-studio/src/io/flutter/module/FlutterDescriptionProvider.java');
-//        source = processedFile.readAsStringSync();
-//        files[processedFile] = source;
-//        source = source.replaceAll(
-//          'import com.android.tools.idea.npw.model.ProjectSyncInvoker',
-//          'import com.android.tools.idea.npw.model.NewModuleModel',
-//        );
-//        source = source.replaceAll(
-//          'createStep(@NotNull Project model, @NotNull ProjectSyncInvoker invoker, String parent)',
-//          'createStep(@NotNull NewModuleModel model)',
-//        );
-//        source = source.replaceAll(
-//          'FlutterProjectModel model(@NotNull Project project,',
-//          'FlutterProjectModel model(@NotNull NewModuleModel project,',
-//        );
-//        source = source.replaceAll(
-//          'mySharedModel.getValue().project().setValue(project);',
-//          'mySharedModel.getValue().project().setValue(project.getProject().getValue());',
-//        );
-//        processedFile.writeAsStringSync(source);
-//      } else if (spec.version.startsWith('4.0')) {
-//        processedFile = File(
-//            'flutter-studio/src/io/flutter/project/FlutterProjectSystem.java');
-//        source = processedFile.readAsStringSync();
-//        files[processedFile] = source;
-//        source = source.replaceAll(
-//          'gradleProjectSystem.getAndroidFacetsWithPackageName(project, packageName, scope)',
-//          'Collections.emptyList()',
-//        );
-//        processedFile.writeAsStringSync(source);
-//      }
-//      if (!spec.version.startsWith('4.1')) {
-//        processedFile = File(
-//            'flutter-studio/src/io/flutter/actions/FlutterShowStructureSettingsAction.java');
-//        source = processedFile.readAsStringSync();
-//        files[processedFile] = source;
-//        source = source.replaceAll(
-//          'import com.android.tools.idea.gradle.structure.actions.AndroidShowStructureSettingsAction;',
-//          'import com.android.tools.idea.gradle.actions.AndroidShowStructureSettingsAction;',
-//        );
-//        processedFile.writeAsStringSync(source);
-//
-//        processedFile = File(
-//            'flutter-studio/src/io/flutter/actions/OpenAndroidModule.java');
-//        source = processedFile.readAsStringSync();
-//        files[processedFile] = source;
-//        source = source.replaceAll(
-//          'findGradleTarget', // 3 matches
-//          'findImportTarget',
-//        );
-//        processedFile.writeAsStringSync(source);
-//      } else {
-//        processedFile = File('src/io/flutter/utils/AndroidUtils.java');
-//        source = processedFile.readAsStringSync();
-//        files[processedFile] = source;
-//        source = source.replaceAll(
-//          'import com.android.tools.idea.gradle.dsl.parser.BuildModelContext;',
-//          'import com.android.tools.idea.gradle.dsl.model.BuildModelContext;',
-//        );
-//        processedFile.writeAsStringSync(source);
-//      }
-
     return await compileFn.call();
   } finally {
     // Restore sources.
-//      files.forEach((file, src) {
-//        log('Restoring ${file.path}');
-//        file.writeAsStringSync(src);
-//      });
     edited.forEach((edit, source) {
       log('Restoring ${edit.path}');
       edit.restore(source);
@@ -381,7 +265,7 @@ class Subst extends EditCommand {
   }
 
   String toString() {
-    return "Subst(path: $path)";
+    return "Subst(path: $path, versions: $versions)";
   }
 
   bool versionMatches(BuildSpec spec) {
