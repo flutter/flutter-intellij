12ca1c00f5ef99ead7fc9e4e605197bf843dfb98
Danny Tuppeny <danny@tuppeny.com>
2020-06-16 10:15:31 +0100

    Update Material colors file

--- tool/colors/colors_main.dart
+++ tool/colors/colors_main.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'colors.dart';
+import 'colors_material.dart';
 
 void main() {
   // https://github.com/flutter/flutter/blob/master/packages/flutter/lib/src/material/colors.dart
