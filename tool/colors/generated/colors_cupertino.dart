141d72767eb3fec54684c37369255207837b2efe
Danny Tuppeny <danny@tuppeny.com>
2020-06-16 18:14:29 +0100

    Download and generate colors without needing a hard-coded list

---
+++ tool/colors/generated/colors_cupertino.dart
@@ -0,0 +1,30 @@
+// Generated file - do not edit.
+
+import '../stubs.dart';
+import '../flutter/colors_cupertino.dart';
+
+final Map<String, Color> colors = <String, Color>{
+  "white": CupertinoColors.white,
+  "black": CupertinoColors.black,
+  "lightBackgroundGray": CupertinoColors.lightBackgroundGray,
+  "extraLightBackgroundGray": CupertinoColors.extraLightBackgroundGray,
+  "darkBackgroundGray": CupertinoColors.darkBackgroundGray,
+  "label": CupertinoColors.label,
+  "secondaryLabel": CupertinoColors.secondaryLabel,
+  "tertiaryLabel": CupertinoColors.tertiaryLabel,
+  "quaternaryLabel": CupertinoColors.quaternaryLabel,
+  "systemFill": CupertinoColors.systemFill,
+  "secondarySystemFill": CupertinoColors.secondarySystemFill,
+  "tertiarySystemFill": CupertinoColors.tertiarySystemFill,
+  "quaternarySystemFill": CupertinoColors.quaternarySystemFill,
+  "placeholderText": CupertinoColors.placeholderText,
+  "systemBackground": CupertinoColors.systemBackground,
+  "secondarySystemBackground": CupertinoColors.secondarySystemBackground,
+  "tertiarySystemBackground": CupertinoColors.tertiarySystemBackground,
+  "systemGroupedBackground": CupertinoColors.systemGroupedBackground,
+  "secondarySystemGroupedBackground": CupertinoColors.secondarySystemGroupedBackground,
+  "tertiarySystemGroupedBackground": CupertinoColors.tertiarySystemGroupedBackground,
+  "separator": CupertinoColors.separator,
+  "opaqueSeparator": CupertinoColors.opaqueSeparator,
+  "link": CupertinoColors.link,
+};
