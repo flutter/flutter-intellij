8efd52115efffda466af95d015b4508ff64b006c
Steve Messick <messick@google.com>
2020-06-16 15:08:23 -0700

    Add Flutter News panel for release notes

---
+++ src/io/flutter/assistant/whatsnew/FlutterNewsBundle.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2020 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+package io.flutter.assistant.whatsnew;
+
+import com.android.tools.idea.assistant.DefaultTutorialBundle;
+
+public class FlutterNewsBundle extends DefaultTutorialBundle {
+}
