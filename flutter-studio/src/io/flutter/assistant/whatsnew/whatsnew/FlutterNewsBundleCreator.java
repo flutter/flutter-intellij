4a37b779f20282529dd5623f6df296a4f6693dcd
Steve Messick <messick@google.com>
2020-07-06 14:06:26 -0700

    Move to flutter-studio

--- flutter-studio/src/io/flutter/assistant/whatsnew/whatsnew/FlutterNewsBundleCreator.java
+++ flutter-studio/src/io/flutter/assistant/whatsnew/whatsnew/FlutterNewsBundleCreator.java
@@ -1,27 +1,32 @@
 /*
- * Copyright 2020 The Chromium Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package io.flutter.assistant.whatsnew;
+package io.flutter.assistant.whatsnew.whatsnew;
 
 import com.android.tools.idea.assistant.AssistantBundleCreator;
 import com.android.tools.idea.assistant.DefaultTutorialBundle;
 import com.android.tools.idea.assistant.datamodel.AnalyticsProvider;
-import com.intellij.execution.process.ProcessOutput;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManager;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Ref;
 import io.flutter.sdk.FlutterSdk;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
