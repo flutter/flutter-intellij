3991f409800afdf03761a638bd65b857338a6791
Helin Shiah <helinx@google.com>
2020-07-07 11:50:46 -0700

    Change directory to google for storing jxbrowser file

--- src/io/flutter/utils/FileUtils.java
+++ src/io/flutter/utils/FileUtils.java
@@ -9,7 +9,7 @@ import com.intellij.openapi.util.SystemInfoRt;
 import com.intellij.util.SystemProperties;
 
 public class FileUtils {
-  private static final String IDE_VENDOR_NAME = System.getProperty("idea.vendor.name", "JetBrains");
+  private static final String GOOGLE_DIRECTORY = "Google";
   // This method is a copy of com.intellij.openapi.application.PathManager#platformPath modified to provide the path to the directory
   // containing all JetBrains application files.
   public static String platformPath() {
@@ -28,7 +28,7 @@ public class FileUtils {
     final String userHome = SystemProperties.getUserHome();
 
     if (SystemInfoRt.isMac) {
-      String dir = userHome + "/Library/" + macDir + '/' + IDE_VENDOR_NAME + '/' + selector;
+      String dir = userHome + "/Library/" + macDir + '/' + GOOGLE_DIRECTORY + '/' + selector;
       if (!macSub.isEmpty()) dir = dir + '/' + macSub;
       return dir;
     }
@@ -36,7 +36,7 @@ public class FileUtils {
     if (SystemInfoRt.isWindows) {
       String dir = System.getenv(winVar);
       if (dir == null || dir.isEmpty()) dir = userHome + "\\AppData\\" + (winVar.startsWith("LOCAL") ? "Local" : "Roaming");
-      dir = dir + '\\' + IDE_VENDOR_NAME + '\\' + selector;
+      dir = dir + '\\' + GOOGLE_DIRECTORY + '\\' + selector;
       if (!winSub.isEmpty()) dir = dir + '\\' + winSub;
       return dir;
     }
@@ -44,7 +44,7 @@ public class FileUtils {
     if (SystemInfoRt.isUnix) {
       String dir = System.getenv(xdgVar);
       if (dir == null || dir.isEmpty()) dir = userHome + '/' + xdgDfl;
-      dir = dir + '/' + IDE_VENDOR_NAME + '/' + selector;
+      dir = dir + '/' + GOOGLE_DIRECTORY + '/' + selector;
       if (!xdgSub.isEmpty()) dir = dir + '/' + xdgSub;
       return dir;
     }
