2da8233439e17d94c2bf0c1a1bd8a815d1b54a86
Devon Carew <devoncarew@google.com>
2020-07-26 21:58:05 -0700

    use DartSyntax.findEnclosingFunctionCall

--- testSrc/unit/io/flutter/editor/FlutterEditorAnnotatorTest.java
+++ testSrc/unit/io/flutter/editor/FlutterEditorAnnotatorTest.java
@@ -218,8 +218,7 @@ public class FlutterEditorAnnotatorTest extends AbstractDartElementTest {
   public void locatesARGBColor() throws Exception {
     run(() -> {
       final PsiElement testIdentifier = setUpDartElement("main() { Color.fromARGB(255, 255, 0,0); }", "Color", LeafPsiElement.class);
-      // todo: use findEnclosingFunctionCall?
-      final DartNewExpression element = DartSyntax.findEnclosingNewExpression(testIdentifier);
+      final DartNewExpression element = DartSyntax.findEnclosingFunctionCall(testIdentifier);
       assert element != null;
 
       final FlutterEditorAnnotator annotator = new FlutterEditorAnnotator();
@@ -261,8 +260,7 @@ public class FlutterEditorAnnotatorTest extends AbstractDartElementTest {
   public void locatesRGBOColor() throws Exception {
     run(() -> {
       final PsiElement testIdentifier = setUpDartElement("main() { Color.fromRGBO(255, 255, 0, 1.0); }", "Color", LeafPsiElement.class);
-      // todo: use findEnclosingFunctionCall?
-      final DartNewExpression element = DartSyntax.findEnclosingNewExpression(testIdentifier);
+      final DartNewExpression element = DartSyntax.findEnclosingFunctionCall(testIdentifier);
       assert element != null;
 
       final FlutterEditorAnnotator annotator = new FlutterEditorAnnotator();
