2fdcf87c481d6a7147519f99984adc078ed08aab
Helin Shiah <helinx@google.com>
2020-08-04 12:16:42 -0700

    Use workspace to return root for bazel projects

--- src/io/flutter/run/MainFile.java
+++ src/io/flutter/run/MainFile.java
@@ -20,6 +20,7 @@ import com.jetbrains.lang.dart.psi.DartFile;
 import com.jetbrains.lang.dart.psi.DartImportStatement;
 import com.jetbrains.lang.dart.util.DartResolveUtil;
 import io.flutter.FlutterBundle;
+import io.flutter.bazel.Workspace;
 import io.flutter.bazel.WorkspaceCache;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -120,16 +121,22 @@ public class MainFile {
 
   @Nullable
   private static VirtualFile findAppDir(@Nullable VirtualFile file, @NotNull Project project) {
+    if (WorkspaceCache.getInstance(project).isBazel()) {
+      Workspace workspace = WorkspaceCache.getInstance(project).get();
+      if (workspace != null) {
+        return workspace.getRoot();
+      }
+    }
+
     for (VirtualFile candidate = file; inProject(candidate, project); candidate = candidate.getParent()) {
-      if (isAppDir(candidate, project)) return candidate;
+      if (isAppDir(candidate)) return candidate;
     }
     return null;
   }
 
-  private static boolean isAppDir(@NotNull VirtualFile dir, @NotNull Project project) {
+  private static boolean isAppDir(@NotNull VirtualFile dir) {
     return dir.isDirectory() && (
       dir.findChild("pubspec.yaml") != null ||
-      (WorkspaceCache.getInstance(project).isBazel() && dir.findChild("BUILD") != null) ||
       dir.findChild(".packages") != null
     );
   }
