5b12905b60648a6238bd280c886ccfd9193bcee9
Helin Shiah <helinx@google.com>
2020-08-28 09:10:01 -0700

    Fix white screen during embedded browser load (#4787)
    * Use transparent background
    
    * Wait for devtools URL to load before showing content
    
    * Try navigation change
    
    * Make BrowserView after loading and make vars final

--- src/io/flutter/jxbrowser/EmbeddedBrowser.java
+++ src/io/flutter/jxbrowser/EmbeddedBrowser.java
@@ -5,43 +5,57 @@
  */
 package io.flutter.jxbrowser;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentManager;
 import com.teamdev.jxbrowser.browser.Browser;
 import com.teamdev.jxbrowser.engine.Engine;
 import com.teamdev.jxbrowser.engine.EngineOptions;
+import com.teamdev.jxbrowser.navigation.event.LoadFinished;
 import com.teamdev.jxbrowser.view.swing.BrowserView;
 import icons.FlutterIcons;
 
 import java.awt.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.teamdev.jxbrowser.engine.RenderingMode.HARDWARE_ACCELERATED;
 
 public class EmbeddedBrowser {
   public void openPanel(ContentManager contentManager, String tabName, String url) {
-    EngineOptions options =
+    final EngineOptions options =
       EngineOptions.newBuilder(HARDWARE_ACCELERATED).build();
-    Engine engine = Engine.newInstance(options);
-    Browser browser = engine.newBrowser();
+    final Engine engine = Engine.newInstance(options);
+    final Browser browser = engine.newBrowser();
+    browser.settings().enableTransparentBackground();
 
-    if (contentManager.isDisposed()) {
-      return;
-    }
-    contentManager.removeAllContents(false);
-    final Content content = contentManager.getFactory().createContent(null, tabName, false);
-
-    // Creating Swing component for rendering web content
-    // loaded in the given Browser instance.
-    BrowserView view = BrowserView.newInstance(browser);
-    view.setPreferredSize(new Dimension(contentManager.getComponent().getWidth(), contentManager.getComponent().getHeight()));
-
-    content.setComponent(view);
-    content.putUserData(ToolWindow.SHOW_CONTENT_ICON, Boolean.TRUE);
-    // TODO(helin24): Use differentiated icons for each tab and copy from devtools toolbar.
-    content.setIcon(FlutterIcons.Phone);
-    contentManager.addContent(content);
+    // Multiple LoadFinished events can occur, but we only need to add content the first time.
+    final AtomicBoolean contentLoaded = new AtomicBoolean(false);
 
     browser.navigation().loadUrl(url);
+    browser.navigation().on(LoadFinished.class, event -> {
+      if (!contentLoaded.compareAndSet(false, true)) {
+        return;
+      }
+      ApplicationManager.getApplication().invokeLater(() -> {
+        if (contentManager.isDisposed()) {
+          return;
+        }
+
+        contentManager.removeAllContents(false);
+        final Content content = contentManager.getFactory().createContent(null, tabName, false);
+
+        // Creating Swing component for rendering web content
+        // loaded in the given Browser instance.
+        final BrowserView view = BrowserView.newInstance(browser);
+        view.setPreferredSize(new Dimension(contentManager.getComponent().getWidth(), contentManager.getComponent().getHeight()));
+
+        content.setComponent(view);
+        content.putUserData(ToolWindow.SHOW_CONTENT_ICON, Boolean.TRUE);
+        // TODO(helin24): Use differentiated icons for each tab and copy from devtools toolbar.
+        content.setIcon(FlutterIcons.Phone);
+        contentManager.addContent(content);
+      });
+    });
   }
 }
