db7beba75e96e503fea11df80d056f4c1ebb3555
Helin Shiah <helinx@google.com>
2020-09-11 16:38:06 -0700

    Replace 'click' in panel messages

--- src/io/flutter/view/FlutterView.java
+++ src/io/flutter/view/FlutterView.java
@@ -461,12 +461,12 @@ public class FlutterView implements PersistentStateComponent<FlutterViewState>, 
   protected void handleJxBrowserInstallationFailed(FlutterApp app, InspectorService inspectorService, ToolWindow toolWindow) {
     if (System.getProperty(JxBrowserUtils.LICENSE_PROPERTY_NAME) == null) {
       // If the license isn't available, allow the user to open the equivalent page in a non-embedded browser window.
-      presentClickableLabel(toolWindow, "The JxBrowser license could not be found. Click to open Devtools in the browser.", (linkLabel, data) -> {
+      presentClickableLabel(toolWindow, "The JxBrowser license could not be found. Open Devtools in the browser?", (linkLabel, data) -> {
         presentDevTools(app, inspectorService, toolWindow, false);
       });
     } else {
       // Allow the user to manually restart.
-      presentClickableLabel(toolWindow, "JxBrowser installation failed. Click to retry.", (linkLabel, data) -> {
+      presentClickableLabel(toolWindow, "JxBrowser installation failed. Retry?", (linkLabel, data) -> {
         JxBrowserManager.getInstance().retryFromFailed(app.getProject());
         handleJxBrowserInstallationInProgress(app, inspectorService, toolWindow);
       });
