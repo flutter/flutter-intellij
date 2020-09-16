36961a7cc1bb6325afd3319962f4acead82d8990
Devon Carew <devoncarew@google.com>
2020-09-15 21:46:07 -0700

    Remove 2019.2 from the product matrix

--- tool/plugin/test/plugin_test.dart
+++ tool/plugin/test/plugin_test.dart
@@ -44,7 +44,6 @@ void main() {
               'android-studio',
               'android-studio',
               'android-studio',
-              'android-studio',
             ]));
       });
     });
@@ -60,7 +59,6 @@ void main() {
               'android-studio',
               'android-studio',
               'android-studio',
-              'android-studio',
             ]));
       });
     });
@@ -73,7 +71,6 @@ void main() {
         expect(
             specs.map((spec) => spec.ideaProduct).toList(),
             orderedEquals([
-              'android-studio',
               'android-studio',
               'android-studio',
               'android-studio',
