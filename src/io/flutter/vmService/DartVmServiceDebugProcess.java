9bc2b9b57d23a4ee44db60ec6d182314d613b797
Devon Carew <devoncarew@google.com>
2020-09-17 09:51:39 -0700

    support pausing a running app (#4847)

--- src/io/flutter/vmService/DartVmServiceDebugProcess.java
+++ src/io/flutter/vmService/DartVmServiceDebugProcess.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
 package io.flutter.vmService;
 
 import com.google.gson.JsonObject;
@@ -56,7 +61,7 @@ import java.util.List;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
-public class DartVmServiceDebugProcess extends XDebugProcess {
+public abstract class DartVmServiceDebugProcess extends XDebugProcess {
   private static final Logger LOG = Logger.getInstance(DartVmServiceDebugProcess.class.getName());
 
   @NotNull private final ExecutionResult myExecutionResult;
@@ -93,6 +98,8 @@ public class DartVmServiceDebugProcess extends XDebugProcess {
       new DartVmServiceBreakpointHandler(this),
       new DartExceptionBreakpointHandler(this)
     };
+
+    session.setPauseActionSupported(true);
 
     session.addSessionListener(new XDebugSessionListener() {
       @Override
