1cc740cbae058a180dba8537f8d7a575cdc594d9
Steve Messick <messick@google.com>
2020-09-17 16:01:47 -0700

    Clean up debugging code

--- tool/kokoro/deploy.sh
+++ tool/kokoro/deploy.sh
@@ -4,19 +4,11 @@ source ./tool/kokoro/setup.sh
 setup
 
 echo "kokoro build start"
-./bin/plugin make --channel=dev -o4.0 -u
-
-set +e
-pwd
-echo "List releases"
-ls -lR releases
-echo "List build"
-ls -lR build
-set -e
+./bin/plugin make --channel=dev
 
 echo "kokoro build finished"
 
 echo "kokoro deploy start"
-#./bin/plugin deploy --channel=dev
+./bin/plugin deploy --channel=dev
 
 echo "kokoro deploy finished"
