b691f33f0133617ab7538c29ca1bc4673728c8d5
Steve Messick <messick@google.com>
2020-10-08 16:13:45 -0700

    debugging

--- tool/travis.sh
+++ tool/travis.sh
@@ -17,6 +17,7 @@ if [ "$IDEA_VERSION" = "4.0" -o "$IDEA_VERSION" = "4.1" ] ; then
   wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -
   sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
   sudo apt-get update; sudo apt-get install -y java-1.8.0-amazon-corretto-jdk
+  export PATH=/usr/lib/jvm/java-1.8.0-amazon-corretto/jre/bin:$PATH
 
 fi
 
