20d30721ab720b57a0d0a7b6545b86ffe58021e3
Jacob Richman <jacobr@google.com>
2020-10-29 15:29:11 -0700

    Fix bugs where widget indent behavior sometimes impacted non-dart files, stale indent guides were sometimes displayed, and filtered indent guides were unpredictably displayed. (#5008)

---
+++ src/io/flutter/editor/WidgetIndentsHighlightingPassFactoryRegistrar.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2020 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package io.flutter.editor;
+
+import com.intellij.codeHighlighting.*;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Registrar that adds the WidgetIndentsHighlightingPassFactory.
+ *
+ * It is now neccessary to register highlighting passes with a registrar to
+ * ensure that highlighting passes do not unpredictably disappear.
+ */
+public class WidgetIndentsHighlightingPassFactoryRegistrar implements TextEditorHighlightingPassFactoryRegistrar, DumbAware {
+
+  @Override
+  public void registerHighlightingPassFactory(@NotNull TextEditorHighlightingPassRegistrar registrar, @NotNull Project project) {
+    WidgetIndentsHighlightingPassFactory.getInstance(project).registerHighlightingPassFactory(registrar);
+  }
+}
+
