6e34235b8f40509889b2f700fd558aa027e710f8
Steve Messick <messick@google.com>
2020-11-02 11:58:31 -0800

    install dart with choco

--- tool/github.bat
+++ tool/github.bat
@@ -1,16 +1,20 @@
 rem @echo off
 
 set JAVA_HOME=%JAVA_HOME_11_X64%
-set DART_VERSION=2.7.1
 
 echo "install dart"
+choco install dart-sdk
+
+goto :next
 rem Make this adaptive to the possibility of a file or dir named dart, someday.
 md dart
 cd dart
+set DART_VERSION=2.7.1
 curl https://storage.googleapis.com/dart-archive/channels/stable/release/%DART_VERSION%/sdk/dartsdk-windows-ia32-release.zip > dart.zip
 unzip -q dart.zip
 cd ..
 set PATH=.\dart\dart-sdk\bin;%PATH%
+:next
 
 dart --version || goto :error
 
@@ -19,8 +23,10 @@ echo "JAVA_HOME=%JAVA_HOME%"
 
 cd tool\plugin
 echo "pub get"
-..\..\dart\dart-sdk\bin\pub get --no-precompile
+pub get --no-precompile
 cd ..\..
 
 echo "run tests"
 dart tool\plugin\bin\main.dart test
+
+echo "exit"
