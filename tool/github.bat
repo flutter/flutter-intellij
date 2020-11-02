5737fe097ebb2ed08bb93caff0399d84944fba37
Steve Messick <messick@google.com>
2020-11-02 14:32:05 -0800

    clean up bat file

--- tool/github.bat
+++ tool/github.bat
@@ -2,21 +2,10 @@ rem @echo off
 
 set JAVA_HOME=%JAVA_HOME_11_X64%
 
-choco install dart-sdk
+choco install dart-sdk > choco.log
 refreshenv
 
-goto :next
-rem Make this adaptive to the possibility of a file or dir named dart, someday.
-md dart
-cd dart
-set DART_VERSION=2.7.1
-curl https://storage.googleapis.com/dart-archive/channels/stable/release/%DART_VERSION%/sdk/dartsdk-windows-ia32-release.zip > dart.zip
-unzip -q dart.zip
-cd ..
-set PATH=.\dart\dart-sdk\bin;%PATH%
-:next
-
-dart --version || goto :error
+dart --version
 
 java -version
 echo "JAVA_HOME=%JAVA_HOME%"
