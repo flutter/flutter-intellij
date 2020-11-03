a71094e5d47604b839d2bb4edc8fc3c0d60ccb08
Steve Messick <messick@google.com>
2020-11-02 16:10:12 -0800

    revert that, no actions ran

--- tool/github.bat
+++ tool/github.bat
@@ -1,7 +1,5 @@
 @echo on
 
-set JAVA_HOME=%JAVA_HOME_11_X64%
-
 echo "install dart"
 choco install dart-sdk > choco.log
 call RefreshEnv.cmd
@@ -9,16 +7,14 @@ call RefreshEnv.cmd
 echo "dart version"
 dart --version
 
-echo "java version"
-java -version
-echo "JAVA_HOME=%JAVA_HOME%"
-
 cd tool\plugin
 echo "pub get"
 call pub get --no-precompile
 cd ..\..
 
 echo "run tests"
+set JAVA_HOME=%JAVA_HOME_11_X64%
+echo "JAVA_HOME=%JAVA_HOME%"
 dart tool\plugin\bin\main.dart test
 
 echo "exit"
