be9341ef2aef0824c072aaa3cb16452a562f11b1
Steve Messick <messick@google.com>
2020-11-02 16:27:38 -0800

    set env in job def

--- tool/github.bat
+++ tool/github.bat
@@ -1,4 +1,5 @@
 @echo on
+echo "JAVA_HOME=%JAVA_HOME%"
 
 echo "install dart"
 choco install dart-sdk > choco.log
@@ -13,8 +14,6 @@ call pub get --no-precompile
 cd ..\..
 
 echo "run tests"
-set JAVA_HOME=%JAVA_HOME_11_X64%
-echo "JAVA_HOME=%JAVA_HOME%"
 dart tool\plugin\bin\main.dart test
 
 echo "exit"
