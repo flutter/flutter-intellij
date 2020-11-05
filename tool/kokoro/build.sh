941891f24c1a91e96a145415d6abb79ccb649aff
Steve Messick <messick@google.com>
2020-11-05 07:32:58 -0800

    Add Java 11 to Kokoro

--- tool/kokoro/build.sh
+++ tool/kokoro/build.sh
@@ -3,6 +3,11 @@
 source ./tool/kokoro/setup.sh
 setup
 
+curl https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_osx-x64_bin.tar.gz > ../java.tar.gz
+(cd ..; tar fx java.tar.gz)
+export JAVA_HOME_OLD=$JAVA_HOME
+export JAVA_HOME=`pwd`/../jdk-11.0.2.jdk/Contents/Home
+
 echo "kokoro build start"
 
 ./bin/plugin make --channel=dev
