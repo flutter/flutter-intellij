b5c83b7e402afd9890f99db876b81d2d05dbe6a8
Helin Shiah <helinx@google.com>
2020-12-15 09:59:18 -0800

    Open DevTools directly using pub for external projects (#5116)
    * Add DevTools opening directly from pub for external
    
    * Clarify two versions of serving
    
    * Expand imports

--- src/io/flutter/run/daemon/DevToolsService.java
+++ src/io/flutter/run/daemon/DevToolsService.java
@@ -6,20 +6,32 @@
 package io.flutter.run.daemon;
 
 import com.google.common.collect.ImmutableList;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.process.OSProcessHandler;
+import com.intellij.execution.process.ProcessAdapter;
+import com.intellij.execution.process.ProcessEvent;
 import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.process.ProcessOutput;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.ProjectManagerListener;
+import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.CharsetToolkit;
 import io.flutter.FlutterUtils;
 import io.flutter.bazel.Workspace;
 import io.flutter.bazel.WorkspaceCache;
+import io.flutter.console.FlutterConsoles;
+import io.flutter.sdk.FlutterCommand;
 import io.flutter.sdk.FlutterSdk;
 import io.flutter.sdk.FlutterSdkUtil;
+import io.flutter.utils.JsonUtils;
 import io.flutter.utils.MostlySilentOsProcessHandler;
 import org.jetbrains.annotations.NotNull;
 
@@ -45,6 +57,21 @@ public class DevToolsService {
 
   private DevToolsService(@NotNull final Project project) {
     this.project = project;
+
+    ApplicationManager.getApplication().executeOnPooledThread(() -> {
+      // TODO(helinx): Also use `setUpWithDaemon` for later flutter SDK versions where the daemon request `devtools.serve` has been changed
+      //  to use the latest DevTools server.
+      if (WorkspaceCache.getInstance(project).isBazel()) {
+        setUpWithDaemon();
+      }
+      else {
+        // For earlier flutter versions we need to use pub directly to run the latest DevTools server.
+        setUpWithPub();
+      }
+    });
+  }
+
+  private void setUpWithDaemon() {
     try {
       final GeneralCommandLine command = chooseCommand(project);
       if (command == null) {
@@ -89,6 +116,90 @@ public class DevToolsService {
     });
   }
 
+  private void setUpWithPub() {
+    final FlutterSdk sdk = FlutterSdk.getFlutterSdk(project);
+    if (sdk == null) {
+      return;
+    }
+
+    pubActivateDevTools(sdk).thenAccept(result -> pubRunDevTools(sdk));
+  }
+
+  private void pubRunDevTools(FlutterSdk sdk) {
+    final FlutterCommand command = sdk.flutterPub(null, "global", "run", "devtools", "--machine", "--port=0");
+
+    final OSProcessHandler handler = command.startProcessOrShowError(project);
+    if (handler == null) {
+      return;
+    }
+
+    handler.addProcessListener(new ProcessAdapter() {
+      @Override
+      public void onTextAvailable(@NotNull ProcessEvent event, @NotNull Key outputType) {
+        final String text = event.getText().trim();
+
+        if (text.startsWith("{") && text.endsWith("}")) {
+          // {"event":"server.started","params":{"host":"127.0.0.1","port":9100}}
+
+          try {
+            final JsonElement element = JsonUtils.parseString(text);
+
+            // params.port
+            final JsonObject obj = element.getAsJsonObject();
+            final JsonObject params = obj.getAsJsonObject("params");
+            final String host = JsonUtils.getStringMember(params, "host");
+            final int port = JsonUtils.getIntMember(params, "port");
+
+            if (port != -1) {
+              devToolsInstance.complete(new DevToolsInstance(host, port));
+            }
+            else {
+              handler.destroyProcess();
+            }
+          }
+          catch (JsonSyntaxException e) {
+            handler.destroyProcess();
+          }
+        }
+      }
+    });
+
+    handler.startNotify();
+
+    ProjectManager.getInstance().addProjectManagerListener(project, new ProjectManagerListener() {
+      @Override
+      public void projectClosing(@NotNull Project project) {
+        devToolsInstance = new CompletableFuture<>();
+        handler.destroyProcess();
+      }
+    });
+  }
+
+  private CompletableFuture<Boolean> pubActivateDevTools(FlutterSdk sdk) {
+    final FlutterCommand command = sdk.flutterPub(null, "global", "activate", "devtools");
+
+    final CompletableFuture<Boolean> result = new CompletableFuture<>();
+
+    final Process process = command.start((ProcessOutput output) -> {
+      if (output.getExitCode() != 0) {
+        final String message = (output.getStdout() + "\n" + output.getStderr()).trim();
+        FlutterConsoles.displayMessage(project, null, message, true);
+      }
+    }, null);
+
+    try {
+      final int resultCode = process.waitFor();
+      result.complete(resultCode == 0);
+    }
+    catch (RuntimeException | InterruptedException re) {
+      if (!result.isDone()) {
+        result.complete(false);
+      }
+    }
+
+    return result;
+  }
+
   private static GeneralCommandLine chooseCommand(@NotNull final Project project) {
     // Use daemon script if this is a bazel project.
     final Workspace workspace = WorkspaceCache.getInstance(project).get();
@@ -97,7 +208,6 @@ public class DevToolsService {
       if (script != null) {
         return createCommand(workspace.getRoot().getPath(), script, ImmutableList.of());
       }
-
     }
 
     // Otherwise, use the Flutter SDK.
