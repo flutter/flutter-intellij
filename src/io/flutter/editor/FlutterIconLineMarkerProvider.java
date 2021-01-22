389022522482a048f8be07df68220a4d676ffd7c
stevemessick <stevemessick@users.noreply.github.com>
2021-01-22 14:00:29 -0800

    Show icons only for known sets (#5201)
    * Show icons only for known sets
    
    * Check all library files
    
    * Skip resolution during unit tests

--- src/io/flutter/editor/FlutterIconLineMarkerProvider.java
+++ src/io/flutter/editor/FlutterIconLineMarkerProvider.java
@@ -5,26 +5,46 @@
  */
 package io.flutter.editor;
 
+import static io.flutter.dart.DartPsiUtil.getNamedArgumentExpression;
+import static io.flutter.dart.DartPsiUtil.getNewExprFromType;
+import static io.flutter.dart.DartPsiUtil.getValueOfNamedArgument;
+import static io.flutter.dart.DartPsiUtil.getValueOfPositionalArgument;
+import static io.flutter.dart.DartPsiUtil.parseLiteralNumber;
+import static io.flutter.dart.DartPsiUtil.topmostReferenceExpression;
+
 import com.intellij.codeInsight.daemon.GutterName;
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
 import com.intellij.codeInsight.daemon.LineMarkerProviderDescriptor;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.markup.GutterIconRenderer;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.newvfs.impl.VirtualDirectoryImpl;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.impl.source.tree.AstBufferUtil;
 import com.jetbrains.lang.dart.DartTokenTypes;
 import com.jetbrains.lang.dart.psi.DartArguments;
 import com.jetbrains.lang.dart.psi.DartCallExpression;
 import com.jetbrains.lang.dart.psi.DartNewExpression;
+import com.jetbrains.lang.dart.psi.DartReference;
 import com.jetbrains.lang.dart.util.DartPsiImplUtil;
+import com.jetbrains.lang.dart.util.DartResolveUtil;
 import io.flutter.FlutterBundle;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.swing.Icon;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
-
-import static io.flutter.dart.DartPsiUtil.*;
-
 public class FlutterIconLineMarkerProvider extends LineMarkerProviderDescriptor {
+
+  static final private Map<String, String> KnownPaths = new HashMap<>();
+  static {
+    KnownPaths.put("Icons", "packages/flutter/lib/src/material");
+    KnownPaths.put("IconData", "packages/flutter/lib/src/widgets");
+    KnownPaths.put("CupertinoIcons", "packages/flutter/lib/src/cupertino");
+  }
 
   @Nullable("null means disabled")
   @Override
@@ -43,6 +63,26 @@ public class FlutterIconLineMarkerProvider extends LineMarkerProviderDescriptor 
     if (refExpr == null) return null;
     PsiElement parent = refExpr.getParent();
     if (parent == null) return null;
+
+    // Resolve the class reference and check that it is one of the known, cached classes.
+    if (!ApplicationManager.getApplication().isUnitTestMode()) {
+      final PsiElement symbol = "IconData".equals(name) ? refExpr : refExpr.getFirstChild();
+      final PsiElement result = ((DartReference)symbol).resolve();
+      if (result == null) return null;
+      final List<VirtualFile> library = DartResolveUtil.findLibrary(result.getContainingFile());
+      boolean found = false;
+      for (VirtualFile file : library) {
+        VirtualFile dir = file.getParent();
+        if (dir.isInLocalFileSystem()) {
+          final String path = dir.getPath();
+          if (path.endsWith(KnownPaths.get(name))) {
+            found = true;
+            break;
+          }
+        }
+      }
+      if (!found) return null;
+    }
 
     if (parent.getNode().getElementType() == DartTokenTypes.CALL_EXPRESSION) {
       // Check font family and package
