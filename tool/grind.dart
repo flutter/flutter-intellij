8dedbc72d62ba9c4532c2c3d993635ee16b3ad17
Steve Messick <messick@google.com>
2021-04-02 09:30:35 -0700

    Null safe update

--- tool/grind.dart
+++ tool/grind.dart
@@ -10,7 +10,7 @@ import 'package:meta/meta.dart';
 
 void main(List<String> args) => grind(args);
 
-@Task('Check plugin URLs for liveness')
+@Task('Check plugin URLs for live-ness')
 void checkUrls() async {
   log('checking URLs in FlutterBundle.properties...');
   var lines =
@@ -21,7 +21,7 @@ void checkUrls() async {
       // flutter.io.gettingStarted.url | flutter.analytics.privacyUrl
       if (split[0].toLowerCase().endsWith('url')) {
         var url = split[1];
-        var response = await http.get(url);
+        var response = await http.get(Uri.parse(url));
         log('checking: $url...');
         if (response.statusCode != 200) {
           fail(
