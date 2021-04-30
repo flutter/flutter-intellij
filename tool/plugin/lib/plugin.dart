5f558d785270cc821a170f8a31873f73cfdb6412
Devon Carew <devoncarew@google.com>
2021-04-30 10:00:06 -0700

    initial null safe conversion of tool/plugin (#5454)
    initial null safe conversion of tool/plugin

--- tool/plugin/lib/plugin.dart
+++ tool/plugin/lib/plugin.dart
@@ -1,6 +1,8 @@
 // Copyright 2017 The Chromium Authors. All rights reserved. Use of this source
 // code is governed by a BSD-style license that can be found in the LICENSE file.
 
+// @dart = 2.10
+
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
@@ -184,7 +186,7 @@ Future<bool> performReleaseChecks(ProductCommand cmd) async {
     var gitDir = await GitDir.fromExisting(rootPath);
     var isClean = await gitDir.isWorkingTreeClean();
     if (isClean) {
-      var branch = await gitDir.getCurrentBranch();
+      var branch = await gitDir.currentBranch();
       var name = branch.branchName;
       var expectedName =
           cmd.isDevChannel ? 'master' : "release_${cmd.releaseMajor}";
