f6b51b85f1911ae63c2ae7941f6b33b71cca2a6c
stevemessick <stevemessick@users.noreply.github.com>
2021-05-27 16:28:52 -0700

    Misc changes to coverage (#5522)

--- src/io/flutter/run/coverage/FlutterCoverageEnabledConfiguration.java
+++ src/io/flutter/run/coverage/FlutterCoverageEnabledConfiguration.java
@@ -10,6 +10,8 @@ import com.intellij.coverage.CoverageRunner;
 import com.intellij.execution.configurations.RunConfigurationBase;
 import com.intellij.execution.configurations.coverage.CoverageEnabledConfiguration;
 import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.GuiUtils;
 import io.flutter.FlutterBundle;
 import io.flutter.pub.PubRoot;
@@ -20,20 +22,31 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class FlutterCoverageEnabledConfiguration extends CoverageEnabledConfiguration {
+  private static final Logger LOG = Logger.getInstance(FlutterCoverageEnabledConfiguration.class.getName());
 
   public FlutterCoverageEnabledConfiguration(@NotNull RunConfigurationBase<?> configuration) {
     super(configuration);
     super.setCoverageRunner(CoverageRunner.getInstance(FlutterCoverageRunner.class));
-    final List<PubRoot> roots = PubRoots.forProject(configuration.getProject());
-    if (roots.isEmpty()) {
-      throw new RuntimeException(FlutterBundle.message("project.root.not.found"));
-    }
-    myCoverageFilePath = roots.get(0).getRoot().getCanonicalPath() + "/coverage/lcov.info";
+    createCoverageFile();
     GuiUtils.invokeLaterIfNeeded(
       () -> setCurrentCoverageSuite(CoverageDataManager.getInstance(configuration.getProject()).addCoverageSuite(this)),
       ModalityState.any());
   }
 
+  @Override
+  protected String createCoverageFile() {
+    if (myCoverageFilePath == null) {
+      final List<PubRoot> roots = PubRoots.forProject(getConfiguration().getProject());
+      if (roots.isEmpty()) {
+        throw new RuntimeException(FlutterBundle.message("project.root.not.found"));
+      }
+      final VirtualFile root = roots.get(0).getRoot();
+      myCoverageFilePath = root.getPath() + "/coverage/lcov.info";
+    }
+    return myCoverageFilePath;
+  }
+
+  @Override
   public void setCoverageRunner(@Nullable final CoverageRunner coverageRunner) {
     // Save and restore myCoverageFilePath because the super method clears it.
     final String path = myCoverageFilePath;
@@ -41,6 +54,7 @@ public class FlutterCoverageEnabledConfiguration extends CoverageEnabledConfigur
     myCoverageFilePath = path;
   }
 
+  @Override
   public void coverageRunnerExtensionRemoved(@NotNull CoverageRunner runner) {
     final String path = myCoverageFilePath;
     super.coverageRunnerExtensionRemoved(runner);
