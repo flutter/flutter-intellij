29d94386bc1c3d12919a222ccb56d94b16eee8b9
Helin Shiah <helinx@google.com>
2021-07-22 11:35:30 -0700

    Add tool window listeners to save visible state

--- src/io/flutter/performance/FlutterPerformanceViewFactory.java
+++ src/io/flutter/performance/FlutterPerformanceViewFactory.java
@@ -5,6 +5,7 @@
  */
 package io.flutter.performance;
 
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.DumbAware;
@@ -13,10 +14,13 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.openapi.wm.ex.ToolWindowManagerListener;
 import io.flutter.view.FlutterViewMessages;
 import org.jetbrains.annotations.NotNull;
 
 public class FlutterPerformanceViewFactory implements ToolWindowFactory, DumbAware {
+  private static final String TOOL_WINDOW_VISIBLE_PROPERTY = "flutter.performance.tool.window.visible";
+
   public static void init(@NotNull Project project) {
     project.getMessageBus().connect().subscribe(
       FlutterViewMessages.FLUTTER_DEBUG_TOPIC, (event) -> initPerfView(project, event)
@@ -24,6 +28,10 @@ public class FlutterPerformanceViewFactory implements ToolWindowFactory, DumbAwa
     final ToolWindow window = ToolWindowManager.getInstance(project).getToolWindow(FlutterPerformanceView.TOOL_WINDOW_ID);
     if (window != null) {
       window.setAvailable(true);
+
+      if (PropertiesComponent.getInstance(project).getBoolean(TOOL_WINDOW_VISIBLE_PROPERTY, false)) {
+        window.activate(null, false);
+      }
     }
   }
 
@@ -46,5 +54,24 @@ public class FlutterPerformanceViewFactory implements ToolWindowFactory, DumbAwa
   @Override
   public boolean shouldBeAvailable(@NotNull Project project) {
     return false;
+  }
+
+  /**
+   * Helps to initialize tool window with the same visibility state as it was when the project was previously closed.
+   */
+  public static class FlutterPerformanceViewListener implements ToolWindowManagerListener {
+    private final @NotNull Project myProject;
+
+    public FlutterPerformanceViewListener(@NotNull Project project) {
+      myProject = project;
+    }
+
+    @Override
+    public void stateChanged(@NotNull ToolWindowManager toolWindowManager) {
+      ToolWindow toolWindow = toolWindowManager.getToolWindow(FlutterPerformanceView.TOOL_WINDOW_ID);
+      if (toolWindow != null && toolWindow.isAvailable()) {
+        PropertiesComponent.getInstance(myProject).setValue(TOOL_WINDOW_VISIBLE_PROPERTY, toolWindow.isVisible(), false);
+      }
+    }
   }
 }
--- src/io/flutter/preview/PreviewViewFactory.java
+++ src/io/flutter/preview/PreviewViewFactory.java
@@ -5,6 +5,7 @@
  */
 package io.flutter.preview;
 
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.DumbService;
@@ -12,13 +13,20 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.openapi.wm.ex.ToolWindowManagerListener;
 import org.jetbrains.annotations.NotNull;
 
 public class PreviewViewFactory implements ToolWindowFactory, DumbAware {
+  private static final String TOOL_WINDOW_VISIBLE_PROPERTY = "flutter.preview.tool.window.visible";
+
   public static void init(@NotNull Project project) {
     final ToolWindow window = ToolWindowManager.getInstance(project).getToolWindow(PreviewView.TOOL_WINDOW_ID);
     if (window != null) {
       window.setAvailable(true);
+
+      if (PropertiesComponent.getInstance(project).getBoolean(TOOL_WINDOW_VISIBLE_PROPERTY, false)) {
+        window.activate(null, false);
+      }
     }
   }
 
@@ -33,5 +41,24 @@ public class PreviewViewFactory implements ToolWindowFactory, DumbAware {
   @Override
   public boolean shouldBeAvailable(@NotNull Project project) {
     return false;
+  }
+
+  /**
+   * Helps to initialize tool window with the same visibility state as it was when the project was previously closed.
+   */
+  public static class PreviewViewListener implements ToolWindowManagerListener {
+    private final @NotNull Project myProject;
+
+    public PreviewViewListener(@NotNull Project project) {
+      myProject = project;
+    }
+
+    @Override
+    public void stateChanged(@NotNull ToolWindowManager toolWindowManager) {
+      ToolWindow toolWindow = toolWindowManager.getToolWindow(PreviewView.TOOL_WINDOW_ID);
+      if (toolWindow != null && toolWindow.isAvailable()) {
+        PropertiesComponent.getInstance(myProject).setValue(TOOL_WINDOW_VISIBLE_PROPERTY, toolWindow.isVisible(), false);
+      }
+    }
   }
 }
