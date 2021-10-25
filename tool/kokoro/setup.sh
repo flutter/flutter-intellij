bafc6b64cb94c7b5c60d075bcb33c8a070bf8283
stevemessick <stevemessick@users.noreply.github.com>
2021-10-25 09:59:16 -0700

    Use Java 11 everywhere (#5836)
    * Use Java 11 everywhere
    
    * debug

--- tool/kokoro/setup.sh
+++ tool/kokoro/setup.sh
@@ -16,8 +16,14 @@ setup() {
   flutter doctor
   export FLUTTER_SDK=`pwd`/../flutter
 
-  java -version
+  export JAVA_HOME_OLD=$JAVA_HOME
+  curl https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_osx-x64_bin.tar.gz > ../java.tar.gz
+  (cd ..; tar fx java.tar.gz)
+  export JAVA_HOME=`pwd`/../jdk-11.0.2.jdk/Contents/Home
+  export PATH=$PATH:$JAVA_HOME/bin
+  export JAVA_OPTS=" -Djava.net.preferIPv4Stack=false -Djava.net.preferIPv6Addresses=true"
   echo "JAVA_HOME=$JAVA_HOME"
+  java -version
 
   export FLUTTER_KEYSTORE_ID=74840
   export FLUTTER_KEYSTORE_NAME=flutter-intellij-plugin-auth-token
--- tool/kokoro/test.sh
+++ tool/kokoro/test.sh
@@ -3,12 +3,6 @@
 source ./tool/kokoro/setup.sh
 setup
 
-curl https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_osx-x64_bin.tar.gz > ../java.tar.gz
-(cd ..; tar fx java.tar.gz)
-export JAVA_HOME=`pwd`/../jdk-11.0.2.jdk/Contents/Home
-export PATH=$PATH:$JAVA_HOME/bin
-echo "JAVA_HOME=$JAVA_HOME"
-
 (cd testData/sample_tests; echo "pub get `pwd`"; pub get --no-precompile)
 
 echo "kokoro test start"
