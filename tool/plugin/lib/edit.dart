8736a49949eaade2a50574cd54e90275ca455b03
Steve Messick <messick@google.com>
2022-04-11 15:45:12 -0700

    Fix typo

--- tool/plugin/lib/edit.dart
+++ tool/plugin/lib/edit.dart
@@ -63,7 +63,7 @@ List<EditCommand> editCommands = [
     path: 'flutter-idea/src/io/flutter/analytics/FlutterAnalysisServerListener.java',
     initial: '<@NotNull Analytics>',
     replacement: '<Analytics>',
-    versions: [AS.211', 'AS.212', '2021.3'],
+    versions: ['AS.211', 'AS.212', '2021.3'],
   ),
   Subst(
     path: 'flutter-idea/src/io/flutter/analytics/DartCompletionTimerListener.java',
