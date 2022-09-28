a3de6f501fd8731ed860b6e66cc4331a0aafb5a3
Steve Messick <messick@google.com>
2022-09-28 10:23:50 -0700

    Set JAVA_HOME from envar

--- tool/github.sh
+++ tool/github.sh
@@ -7,7 +7,7 @@
 # Fast fail the script on failures.
 set -e
 
-echo "JAVA_HOME=$JAVA_HOME"
+export JAVA_HOME=$JAVA_HOME_17_X64
 
 git clone --depth 1 https://github.com/flutter/flutter.git ../flutter
 export PATH="$PATH":`pwd`/../flutter/bin:`pwd`/../flutter/bin/cache/dart-sdk/bin
