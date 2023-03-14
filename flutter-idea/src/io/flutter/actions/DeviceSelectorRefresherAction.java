477d1da1ce7edd1ac633d16e9de83a000d989df6
Steve Messick <messick@google.com>
2023-03-14 09:56:26 -0700

    Update Refresh on BGT

--- flutter-idea/src/io/flutter/actions/DeviceSelectorRefresherAction.java
+++ flutter-idea/src/io/flutter/actions/DeviceSelectorRefresherAction.java
@@ -5,6 +5,7 @@
  */
 package io.flutter.actions;
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
@@ -24,6 +25,10 @@ public class DeviceSelectorRefresherAction extends AnAction {
     if (project != null) {
       DeviceService.getInstance(project).restart();
     }
+  }
+
+  public @NotNull ActionUpdateThread getActionUpdateThread() {
+    return ActionUpdateThread.BGT;
   }
 
   @Override
