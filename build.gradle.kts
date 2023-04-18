405c6999e18857b918aeca191ef32849e352f3e9
Steve Messick <messick@google.com>
2023-04-18 10:29:21 -0700

    Update versions in build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ buildscript {
 }
 
 plugins {
-  id("org.jetbrains.intellij") version "1.13.2"
+  id("org.jetbrains.intellij") version "1.13.3"
   id("org.jetbrains.kotlin.jvm") version "1.8.20-RC"
 }
 
