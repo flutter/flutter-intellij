b498a24b16c6e489e5df5a18e3b93eeae495d7b0
Alex Li <github@alexv525.com>
2023-04-27 10:25:25 +0800

    ✨ Handle symlinks on Windows for products

--- tool/plugin/lib/plugin.dart
+++ tool/plugin/lib/plugin.dart
@@ -705,6 +705,7 @@ abstract class ProductCommand extends Command {
   Future<int> run() async {
     await _initGlobals();
     await _initSpecs();
+    _handleSymlinksOnWindows();
     try {
       return await doit();
     } catch (ex, stack) {
@@ -737,6 +738,26 @@ abstract class ProductCommand extends Command {
       await specs[i].initChangeLog();
     }
     return specs.length;
+  }
+
+  void _handleSymlinksOnWindows() {
+    if (!Platform.isWindows) {
+      return;
+    }
+    const List<String> symlinks = [
+      r'flutter-idea\resources',
+      r'flutter-studio\resources',
+    ];
+    final currentPath = Directory.current.path;
+    for (final entityPath in symlinks) {
+      final path = '$currentPath\\$entityPath';
+      if (FileSystemEntity.isLinkSync(path)) {
+        continue;
+      }
+      final content = File(path).readAsStringSync();
+      File(path).deleteSync();
+      Link(path).createSync(content);
+    }
   }
 }
 
