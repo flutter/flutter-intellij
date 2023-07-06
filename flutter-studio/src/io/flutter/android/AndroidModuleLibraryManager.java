93eb2e890ac9f7e5f5dfaec6280ac53ef9ba1f4c
Steve Messick <messick@google.com>
2023-07-06 16:33:57 -0700

    Disable java indexing in AS canary

--- flutter-studio/src/io/flutter/android/AndroidModuleLibraryManager.java
+++ flutter-studio/src/io/flutter/android/AndroidModuleLibraryManager.java
@@ -343,7 +343,9 @@ public class AndroidModuleLibraryManager extends AbstractLibraryManager<AndroidM
         Method method = ReflectionUtil
           .getDeclaredMethod(ProjectManagerImpl.class, "initProject", Path.class, ProjectImpl.class, boolean.class, boolean.class,
                              Project.class, ProgressIndicator.class);
-        assert (method != null);
+        if (method == null) {
+          throw new IllegalAccessException(); // caught below
+        }
         try {
           method.invoke(null, path, this, true, true, null, null);
         }
