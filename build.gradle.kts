bd73695135aa6a15baa431a6f9a55d2102b667c7
Steve Messick <messick@google.com>
2023-07-12 16:30:04 -0700

    build

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,9 @@ buildscript {
 }
 
 plugins {
-  id("org.jetbrains.intellij") version "1.15.0"
-  id("org.jetbrains.kotlin.jvm") version "1.9.0"}
+  id("org.jetbrains.intellij") version "1.13.3"
+  id("org.jetbrains.kotlin.jvm") version "1.8.20-RC"
+}
 
 repositories {
   mavenLocal()
