172c772a565b2bdc5066673f279a32f5d40beac3
stevemessick <stevemessick@users.noreply.github.com>
2023-07-12 16:40:57 -0700

    Update Java, Gradle, plugins, and clean up (#6825)
    cc @AlexV525

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,8 @@ buildscript {
 }
 
 plugins {
-  id("org.jetbrains.intellij") version "1.13.3"
-  id("org.jetbrains.kotlin.jvm") version "1.8.20-RC"
+  id("org.jetbrains.intellij") version "1.15.0"
+  id("org.jetbrains.kotlin.jvm") version "1.9.0"
 }
 
 repositories {
@@ -93,6 +93,12 @@ intellij {
 tasks {
   buildSearchableOptions {
     enabled = false
+  }
+  prepareSandbox {
+    dependsOn(":flutter-idea:prepareSandbox")
+    if (ide == "android-studio") {
+      dependsOn(":flutter-studio:prepareSandbox")
+    }
   }
 }
 
