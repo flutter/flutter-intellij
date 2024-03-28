6f53d6fdb7668dfa3e0115e13b7a8d4816d48e6a
Helin Shiah <helinx@google.com>
2024-03-28 16:01:01 -0700

    Remove unused 2020 method

--- flutter-idea/src/io/flutter/jxbrowser/JxBrowserManager.java
+++ flutter-idea/src/io/flutter/jxbrowser/JxBrowserManager.java
@@ -337,44 +337,6 @@ public class JxBrowserManager {
     ProgressManager.getInstance().runProcessWithProgressAsynchronously(task, processIndicator);
   }
 
-  private void loadClasses2020(@NotNull String[] fileNames) {
-    final ClassLoader current = Thread.currentThread().getContextClassLoader();
-    try {
-      //noinspection ConstantConditions
-      Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
-      //noinspection ConstantConditions
-      for (String fileName : fileNames) {
-        final String fullPath = getFilePath(fileName);
-        try {
-          //noinspection ConstantConditions
-          fileUtils.loadClass(this.getClass().getClassLoader(), fullPath);
-        }
-        catch (Exception ex) {
-          LOG.info("Failed to load JxBrowser file", ex);
-          setStatusFailed(new InstallationFailedReason(FailureType.CLASS_LOAD_FAILED));
-          return;
-        }
-        LOG.info("Loaded JxBrowser file successfully: " + fullPath);
-      }
-      try {
-        final Class<?> clazz = Class.forName("com.teamdev.jxbrowser.browser.UnsupportedRenderingModeException");
-        final Constructor<?> constructor = clazz.getConstructor(RenderingMode.class);
-        constructor.newInstance(RenderingMode.HARDWARE_ACCELERATED);
-      }
-      catch (NoClassDefFoundError | ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-        LOG.info("Failed to find JxBrowser class: " + e.getMessage());
-        setStatusFailed(new InstallationFailedReason(FailureType.CLASS_NOT_FOUND));
-        return;
-      }
-    }
-    finally {
-      Thread.currentThread().setContextClassLoader(current);
-    }
-    analytics.sendEvent(ANALYTICS_CATEGORY, "installed");
-    status.set(JxBrowserStatus.INSTALLED);
-    installation.complete(JxBrowserStatus.INSTALLED);
-  }
-
   private void loadClasses(@NotNull String[] fileNames) {
     final List<Path> paths = new ArrayList<>();
     final ClassLoader current = Thread.currentThread().getContextClassLoader();
