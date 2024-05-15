9536817e680a91ce1e7c738420f098a87839f9f6
Jaime Wren <jwren@google.com>
2024-05-15 13:36:00 -0700

    Cleanup: remove unnecessary Java imports (#7431)

--- flutter-idea/src/io/flutter/module/FlutterModuleBuilder.java
+++ flutter-idea/src/io/flutter/module/FlutterModuleBuilder.java
@@ -56,7 +56,6 @@ import javax.swing.ComboBoxEditor;
 import javax.swing.Icon;
 import javax.swing.JComponent;
 
-import javaslang.collection.Array;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
