7b05f6c73da6a9cf62e36f6b8afdf83d5122b0d2
Jaime Wren <jwren@google.com>
2024-06-18 13:29:54 -0700

    Migrate PlatformsForm.kt to Kotlin UI DSL Version 2
    This resolves https://github.com/flutter/flutter-intellij/issues/7310

--- flutter-idea/src/io/flutter/module/settings/PlatformsForm.kt
+++ flutter-idea/src/io/flutter/module/settings/PlatformsForm.kt
@@ -6,66 +6,55 @@
 package io.flutter.module.settings
 
 import com.intellij.openapi.ui.DialogPanel
-import com.intellij.ui.layout.Cell
-import com.intellij.ui.layout.panel
+import com.intellij.ui.dsl.builder.*
+import com.intellij.ui.layout.ComponentPredicate
 import io.flutter.FlutterBundle
 import io.flutter.sdk.FlutterCreateAdditionalSettings
 import io.flutter.sdk.FlutterSdkChannel
 
-// This file needs to be migrated due to deprecation,
-// https://github.com/flutter/flutter-intellij/issues/7310
-class PlatformsForm() {
-
+class PlatformsForm {
   var channel: FlutterSdkChannel? = null
-  var configAndroid: Boolean = true
-  var configIos: Boolean = true
-  var configLinux: Boolean = true
-  var configMacos: Boolean = true
-  var configWeb: Boolean = true
-  var configWindows: Boolean = true
+  private var configAndroid: Boolean = true
+  private var configIos: Boolean = true
+  private var configLinux: Boolean = true
+  private var configMacos: Boolean = true
+  private var configWeb: Boolean = true
+  private var configWindows: Boolean = true
   var component: DialogPanel? = null
 
   fun panel(settings: FlutterCreateAdditionalSettings) =
     panel {
       row {
-        cell(isVerticalFlow = false) {
-          makeCheckBox(this, FlutterBundle.message("npw_platform_android"), settings.platformAndroidProperty, configAndroid)
-          makeCheckBox(this, FlutterBundle.message("npw_platform_ios"), settings.platformIosProperty, configIos)
-          makeCheckBox(this, FlutterBundle.message("npw_platform_linux"), settings.platformLinuxProperty, configLinux)
-          makeCheckBox(this, FlutterBundle.message("npw_platform_macos"), settings.platformMacosProperty, configMacos)
-          makeCheckBox(this, FlutterBundle.message("npw_platform_web"), settings.platformWebProperty, configWeb)
-          makeCheckBox(this, FlutterBundle.message("npw_platform_windows"), settings.platformWindowsProperty, configWindows)
-        }
+        makeCheckBox(FlutterBundle.message("npw_platform_android"), settings.platformAndroidProperty, configAndroid)
+        makeCheckBox(FlutterBundle.message("npw_platform_ios"), settings.platformIosProperty, configIos)
+        makeCheckBox(FlutterBundle.message("npw_platform_linux"), settings.platformLinuxProperty, configLinux)
+        makeCheckBox(FlutterBundle.message("npw_platform_macos"), settings.platformMacosProperty, configMacos)
+        makeCheckBox(FlutterBundle.message("npw_platform_web"), settings.platformWebProperty, configWeb)
+        makeCheckBox(FlutterBundle.message("npw_platform_windows"), settings.platformWindowsProperty, configWindows)
       }
       row {
         label(FlutterBundle.message("npw_platform_selection_help"))
       }
     }.apply { component = this }
 
-  private fun makeCheckBox(context: Cell,
-                           name: String,
-                           property: InitializeOnceBoolValueProperty,
-                           config: Boolean?) {
-    context.apply {
-      val wasSelected = config == true
-      property.initialize(wasSelected)
-      checkBox(name,
-               property.get(),
-               actionListener = { _, checkBox ->
-                 property.set(checkBox.isSelected)
-               }).apply {
-        val names: List<String> = listOfNotNull(
-          FlutterBundle.message("npw_platform_android"),
-          FlutterBundle.message("npw_platform_ios"),
-          FlutterBundle.message("npw_platform_web"),
-          FlutterBundle.message("npw_platform_windows"),
-          FlutterBundle.message("npw_platform_linux"),
-          FlutterBundle.message("npw_platform_macos"),
-        )
-        // TODO(jwren) for the 2024.1 roll I needed to comment out the following line, it should be resolved however when
-        //  https://github.com/flutter/flutter-intellij/issues/7310 is addressed.
-        // enabled(names.contains(name) || wasSelected)
-      }
-    }
+  private fun Row.makeCheckBox(
+    name: String,
+    property: InitializeOnceBoolValueProperty,
+    config: Boolean?
+  ) {
+    val wasSelected = config == true
+    property.initialize(wasSelected)
+
+    val names: List<String> = listOfNotNull(
+      FlutterBundle.message("npw_platform_android"),
+      FlutterBundle.message("npw_platform_ios"),
+      FlutterBundle.message("npw_platform_web"),
+      FlutterBundle.message("npw_platform_windows"),
+      FlutterBundle.message("npw_platform_linux"),
+      FlutterBundle.message("npw_platform_macos"),
+    )
+
+    checkBox(name).actionListener { _, button -> property.set(button.isSelected) }.selected(property.get())
+      .enabledIf(ComponentPredicate.fromValue(names.contains(name) || wasSelected))
   }
 }
