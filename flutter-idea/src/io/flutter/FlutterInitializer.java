5d838d4ee4d7abef4ab8ab006144674792c86a11
Helin Shiah <helinx@google.com>
2024-09-12 10:04:29 -0700

    Send theme changed event over DTD (#7647)
    Addresses https://github.com/flutter/flutter-intellij/issues/7626

--- flutter-idea/src/io/flutter/FlutterInitializer.java
+++ flutter-idea/src/io/flutter/FlutterInitializer.java
@@ -6,16 +6,22 @@
 package io.flutter;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.intellij.ProjectTopics;
 import com.intellij.ide.BrowserUtil;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManagerCore;
+import com.intellij.ide.ui.UISettingsListener;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.notification.*;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.colors.EditorColorsListener;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.module.Module;
@@ -25,16 +31,19 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.messages.MessageBusConnection;
 import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
+import com.jetbrains.lang.dart.ide.toolingDaemon.DartToolingDaemonService;
+import de.roderick.weberknecht.WebSocketException;
 import io.flutter.analytics.Analytics;
 import io.flutter.analytics.FlutterAnalysisServerListener;
 import io.flutter.analytics.ToolWindowTracker;
 import io.flutter.analytics.UnifiedAnalytics;
 import io.flutter.android.IntelliJAndroidSdk;
 import io.flutter.bazel.WorkspaceCache;
-import io.flutter.deeplinks.DeepLinksViewFactory;
+import io.flutter.dart.DtdUtils;
 import io.flutter.devtools.DevToolsExtensionsViewFactory;
-import io.flutter.devtools.DevToolsExtensionsViewService;
+import io.flutter.devtools.DevToolsUtils;
 import io.flutter.devtools.RemainingDevToolsViewFactory;
 import io.flutter.editor.FlutterSaveActionsManager;
 import io.flutter.logging.FlutterConsoleLogManager;
@@ -59,6 +68,10 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.event.HyperlinkEvent;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Runs actions after the project has started up and the index is up to date.
@@ -68,6 +81,7 @@ import java.util.UUID;
  * may run when a project is being imported.
  */
 public class FlutterInitializer implements StartupActivity {
+  private static final Logger LOG = Logger.getInstance(FlutterInitializer.class);
   private static final String analyticsClientIdKey = "io.flutter.analytics.clientId";
   private static final String analyticsOptOutKey = "io.flutter.analytics.optOut";
   private static final String analyticsToastShown = "io.flutter.analytics.toastShown";
@@ -75,6 +89,10 @@ public class FlutterInitializer implements StartupActivity {
   private static Analytics analytics;
 
   private boolean toolWindowsInitialized = false;
+
+  private boolean busSubscribed = false;
+
+  private @NotNull AtomicLong lastScheduledThemeChangeTime = new AtomicLong();
 
   @Override
   public void runActivity(@NotNull Project project) {
@@ -194,6 +212,9 @@ public class FlutterInitializer implements StartupActivity {
     // Set our preferred settings for the run console.
     FlutterConsoleLogManager.initConsolePreferences();
 
+    // Initialize notifications for theme changes.
+    setUpThemeChangeNotifications(project);
+
     setUpDtdAnalytics(project);
 
     // Initialize analytics.
@@ -253,6 +274,80 @@ public class FlutterInitializer implements StartupActivity {
       //unifiedAnalytics.manageConsent();
     });
     t1.start();
+  }
+
+  private void setUpThemeChangeNotifications(Project project) {
+    if (project == null) return;
+    FlutterSdk sdk = FlutterSdk.getFlutterSdk(project);
+    if (sdk == null || !sdk.getVersion().canUseDtd()) return;
+    Thread t1 = new Thread(() -> {
+      if (busSubscribed) return;
+      final MessageBusConnection connection = ApplicationManager.getApplication().getMessageBus().connect();
+      connection.subscribe(EditorColorsManager.TOPIC, (EditorColorsListener)scheme -> {
+        sendThemeChangedEvent(project);
+      });
+      connection.subscribe(UISettingsListener.TOPIC, (UISettingsListener)scheme -> {
+        sendThemeChangedEvent(project);
+      });
+      busSubscribed = true;
+    });
+    t1.start();
+  }
+
+  private void sendThemeChangedEvent(@NotNull Project project) {
+    // Debounce this request because the topic subscriptions can trigger multiple times (potentially from initial notification of change and
+    // also from application of change)
+
+    // Set the current time of this request
+    final long requestTime = System.currentTimeMillis();
+    lastScheduledThemeChangeTime.set(requestTime);
+
+    // Schedule event to be sent in a second if nothing more recent has come in.
+    Executors.newSingleThreadScheduledExecutor().schedule(() -> {
+      if (lastScheduledThemeChangeTime.get() != requestTime) {
+        // A more recent request has been set, so drop this request.
+        return;
+      }
+
+      final JsonObject params = new JsonObject();
+      params.addProperty("eventKind", "themeChanged");
+      params.addProperty("streamId", "Editor");
+      final JsonObject eventData = new JsonObject();
+      final DevToolsUtils utils = new DevToolsUtils();
+      eventData.addProperty("isDarkMode", Boolean.FALSE.equals(utils.getIsBackgroundBright()));
+      eventData.addProperty("backgroundColor", utils.getColorHexCode());
+      eventData.addProperty("fontSize", utils.getFontSize().intValue());
+      params.add("eventData", eventData);
+
+      try {
+        final DtdUtils dtdUtils = new DtdUtils();
+        final DartToolingDaemonService dtdService = dtdUtils.readyDtdService(project).get();
+        if (dtdService == null) {
+          LOG.error("Unable to send theme changed event because DTD service is null");
+          return;
+        }
+
+        dtdService.sendRequest("postEvent", params, false, object -> {
+                                 JsonObject result = object.getAsJsonObject("result");
+                                 if (result == null) {
+                                   LOG.error("Theme changed event returned null result");
+                                   return;
+                                 }
+                                 JsonPrimitive type = result.getAsJsonPrimitive("type");
+                                 if (type == null) {
+                                   LOG.error("Theme changed event result type is null");
+                                   return;
+                                 }
+                                 if (!"Success".equals(type.getAsString())) {
+                                   LOG.error("Theme changed event result: " + type.getAsString());
+                                 }
+                               }
+        );
+      }
+      catch (WebSocketException | InterruptedException | ExecutionException e) {
+        LOG.error("Unable to send theme changed event", e);
+      }
+    }, 1, TimeUnit.SECONDS);
   }
 
   private static void enableAnalytics(@NotNull Project project) {
