9ead456aa7da7f65ef40dbd708093b08ff50ece2
Jaime Wren <jwren@google.com>
2024-10-15 10:36:47 -0700

    Continued removal of deprecated or dead code
    Continuation of https://github.com/flutter/flutter-intellij/pull/7706

--- flutter-idea/src/io/flutter/preview/ModelUtils.java
+++ flutter-idea/src/io/flutter/preview/ModelUtils.java
@@ -23,28 +23,4 @@ public class ModelUtils {
     return StringUtil.equals("build", element.getName()) &&
            StringUtil.startsWith(element.getParameters(), "(BuildContext ");
   }
-
-  public static boolean containsBuildMethod(FlutterOutline outline) {
-    final Element element = outline.getDartElement();
-    if (element == null) {
-      return false;
-    }
-
-    if (isBuildMethod(element)) {
-      return true;
-    }
-
-    final List<FlutterOutline> children = outline.getChildren();
-    if (children == null) {
-      return false;
-    }
-
-    for (FlutterOutline child : children) {
-      if (containsBuildMethod(child)) {
-        return true;
-      }
-    }
-
-    return false;
-  }
 }
