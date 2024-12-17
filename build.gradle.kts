281f65d97b75963c2b32cd8cdfdc5c183d9133b1
Jaime Wren <jwren@google.com>
2024-12-17 14:25:17 -0800

    Update the org.jetbrains.kotlin.jvm version to 2.1.0, from 2.0.0
    https://plugins.gradle.org/plugin/org.jetbrains.kotlin.jvm/2.1.0
    
    Additionally, a TODO was added for org.jetbrains.intellij.platform and version 2023.3 of the platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,8 +31,10 @@ repositories {
 plugins {
   // https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
   // https://github.com/JetBrains/intellij-platform-gradle-plugin/releases
+  // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.jvm
+  // TODO(jwren) "2.2.0" can't be used to build 2023.3, as soon as we don't support this version, update the version here and elsewhere:
   id("org.jetbrains.intellij.platform") version "2.1.0"
-  id("org.jetbrains.kotlin.jvm") version "2.0.0"
+  id("org.jetbrains.kotlin.jvm") version "2.1.0"
 }
 
 val flutterPluginVersion = providers.gradleProperty("flutterPluginVersion").get()
