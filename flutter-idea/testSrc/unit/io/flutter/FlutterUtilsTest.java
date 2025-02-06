dc330c55008a34c167566226f71ea0a08975590f
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2025-02-05 16:18:42 -0800

    More clean up

--- flutter-idea/testSrc/unit/io/flutter/FlutterUtilsTest.java
+++ flutter-idea/testSrc/unit/io/flutter/FlutterUtilsTest.java
@@ -11,7 +11,9 @@ import org.junit.Test;
 
 import static io.flutter.FlutterUtils.isValidDartIdentifier;
 import static io.flutter.FlutterUtils.isValidPackageName;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 public class FlutterUtilsTest {
 
