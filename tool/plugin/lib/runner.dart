6afae9dbdb4f19e5b4ee3b5cb6cd53a6ab8c54a0
Helin Shiah <helinx@google.com>
2025-05-06 12:18:45 -0700

    Revert deletion of deploy command to fix dev build (#8155)
    I think this should fix the dev build, which stopped uploading back in
    February. It uses the command `./bin/plugin deploy --channel=dev` (see
    `tool/kokoro/deploy.sh`). The deletion happened in
    https://github.com/flutter/flutter-intellij/pull/7929.
    
    I can't test it from an internal command until these changes are in the
    main branch though.
    
    It could be we no longer need the dev build, but this seems like an easy
    enough change.

--- tool/plugin/lib/runner.dart
+++ tool/plugin/lib/runner.dart
@@ -14,7 +14,7 @@ class BuildCommandRunner extends CommandRunner<int> {
   BuildCommandRunner()
       : super(
           'plugin',
-          'A script to build and test the Flutter IntelliJ plugin.',
+          'A script to build, test, and deploy the Flutter IntelliJ plugin.',
         ) {
     argParser.addOption(
       'release',
@@ -79,9 +79,8 @@ org.gradle.parallel=true
 org.gradle.jvmargs=-Xms1024m -Xmx4048m
 ''';
     final propertiesFile = File("$rootPath/gradle.properties");
-    final source = propertiesFile.existsSync()
-        ? propertiesFile.readAsStringSync()
-        : null;
+    final source =
+        propertiesFile.existsSync() ? propertiesFile.readAsStringSync() : null;
     propertiesFile.writeAsStringSync(contents);
     int result;
     // Using the Gradle daemon causes a strange problem.
