1169f5e8a3adeb1e83c2a6d83231cd87d3ed5d1c
Phil Quitslund <pq@users.noreply.github.com>
2025-05-13 15:26:42 -0700

    [perf] create action presentations lazily (#8187)
    Stops eagerly creating action presentations (allocating resources
    prematurely and possibly unnecessarily) in favor of the preferred
    registration in plugin metadata that will only get loaded as-needed.
    
    From the inspection:
    
    > Reports any actions that are registered in the plugin.xml file and
    instantiate the com.intellij.openapi.actionSystem.Presentation object in
    their constructors.
    
    > Any of the constructors of AnAction with parameters instantiate the
    Presentation object. However, instantiating the Presentation object in
    constructor results in allocating resources, which may not be necessary.
    Instead of creating an instance of Presentation that stores text,
    description, or icon, it is more efficient to utilize no-argument
    constructors of
    AnAction and other base classes and follow the convention for setting
    the text, description, and icon in plugin.xml. The IDE will load text,
    description, and icon only when the action is actually displayed in the
    UI.
    
    > The convention for setting the text, description, and icon is as
    follows:
    Set the id attribute for the action in the plugin.xml file.
    Optionally, set the icon attribute if an icon is needed.
    
    Our actions:
    
    
    ![image](https://github.com/user-attachments/assets/7eb77ea8-006b-44aa-afcf-e8c3784f52b3)
    
    The only risk is in ensuring that the templated menu text is still
    getting properly filled in, and I confirmed that:
    
    
    ![image](https://github.com/user-attachments/assets/00697fdf-9a32-4204-ac49-a73c32cdf0c2)
    
    
    
    
    
    ---
    
    - [x] I’ve reviewed the contributor guide and applied the relevant
    portions to this PR.
    
    <details>
      <summary>Contribution guidelines:</summary><br>
    
    - See our [contributor
    guide]([https://github.com/dart-lang/sdk/blob/main/CONTRIBUTING.md](https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview)
    for general expectations for PRs.
    - Larger or significant changes should be discussed in an issue before
    creating a PR.
    - Dart contributions to our repos should follow the [Dart style
    guide](https://dart.dev/guides/language/effective-dart) and use `dart
    format`.
    - Java and Kotlin contributions should strive to follow Java and Kotlin
    best practices
    ([discussion](https://github.com/flutter/flutter-intellij/issues/8098)).
    </details>

--- flutter-idea/src/io/flutter/actions/RunReleaseFlutterApp.java
+++ flutter-idea/src/io/flutter/actions/RunReleaseFlutterApp.java
@@ -5,17 +5,13 @@
  */
 package io.flutter.actions;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.wm.ToolWindowId;
-import io.flutter.FlutterBundle;
 import io.flutter.run.FlutterLaunchMode;
 
 public class RunReleaseFlutterApp extends RunFlutterAction {
-  public static final String TEXT = FlutterBundle.message("app.release.action.text");
-  public static final String DESCRIPTION = FlutterBundle.message("app.release.action.description");
   private static final String TEXT_DETAIL_MSG_KEY = "app.release.config.action.text";
 
   public RunReleaseFlutterApp() {
-    super(TEXT, TEXT_DETAIL_MSG_KEY, DESCRIPTION, AllIcons.Actions.Execute, FlutterLaunchMode.RELEASE, ToolWindowId.RUN);
+    super(TEXT_DETAIL_MSG_KEY, FlutterLaunchMode.RELEASE, ToolWindowId.RUN);
   }
 }
