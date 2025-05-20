ceb5fe4c8af7203ad5d633758c1e3d2abfa86c73
Phil Quitslund <pq@users.noreply.github.com>
2025-05-20 12:08:43 -0700

    [CQ] migrate post-startup `Startup` to `Project` activities (#8200)
    `StartupActivity` is marked as obsolete with `ProjectActivity`
    identified as the path forward.
    
    This introduces a `FlutterProjectActivity` that does some rudimentary
    error handling as well.
    
    See https://github.com/flutter/flutter-intellij/issues/7718 and
    https://github.com/flutter/flutter-intellij/issues/8100
    
    ---
    
    - [x] I’ve reviewed the contributor guide and applied the relevant
    portions to this PR.
    
    <details>
      <summary>Contribution guidelines:</summary><br>
    
    - See our [contributor
    guide]([https://github.com/dart-lang/sdk/blob/main/CONTRIBUTING.md](https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview)
    for general expectations for PRs.
    - Larger or significant changes should be discussed in an issue before
    creating a PR.
    - Dart contributions to our repos should follow the [Dart style
    guide](https://dart.dev/guides/language/effective-dart) and use `dart
    format`.
    - Java and Kotlin contributions should strive to follow Java and Kotlin
    best practices
    ([discussion](https://github.com/flutter/flutter-intellij/issues/8098)).
    </details>

--- flutter-idea/src/io/flutter/ProjectOpenActivity.java
+++ flutter-idea/src/io/flutter/ProjectOpenActivity.java
@@ -20,7 +20,6 @@ import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectType;
 import com.intellij.openapi.project.ProjectTypeService;
-import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.ui.Messages;
 import icons.FlutterIcons;
 import io.flutter.bazel.WorkspaceCache;
@@ -40,14 +39,14 @@ import java.util.Collection;
  *
  * @see FlutterInitializer for actions that run later.
  */
-public class ProjectOpenActivity implements StartupActivity, DumbAware {
+public class ProjectOpenActivity extends FlutterProjectActivity implements DumbAware {
   public static final ProjectType FLUTTER_PROJECT_TYPE = new ProjectType("io.flutter");
 
   public ProjectOpenActivity() {
   }
 
   @Override
-  public void runActivity(@NotNull Project project) {
+  public void executeProjectStartup(@NotNull Project project) {
     // TODO(helinx): We don't have a good way to check whether a Bazel project is using Flutter. Look into whether we can
     // build a better Flutter Bazel check into `declaresFlutter` so we don't need the second condition.
     if (!FlutterModuleUtils.declaresFlutter(project) && !WorkspaceCache.getInstance(project).isBazel()) {
