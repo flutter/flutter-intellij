1b2fa45792779cfb3a346b733a93f9df7a646984
Phil Quitslund <pq@users.noreply.github.com>
2025-07-23 08:56:50 -0700

    [CQ] remove experimental `BadgeIcon` dependency (#8365)
    The class `com.intellij.ui.BadgeIcon` is marked `@experimental` and is
    flagged by the verifier:
    
    ```
      Experimental API usages (4): 
          #Experimental API class com.intellij.ui.BadgeIcon reference
              Experimental API class com.intellij.ui.BadgeIcon is referenced in io.flutter.toolwindow.ToolWindowBadgeUpdater.lambda$updateBadgedIcon$1(ToolWindow) : void. This class can be changed in a future release leading to incompatibilities
              Experimental API class com.intellij.ui.BadgeIcon is referenced in io.flutter.toolwindow.ToolWindowBadgeUpdater.lambda$updateBadgedIcon$0(ToolWindow) : void. This class can be changed in a future release leading to incompatibilities
          #Experimental API constructor com.intellij.ui.BadgeIcon.<init>(Icon, Paint) invocation
              Experimental API constructor com.intellij.ui.BadgeIcon.<init>(javax.swing.Icon icon, java.awt.Paint paint) is invoked in io.flutter.toolwindow.ToolWindowBadgeUpdater.lambda$updateBadgedIcon$1(ToolWindow) : void. This constructor can be changed in a future release leading to incompatibilities
              Experimental API constructor com.intellij.ui.BadgeIcon.<init>(javax.swing.Icon icon, java.awt.Paint paint) is invoked in io.flutter.toolwindow.ToolWindowBadgeUpdater.lambda$updateBadgedIcon$0(ToolWindow) : void. This constructor can be changed in a future release leading to incompatibilities
    ```
    
    This change introduces a lightweight replacement, allowing us to remove
    the dependency and enable
    `VerifyPluginTask.FailureLevel.EXPERIMENTAL_API_USAGES` failure checking
    in the verifier (see:
    https://github.com/flutter/flutter-intellij/issues/8361).
    
    ---
    Behavior is preserved w/ the replacement:
    
    Default:
    
    <img width="1056" height="76" alt="image"
    src="https://github.com/user-attachments/assets/ecbe93f7-6f71-4862-abda-49434cbea405"
    />
    
    
    with badge:
    
    <img width="1124" height="84" alt="image"
    src="https://github.com/user-attachments/assets/ddda5bc5-6e79-4866-b2a5-07d1a4d466b1"
    />
    
    
    
    
    ---
    
    - [x] I’ve reviewed the contributor guide and applied the relevant
    portions to this PR.
    
    <details>
      <summary>Contribution guidelines:</summary><br>
    
    - See our [contributor
    guide]([https://github.com/dart-lang/sdk/blob/main/CONTRIBUTING.md](https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview)
    for general expectations for PRs.
    - Larger or significant changes should be discussed in an issue before
    creating a PR.
    - Dart contributions to our repos should follow the [Dart style
    guide](https://dart.dev/guides/language/effective-dart) and use `dart
    format`.
    - Java and Kotlin contributions should strive to follow Java and Kotlin
    best practices
    ([discussion](https://github.com/flutter/flutter-intellij/issues/8098)).
    </details>

--- src/io/flutter/toolwindow/ToolWindowBadgeUpdater.java
+++ src/io/flutter/toolwindow/ToolWindowBadgeUpdater.java
@@ -10,22 +10,20 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowId;
 import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.ui.BadgeIcon;
 import io.flutter.run.common.RunMode;
 import io.flutter.run.daemon.FlutterApp;
 
-import java.awt.Color;
-import java.awt.Paint;
+import javax.swing.*;
+import java.awt.*;
 import java.util.Objects;
-import javax.swing.Icon;
 
 public class ToolWindowBadgeUpdater {
-  public static final Paint BADGE_PAINT = Color.decode("#5ca963");
+  public static final Color BADGE_PAINT = Color.decode("#5ca963");
 
   /**
    * Updates the tool window icons for RUN or DEBUG mode with a green badge.
    *
-   * @param app The FlutterApp instance running in a given mode.
+   * @param app     The FlutterApp instance running in a given mode.
    * @param project The current IntelliJ project context.
    */
   public static void updateBadgedIcon(FlutterApp app, Project project) {
@@ -33,24 +31,60 @@ public class ToolWindowBadgeUpdater {
     final ToolWindow runToolWindow = manager.getToolWindow(ToolWindowId.RUN);
     final ToolWindow debugToolWindow = manager.getToolWindow(ToolWindowId.DEBUG);
 
-    if(Objects.requireNonNull(app).getMode() == RunMode.RUN) {
+    if (Objects.requireNonNull(app).getMode() == RunMode.RUN) {
       if (runToolWindow != null) {
         manager.invokeLater(() -> {
           Icon baseIcon = AllIcons.Toolwindows.ToolWindowRun;
           BadgeIcon iconWithBadge = new BadgeIcon(baseIcon, BADGE_PAINT);
-
           runToolWindow.setIcon(iconWithBadge);
         });
       }
     }
-    else if(app.getMode() == RunMode.DEBUG) {
+    else if (app.getMode() == RunMode.DEBUG) {
       manager.invokeLater(() -> {
         Icon baseIcon = AllIcons.Toolwindows.ToolWindowDebugger;
         BadgeIcon iconWithBadge = new BadgeIcon(baseIcon, BADGE_PAINT);
-
         Objects.requireNonNull(debugToolWindow).setIcon(iconWithBadge);
       });
     }
+  }
 
+  private static class BadgeIcon implements Icon {
+    private final Icon baseIcon;
+    private final Color overlayColor;
+    private static final float alpha = 1.0F;
+
+    public BadgeIcon(Icon baseIcon, Color overlayColor) {
+      this.baseIcon = baseIcon;
+      this.overlayColor = overlayColor;
+    }
+
+    @Override
+    public void paintIcon(Component c, Graphics g, int x, int y) {
+      baseIcon.paintIcon(c, g, x, y);
+
+      Graphics2D g2d = (Graphics2D)g.create();
+      try {
+        g2d.translate(x, y);
+
+        g2d.setComposite(AlphaComposite.SrcOver.derive(alpha));
+
+        g2d.setColor(overlayColor);
+        g2d.fillRect(0, 0, getIconWidth(), getIconHeight());
+      }
+      finally {
+        g2d.dispose();
+      }
+    }
+
+    @Override
+    public int getIconWidth() {
+      return baseIcon.getIconWidth();
+    }
+
+    @Override
+    public int getIconHeight() {
+      return baseIcon.getIconHeight();
+    }
   }
 }
