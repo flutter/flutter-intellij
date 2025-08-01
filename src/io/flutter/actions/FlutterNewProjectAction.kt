43994c8677c7bfd759d6729c14c28a91eb3a9498
Jaime Wren <jwren@google.com>
2025-08-01 10:39:39 -0700

    Fix the New Flutter Project action from hanging
    This resolves https://github.com/flutter/flutter-intellij/issues/8310

--- src/io/flutter/actions/FlutterNewProjectAction.kt
+++ src/io/flutter/actions/FlutterNewProjectAction.kt
@@ -3,45 +3,52 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-package io.flutter.actions;
+package io.flutter.actions
 
-import com.intellij.ide.impl.NewProjectUtil;
-import com.intellij.ide.projectWizard.NewProjectWizard;
-import com.intellij.openapi.actionSystem.ActionUpdateThread;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
-import com.intellij.openapi.wm.impl.welcomeScreen.NewWelcomeScreen;
-import io.flutter.FlutterBundle;
-import io.flutter.FlutterUtils;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.ide.impl.createNewProjectAsync
+import com.intellij.ide.projectWizard.NewProjectWizard
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.roots.ui.configuration.ModulesProvider
+import com.intellij.openapi.wm.impl.welcomeScreen.NewWelcomeScreen
+import io.flutter.FlutterBundle
+import io.flutter.FlutterUtils
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-public class FlutterNewProjectAction extends AnAction implements DumbAware {
-
-  public FlutterNewProjectAction() {
-    super(FlutterBundle.message("action.new.project.title"));
-  }
-
-  @Override
-  public void update(@NotNull AnActionEvent e) {
+/**
+ * This classes was re-implemented with Kotlin from FlutterNewProjectAction.java to resolve the New Flutter Project action from hanging,
+ * https://github.com/flutter/flutter-intellij/issues/8310.
+ *
+ * See https://github.com/JetBrains/intellij-community/blob/master/java/idea-ui/src/com/intellij/ide/impl/NewProjectUtil.kt
+ */
+class FlutterNewProjectAction : AnAction(FlutterBundle.message("action.new.project.title")), DumbAware {
+  override fun update(e: AnActionEvent) {
     if (NewWelcomeScreen.isNewWelcomeScreen(e)) {
-      e.getPresentation().setText(FlutterBundle.message("welcome.new.project.compact"));
+      e.presentation.setText(FlutterBundle.message("welcome.new.project.compact"))
     }
   }
 
-  @Override
-  public void actionPerformed(@NotNull AnActionEvent e) {
+  @OptIn(DelicateCoroutinesApi::class)
+  override fun actionPerformed(e: AnActionEvent) {
     if (FlutterUtils.isAndroidStudio()) {
-      System.setProperty("studio.projectview", "true");
+      System.setProperty("studio.projectview", "true")
     }
-    NewProjectWizard wizard = new NewProjectWizard(null, ModulesProvider.EMPTY_MODULES_PROVIDER, null);
-    NewProjectUtil.createNewProject(wizard);
+
+    GlobalScope.launch {
+      val wizard = withContext(Dispatchers.EDT) {
+        NewProjectWizard(null, ModulesProvider.EMPTY_MODULES_PROVIDER, null)
+      }
+      createNewProjectAsync(wizard)
+    }
+
   }
 
-  @Override
-  @NotNull
-  public ActionUpdateThread getActionUpdateThread() {
-    return ActionUpdateThread.BGT;
-  }
+  override fun getActionUpdateThread() = ActionUpdateThread.BGT
 }
