69e52a4ca8db82c8c2d40c5b71522bb889f1bd29
Helin Shiah <helinx@google.com>
2025-08-13 15:26:02 -0700

    Add verify and test steps to dev build (#8455)
    I can't test that these work without pointing kokoro to a different
    branch, so I figure I'll just run a test after this is committed to main
    branch.

--- tool/kokoro/deploy.sh
+++ tool/kokoro/deploy.sh
@@ -3,6 +3,21 @@
 source ./tool/kokoro/setup.sh
 setup
 
+echo "kokoro verify start"
+
+./gradlew verifyPluginProjectConfiguration
+./gradlew verifyPluginStructure
+./gradlew verifyPluginSignature
+./gradlew verifyPlugin
+
+echo "kokoro verify finished"
+
+echo "kokoro test start"
+
+./gradlew test
+
+echo "kokoro test finished"
+
 echo "kokoro build start"
 
 ./gradlew buildPlugin -Pdev-version=88.0
