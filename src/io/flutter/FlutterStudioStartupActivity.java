efce31546971f829717ea7a75f2c4df2dfe262e1
Helin Shiah <helinx@google.com>
2025-10-09 09:41:52 -0700

    Open project view for flutter projects

--- src/io/flutter/FlutterStudioStartupActivity.java
+++ src/io/flutter/FlutterStudioStartupActivity.java
@@ -5,19 +5,32 @@
  */
 package io.flutter;
 
+import com.intellij.ide.projectView.ProjectView;
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
 import com.intellij.util.concurrency.AppExecutorUtil;
 import io.flutter.android.AndroidModuleLibraryManager;
 import io.flutter.dart.FlutterDartAnalysisServer;
+import io.flutter.logging.PluginLogger;
 import io.flutter.settings.FlutterSettings;
-import io.flutter.utils.AddToAppUtils;
-import io.flutter.utils.AndroidUtils;
-import io.flutter.utils.GradleUtils;
+import io.flutter.utils.*;
 import org.jetbrains.annotations.NotNull;
 
 public class FlutterStudioStartupActivity extends FlutterProjectActivity {
+  private static @NotNull Logger LOG = PluginLogger.createLogger(FlutterStudioStartupActivity.class);
+  private static final String IS_FIRST_OPEN_KEY = "io.flutter.project.isFirstOpen";
+  private static final String PROJECT_VIEW_ID = "ProjectPane";
+
   private static Void doNonBlockingStartup(@NotNull Project project) {
+    LOG.info("Project " + project.getName() + ": Executing Studio startup");
+
     if (AndroidUtils.isAndroidProject(project)) {
       GradleUtils.addGradleListeners(project);
     }
@@ -40,5 +53,25 @@ public class FlutterStudioStartupActivity extends FlutterProjectActivity {
   public void executeProjectStartup(@NotNull Project project) {
     ReadAction.nonBlocking(() -> doNonBlockingStartup(project)).expireWith(FlutterDartAnalysisServer.getInstance(project))
       .submit(AppExecutorUtil.getAppExecutorService());
+
+    if (!FlutterModuleUtils.declaresFlutter(project)) {
+      LOG.info("Project " + project.getName() + ": does not declare Flutter; exiting Studio startup before setting project view");
+      return;
+    }
+
+    // Set project tool window to show the project on first open (Android Studio sometimes opens android view instead).
+    PropertiesComponent properties = PropertiesComponent.getInstance(project);
+    if (properties == null) return;
+    if (!properties.isValueSet(IS_FIRST_OPEN_KEY)) {
+      OpenApiUtils.safeInvokeLater(() -> {
+        ProjectView projectView = ProjectView.getInstance(project);
+        if (projectView == null) return;
+        if (projectView.getPaneIds().contains(PROJECT_VIEW_ID)) {
+          LOG.info("Project " + project.getName() + ": Setting project tool window to be project view");
+          projectView.changeView(PROJECT_VIEW_ID);
+        }
+      });
+      properties.setValue(IS_FIRST_OPEN_KEY, "true");
+    }
   }
 }
