58d432e489b000384669362d9f9d057f8c4542ef
Helin Shiah <helinx@google.com>
2025-10-10 08:49:32 -0700

    Add comments and clean up messages

--- src/io/flutter/FlutterStudioStartupActivity.java
+++ src/io/flutter/FlutterStudioStartupActivity.java
@@ -28,7 +28,7 @@ public class FlutterStudioStartupActivity extends FlutterProjectActivity {
   private static final String PROJECT_VIEW_ID = "ProjectPane";
 
   private static Void doNonBlockingStartup(@NotNull Project project) {
-    LOG.info("Project " + project.getName() + ": Executing Studio startup");
+    LOG.info("Project " + project.getName() + ": Executing non-blocking Studio startup");
 
     if (AndroidUtils.isAndroidProject(project)) {
       GradleUtils.addGradleListeners(project);
@@ -54,11 +54,15 @@ public class FlutterStudioStartupActivity extends FlutterProjectActivity {
       .submit(AppExecutorUtil.getAppExecutorService());
 
     if (!FlutterModuleUtils.declaresFlutter(project)) {
-      LOG.info("Project " + project.getName() + ": does not declare Flutter; exiting Studio startup before setting project view");
+      LOG.info("Project " + project.getName() + ": does not declare Flutter; exiting Studio startup before checking project view");
       return;
     }
 
     // Set project tool window to show the project on first open (Android Studio sometimes opens android view instead).
+    // See https://github.com/flutter/flutter-intellij/issues/8556.
+    // Note: After showing the project view, subsequent opens should go to the project view also. However, if the android subdirectory is
+    // opened, it seems the full project settings will be modified to open to the Android view instead. See
+    // https://github.com/flutter/flutter-intellij/pull/8573 for more details.
     PropertiesComponent properties = PropertiesComponent.getInstance(project);
     if (properties == null) return;
     if (!properties.isValueSet(IS_FIRST_OPEN_KEY)) {
@@ -66,7 +70,7 @@ public class FlutterStudioStartupActivity extends FlutterProjectActivity {
         ProjectView projectView = ProjectView.getInstance(project);
         if (projectView == null) return;
         if (projectView.getPaneIds().contains(PROJECT_VIEW_ID)) {
-          LOG.info("Project " + project.getName() + ": Setting project tool window to be project view");
+          LOG.info("Project " + project.getName() + ": Setting project tool window to be project view on first open");
           projectView.changeView(PROJECT_VIEW_ID);
         }
       });
