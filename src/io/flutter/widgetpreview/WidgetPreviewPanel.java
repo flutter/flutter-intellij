810aabeaaf7ab48ab549db9236776ec125551bd1
Helin Shiah <helinx@google.com>
2025-11-04 10:58:59 -0800

    Send DTD and DevTools URI to widget preview (#8599)
    Follow up to https://github.com/flutter/flutter-intellij/pull/8595
    
    At the moment I don't see consequences to having or not having the DTD
    connection, but I expect that in the future, the widget preview will
    respond to theme change updates over DTD. (and potentially other
    things?)
    
    Based on the conversation in
    https://github.com/flutter/flutter-intellij/pull/8595#discussion_r2482336086,
    the DevTools URI also does not have a visible consequence, but it should
    prevent flutter tools from starting its own DevTools instance when
    widget-preview is run.

--- src/io/flutter/widgetpreview/WidgetPreviewPanel.java
+++ src/io/flutter/widgetpreview/WidgetPreviewPanel.java
@@ -7,18 +7,22 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.colors.EditorColorsListener;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
-import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.util.messages.MessageBusConnection;
+import com.jetbrains.lang.dart.ide.toolingDaemon.DartToolingDaemonService;
 import icons.FlutterIcons;
 import io.flutter.FlutterBundle;
 import io.flutter.FlutterUtils;
+import io.flutter.dart.DtdUtils;
+import io.flutter.devtools.DevToolsUrl;
 import io.flutter.devtools.DevToolsUtils;
 import io.flutter.logging.PluginLogger;
 import io.flutter.pub.PubRoot;
+import io.flutter.run.daemon.DevToolsInstance;
+import io.flutter.run.daemon.DevToolsService;
 import io.flutter.sdk.FlutterCommand;
 import io.flutter.sdk.FlutterSdk;
 import io.flutter.sdk.FlutterSdkVersion;
@@ -27,8 +31,8 @@ import io.flutter.utils.MostlySilentColoredProcessHandler;
 import io.flutter.utils.OpenApiUtils;
 import io.flutter.view.BrowserUrlProvider;
 import io.flutter.view.EmbeddedTab;
-import io.flutter.view.WidgetPreviewUrlProvider;
 import io.flutter.view.ViewUtils;
+import io.flutter.view.WidgetPreviewUrlProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -37,6 +41,8 @@ import java.awt.BorderLayout;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
@@ -67,7 +73,7 @@ public class WidgetPreviewPanel extends SimpleToolWindowPanel implements Disposa
   }
 
   private void startWidgetPreview() {
-    ApplicationManager.getApplication().executeOnPooledThread(() -> {
+    OpenApiUtils.safeExecuteOnPooledThread(() -> {
       try {
         // Check versioning of Flutter SDK.
         FlutterSdk sdk = FlutterSdk.getFlutterSdk(project);
@@ -102,7 +108,9 @@ public class WidgetPreviewPanel extends SimpleToolWindowPanel implements Disposa
         }
 
         boolean isVerboseMode = FlutterSettings.getInstance().isVerboseLogging();
-        final FlutterCommand command = sdk.widgetPreview(root, isVerboseMode);
+        final String dtdUri = getDtdUri();
+        final String devToolsUri = getDevToolsUri();
+        final FlutterCommand command = sdk.widgetPreview(root, isVerboseMode, dtdUri, devToolsUri);
         LOG.info(command.getDisplayCommand());
 
         final ProcessHandler handler = new MostlySilentColoredProcessHandler(command.createGeneralCommandLine(project));
@@ -118,6 +126,43 @@ public class WidgetPreviewPanel extends SimpleToolWindowPanel implements Disposa
         throw new RuntimeException(e);
       }
     });
+  }
+
+  private @Nullable String getDevToolsUri() {
+    try {
+      final CompletableFuture<DevToolsInstance> devToolsFuture = DevToolsService.getInstance(project).getDevToolsInstance();
+      if (devToolsFuture == null) {
+        LOG.error("DevTools future is null.");
+        return null;
+      }
+
+      final DevToolsInstance instance = devToolsFuture.get(30, TimeUnit.SECONDS);
+      if (instance == null) {
+        LOG.error("DevTools instance is null.");
+        return null;
+      }
+      return new DevToolsUrl.Builder().setDevToolsHost(instance.host()).setDevToolsPort(instance.port()).build().getUrlString();
+    }
+    catch (InterruptedException | java.util.concurrent.ExecutionException | TimeoutException e) {
+      LOG.error("DevTools service failed: ", e);
+    }
+    return null;
+  }
+
+  private @Nullable String getDtdUri() {
+    try {
+      final DartToolingDaemonService dtd = new DtdUtils().readyDtdService(project).get(30, TimeUnit.SECONDS);
+      if (dtd == null) {
+        LOG.error("DTD service is null.");
+        return null;
+      }
+
+      return dtd.getUri();
+    }
+    catch (TimeoutException | java.util.concurrent.ExecutionException | InterruptedException e) {
+      LOG.error("DTD service is not available after 30 seconds.", e);
+    }
+    return null;
   }
 
   // This is intended for the first time we load the panel - save the URL and listen for changes.
@@ -142,19 +187,18 @@ public class WidgetPreviewPanel extends SimpleToolWindowPanel implements Disposa
     });
   }
 
+  // TODO(https://github.com/flutter/flutter/issues/177945): Ideally widget preview would change colors based on theme changes events,
+  //  which we already send for the DevTools panels. If this is implemented then we can remove this listening code.
   private void listenForReload() {
     MessageBusConnection connection = project.getMessageBus().connect();
-    connection.subscribe(EditorColorsManager.TOPIC, new EditorColorsListener() {
-      @Override
-      public void globalSchemeChange(@Nullable EditorColorsScheme scheme) {
-        if (urlProvider == null) {
-          return;
-        }
+    connection.subscribe(EditorColorsManager.TOPIC, (EditorColorsListener)scheme -> {
+      if (urlProvider == null) {
+        return;
+      }
 
-        final boolean changed = urlProvider.maybeUpdateColor();
-        if (changed) {
-          loadUrl(urlProvider);
-        }
+      final boolean changed = urlProvider.maybeUpdateColor();
+      if (changed) {
+        loadUrl(urlProvider);
       }
     });
     Disposer.register(toolWindow.getDisposable(), connection);
