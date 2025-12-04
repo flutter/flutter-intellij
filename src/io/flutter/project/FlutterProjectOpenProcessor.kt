b7e4ec1b194b317e9ef5dcb7b4cea2e58869a20d
Jaime Wren <jwren@google.com>
2025-12-03 23:34:47 -0800

    Fix IllegalStateException in ProjectOpenProcessor
    Migrate [FlutterProjectOpenProcessor](cci:2://file:///usr/local/google/home/jwren/src/flutter-intellij/src/io/flutter/project/FlutterProjectOpenProcessor.kt:21:0-89:1) and [FlutterStudioProjectOpenProcessor](cci:2://file:///usr/local/google/home/jwren/src/flutter-intellij/src/io/flutter/editor/FlutterStudioProjectOpenProcessor.kt:17:0-61:1)
    from the deprecated [doOpenProject](cci:1://file:///usr/local/google/home/jwren/src/flutter-intellij/src/io/flutter/editor/FlutterStudioProjectOpenProcessor.kt:54:2-60:3) to [openProjectAsync](cci:1://file:///usr/local/google/home/jwren/src/flutter-intellij/src/io/flutter/project/FlutterProjectOpenProcessor.kt:38:2-54:3) to fix
    compatibility with IntelliJ IDEA 2025.3+.
    
    This change involves:
    - Converting [FlutterProjectOpenProcessor](cci:2://file:///usr/local/google/home/jwren/src/flutter-intellij/src/io/flutter/project/FlutterProjectOpenProcessor.kt:21:0-89:1) to Kotlin.
    - Converting [FlutterStudioProjectOpenProcessor](cci:2://file:///usr/local/google/home/jwren/src/flutter-intellij/src/io/flutter/editor/FlutterStudioProjectOpenProcessor.kt:17:0-61:1) to Kotlin.
    - Implementing [openProjectAsync](cci:1://file:///usr/local/google/home/jwren/src/flutter-intellij/src/io/flutter/project/FlutterProjectOpenProcessor.kt:38:2-54:3) using `writeAction` for project setup.
    - Enabling Kotlin compilation for the main source set in [build.gradle.kts](cci:7://file:///usr/local/google/home/jwren/src/flutter-intellij/build.gradle.kts:0:0-0:0).
    
    Fixes https://github.com/flutter/flutter-intellij/issues/8629
    See https://plugins.jetbrains.com/docs/intellij/project-open-processor.html

---
+++ src/io/flutter/project/FlutterProjectOpenProcessor.kt
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2025 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+package io.flutter.project
+
+import com.intellij.openapi.application.ApplicationInfo
+import com.intellij.openapi.application.writeAction
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.projectImport.ProjectOpenProcessor
+import icons.FlutterIcons
+import io.flutter.FlutterBundle
+import io.flutter.FlutterUtils
+import io.flutter.pub.PubRoot
+import io.flutter.utils.FlutterModuleUtils
+import java.util.Objects
+import javax.swing.Icon
+
+/**
+ * Originally `FlutterProjectOpenProcessor.java`.
+ *
+ * This processor handles opening Flutter projects when they are selected directly (e.g. via "Open" in the IDE).
+ * It delegates the actual opening to the platform's default processor (e.g. Gradle or Maven processor if applicable,
+ * or the generic project opener) and then ensures that any modules in the project are correctly configured as Flutter modules.
+ *
+ * Converted to Kotlin to support `openProjectAsync` which is a suspend function.
+ */
+open class FlutterProjectOpenProcessor : ProjectOpenProcessor() {
+  override val name: String
+    get() = FlutterBundle.message("flutter.module.name")
+
+  override fun getIcon(file: VirtualFile): Icon? {
+    return FlutterIcons.Flutter
+  }
+
+  override fun canOpenProject(file: VirtualFile): Boolean {
+    val info = ApplicationInfo.getInstance()
+    if (FlutterUtils.isAndroidStudio()) {
+      return false
+    }
+    val root = PubRoot.forDirectory(file)
+    return root != null && root.declaresFlutter()
+  }
+
+  /**
+   * Replaces the deprecated `doOpenProject`.
+   *
+   * This method is `suspend` and must be used instead of `doOpenProject` to avoid `IllegalStateException` in newer IDE versions.
+   *
+   * It performs the following steps:
+   * 1. Finds a delegate processor (e.g. Gradle) to open the project.
+   * 2. Opens the project asynchronously.
+   * 3. Once opened, checks if the project contains Flutter modules that are not yet configured as such (e.g. missing module type).
+   * 4. Configures these modules as Flutter modules within a write action.
+   */
+  override suspend fun openProjectAsync(
+    virtualFile: VirtualFile,
+    projectToClose: Project?,
+    forceOpenInNewFrame: Boolean,
+  ): Project? {
+    // Delegate opening to the platform open processor.
+    val importProvider = getDelegateImportProvider(virtualFile) ?: return null
+    val project = importProvider.openProjectAsync(virtualFile, projectToClose, forceOpenInNewFrame)
+    if (project == null || project.isDisposed) return project
+
+    // Convert any modules that use Flutter but don't have IntelliJ Flutter metadata.
+    writeAction {
+      convertToFlutterProject(project)
+    }
+
+    return project
+  }
+
+  /**
+   * Deprecated method, kept to satisfy the compiler/interface.
+   *
+   * We return `null` to indicate that this processor does not support the synchronous opening method
+   * and that `openProjectAsync` should be used instead.
+   */
+  override fun doOpenProject(
+    virtualFile: VirtualFile,
+    projectToClose: Project?,
+    forceOpenInNewFrame: Boolean,
+  ): Project? {
+    return null
+  }
+
+  protected open fun getDelegateImportProvider(file: VirtualFile): ProjectOpenProcessor? {
+    return EXTENSION_POINT_NAME.extensionList.stream().filter { processor: ProjectOpenProcessor ->
+      processor.canOpenProject(file) && !Objects.equals(
+        processor.name,
+        name
+      )
+    }.findFirst().orElse(null)
+  }
+
+
+  companion object {
+    /**
+     * Sets up a project that doesn't have any Flutter modules.
+     *
+     *
+     * (It probably wasn't created with "flutter create" and probably didn't have any IntelliJ configuration before.)
+     */
+    private fun convertToFlutterProject(project: Project) {
+      for (module in FlutterModuleUtils.getModules(project)) {
+        if (FlutterModuleUtils.declaresFlutter(module) && !FlutterModuleUtils.isFlutterModule(module)) {
+          FlutterModuleUtils.setFlutterModuleAndReload(module, project)
+        }
+      }
+    }
+  }
+}
