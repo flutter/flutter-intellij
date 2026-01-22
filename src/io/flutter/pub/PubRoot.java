cda923e291a2ed3ba730024b7b742a2f498790d3
Phil Quitslund <pq@users.noreply.github.com>
2026-01-22 13:33:25 -0800

    [cq] remove straggling `.packages` support (#8724)
    Fixes: https://github.com/flutter/flutter-intellij/issues/8722 and
    opportunistically removes some dead code.
    
    **Note:** nothing urgent here; just tidying up! 🧹 
    
    ---
    
    - [x] I’ve reviewed the contributor guide and applied the relevant
    portions to this PR.
    
    <details>
      <summary>Contribution guidelines:</summary><br>
    
    - See our [contributor
    guide]([https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md](https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md)
    for general expectations for PRs.
    - Larger or significant changes should be discussed in an issue before
    creating a PR.
    - Dart contributions to our repos should follow the [Dart style
    guide](https://dart.dev/guides/language/effective-dart) and use `dart
    format`.
    - Java and Kotlin contributions should strive to follow Java and Kotlin
    best practices
    ([discussion](https://github.com/flutter/flutter-intellij/issues/8098)).
    </details>

--- src/io/flutter/pub/PubRoot.java
+++ src/io/flutter/pub/PubRoot.java
@@ -33,7 +33,6 @@ import java.util.Objects;
 public class PubRoot {
   public static final String DOT_DART_TOOL = ".dart_tool";
   public static final String PACKAGE_CONFIG_JSON = "package_config.json";
-  public static final String DOT_PACKAGES = ".packages";
   public static final String PUBSPEC_YAML = "pubspec.yaml";
 
   @NotNull
@@ -271,17 +270,6 @@ public class PubRoot {
     return FlutterUtils.isFlutterPlugin(pubspec);
   }
 
-  /**
-   * Returns true if the directory content looks like a Flutter module.
-   */
-  public boolean isFlutterModule() {
-    return root.findChild(".android") != null;
-  }
-
-  public boolean isNonEditableFlutterModule() {
-    return isFlutterModule() && root.findChild("android") == null;
-  }
-
   @Nullable
   public VirtualFile getPackageConfigFile() {
     VirtualFile rootToExpectToolsDirectory = root;
@@ -301,16 +289,6 @@ public class PubRoot {
     final VirtualFile config = tools.findChild(PACKAGE_CONFIG_JSON);
     if (config != null && !config.isDirectory()) {
       return config;
-    }
-    return null;
-  }
-
-  @Nullable
-  public VirtualFile getPackagesFile() {
-    // Obsolete by Flutter 2.0
-    final VirtualFile packages = root.findChild(DOT_PACKAGES);
-    if (packages != null && !packages.isDirectory()) {
-      return packages;
     }
     return null;
   }
@@ -340,12 +318,6 @@ public class PubRoot {
       long pubspecLastModified = new File(pubspec.getPath()).lastModified();
       long configLastModified = new File(configFile.getPath()).lastModified();
       return pubspecLastModified < configLastModified;
-    }
-    final VirtualFile packagesFile = getPackagesFile();
-    if (packagesFile != null) {
-      long pubspecLastModified = new File(pubspec.getPath()).lastModified();
-      long packagesLastModified = new File(packagesFile.getPath()).lastModified();
-      return pubspecLastModified < packagesLastModified;
     }
     return false;
   }
