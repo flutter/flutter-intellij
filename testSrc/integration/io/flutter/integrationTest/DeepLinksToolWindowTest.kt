4d3c195b5f2c60064b5d4748115935afa3be41ff
Helin Shiah <helinx@google.com>
2026-03-17 11:46:22 -0700

    Clean up project creation, deletion; update styleguide

--- testSrc/integration/io/flutter/integrationTest/DeepLinksToolWindowTest.kt
+++ testSrc/integration/io/flutter/integrationTest/DeepLinksToolWindowTest.kt
@@ -6,55 +6,36 @@
 
 package io.flutter.integrationTest
 
+import com.intellij.driver.sdk.invokeAction
 import com.intellij.driver.sdk.ui.components.UiComponent.Companion.waitFound
 import com.intellij.driver.sdk.ui.components.common.ideFrame
-import com.intellij.driver.sdk.ui.components.common.toolwindows.projectView
 import com.intellij.driver.sdk.ui.components.common.toolWindow
-import com.intellij.driver.sdk.invokeAction
-import com.intellij.driver.sdk.waitForIndicators
+import com.intellij.driver.sdk.ui.components.common.toolwindows.projectView
 import com.intellij.driver.sdk.waitForIndicators
 import com.intellij.ide.starter.driver.engine.BackgroundRun
 import com.intellij.ide.starter.driver.engine.runIdeWithDriver
 import com.intellij.ide.starter.junit5.config.UseLatestDownloadedIdeBuild
-import io.flutter.integrationTest.utils.newProjectWelcomeScreen
+import com.intellij.ide.starter.project.LocalProjectInfo
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import java.nio.file.Paths
 import kotlin.time.Duration.Companion.minutes
-import com.intellij.ide.starter.project.LocalProjectInfo
 
 @Tag("ui")
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(UseLatestDownloadedIdeBuild::class)
 class DeepLinksToolWindowTest {
 
-  companion object {
-    var testProjectName = ""
-
-    @JvmStatic
-    @AfterAll
-    fun cleanUpTestFolder() {
-      val projectPath = Paths.get(System.getProperty("java.io.tmpdir"), testProjectName)
-      val projectFile = projectPath.toFile()
-      if (projectFile.exists()) {
-        projectFile.deleteRecursively()
-        println("Successfully deleted test folder: $projectPath")
-      } else {
-        println("Test folder does not exist, skipping cleanup: $projectPath")
-      }
-    }
-  }
-
+  private val testProjectName = "my_deep_links_test_project_${System.currentTimeMillis()}"
   private lateinit var run: BackgroundRun
 
-  @BeforeEach
-  fun initContext() {
-    testProjectName = "my_deep_links_test_project_${System.currentTimeMillis()}"
+  @BeforeAll
+  fun createProject() {
     val flutterSdk = System.getenv("FLUTTER_SDK")
       ?: throw IllegalStateException("FLUTTER_SDK environment variable not set")
     val flutterExe = Paths.get(flutterSdk, "bin", "flutter").toString()
@@ -83,6 +64,21 @@ class DeepLinksToolWindowTest {
     }
   }
 
+  @AfterAll
+  fun cleanUpTestFolder() {
+    // Clean up the project folder
+    if (testProjectName.isNotEmpty()) {
+      val projectPath = Paths.get(System.getProperty("java.io.tmpdir"), testProjectName)
+      val projectFile = projectPath.toFile()
+      if (projectFile.exists()) {
+        projectFile.deleteRecursively()
+        println("Successfully deleted test folder: $projectPath")
+      } else {
+        println("Test folder does not exist, skipping cleanup: $projectPath")
+      }
+    }
+  }
+
   @Test
   fun testDeepLinksToolWindow() {
     println("Initializing IDE test context")
@@ -105,10 +101,10 @@ class DeepLinksToolWindowTest {
 
         // Wait for the tool window to be focused or exist
         toolWindow("Flutter Deep Links") {
-            waitFound(1.minutes)
-            
-            // Verify a JCEF or JxBrowser component exists. We use 'Browser' in class name robustly.
-            x("//div[contains(@class, 'Browser')]").waitFound(1.minutes)
+          waitFound(1.minutes)
+
+          // Verify a JCEF or JxBrowser component exists. We use 'Browser' in class name robustly.
+          x("//div[contains(@class, 'Browser')]").waitFound(1.minutes)
         }
       }
     }
