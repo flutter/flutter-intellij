2e36fa99c1c44bae77e2c37ec14ec7e9b1c31b5b
Helin Shiah <helinx@google.com>
2026-04-14 14:37:35 -0700

    Clean up

--- src/io/flutter/FlutterInitializer.java
+++ src/io/flutter/FlutterInitializer.java
@@ -97,8 +97,6 @@ public class FlutterInitializer extends FlutterProjectActivity {
     // Start a DevTools server
     DevToolsService.getInstance(project);
 
-
-
     // If the project declares a Flutter dependency, do some extra initialization.
     boolean hasFlutterModule = false;
 
