e12e1c4ccf4cf9c055cc0182feebb9b0f623a930
Helin Shiah <helinx@google.com>
2026-05-04 21:26:08 -0700

    Add java 21 resolver and try removing other settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,9 +4,12 @@
  * found in the LICENSE file.
  */
 
-pluginManagement {
-  repositories {
-    maven("https://oss.sonatype.org/content/repositories/snapshots/")
-    gradlePluginPortal()
-  }
+//pluginManagement {
+//  repositories {
+//    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+//    gradlePluginPortal()
+//  }
+//}
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
