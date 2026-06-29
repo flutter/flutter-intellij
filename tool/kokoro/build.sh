23c9bc79c6b17299cb29af5967bb6789b728a193
Phil Quitslund <pq@users.noreply.github.com>
2026-06-29 15:50:32 -0700

    [infra] gradle retries for deployment (#9024)
    Adds retries to the release script and does a bit of general tidying up.
    
    Fixes: https://github.com/flutter/flutter-intellij/issues/9021.
    
    See also: https://github.com/flutter/dart-intellij-third-party/pull/496.
    
    ---
    
    Review the contribution guidelines below:
    
    - [x] I’ve reviewed the contributor guide and applied the relevant
    portions to this PR.
    - [x] I've included the required information in the description above.
    - [x] My up-to-date information is in the `AUTHORS` file.
    - [x] I've updated `CHANGELOG.md` if appropriate.
    
    <details>
      <summary>Contribution guidelines:</summary><br>
    
    - See
      our [contributor guide](../CONTRIBUTING.md) and
    the [Flutter organization contributor
    guide]([https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md)
      for general expectations for PRs.
    - Larger or significant changes should be discussed in an issue before
    creating a PR.
    - Dart contributions to our repos should follow the [Dart style
    guide](https://dart.dev/guides/language/effective-dart) and use
      `dart format`.
    - Java and Kotlin contributions should strive to follow Java and Kotlin
    best
    practices
    ([discussion](https://github.com/flutter/flutter-intellij/issues/8098)).
    
    </details>

--- tool/kokoro/build.sh
+++ tool/kokoro/build.sh
@@ -4,36 +4,6 @@ setup
 
 echo "kokoro build start"
 
-MAX_RETRIES=3
-RETRY_DELAY_SECS=15
-ATTEMPT=1
-
-# Retry the build a few times to address transient failures.
-# See: https://github.com/flutter/flutter-intellij/issues/9009
-while [[ $ATTEMPT -le $MAX_RETRIES ]]; do
-  echo "Running buildPlugin (Attempt $ATTEMPT of $MAX_RETRIES)..."
-  
-  if ./gradlew buildPlugin; then
-    echo "Gradle build completed successfully."
-    break
-  else
-    exit_code=$?
-    if [[ $exit_code -ge 128 ]]; then
-      echo "Build interrupted. Exiting."
-      exit $exit_code
-    fi
-  fi
-
-  echo "Build failed on attempt $ATTEMPT."
-  
-  if [[ $ATTEMPT -eq $MAX_RETRIES ]]; then
-    echo "All $MAX_RETRIES attempts failed. Exiting."
-    exit $exit_code
-  fi
-
-  echo "Waiting $RETRY_DELAY_SECS seconds before retrying..."
-  sleep $RETRY_DELAY_SECS
-  ATTEMPT=$((ATTEMPT + 1))
-done
+run_gradle_with_retry buildPlugin
 
 echo "kokoro build finished"
